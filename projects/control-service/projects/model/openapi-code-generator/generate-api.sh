9c75d9bbb22b41dbf848855cd330ca4b637a730e
Miroslav Ivanov <miroslavi@vmware.com>
2021-08-05 15:40:18 +0300

    control-service: model initial commit
    This is intial commit of model project into versatile-data-kit
    repository.
    
    Model project contains the definition of Versatile Data Kit APIs.
    
    Testing Done: pull request CICD.
    
    Signed-off-by: Miroslav Ivanov miroslavi@vmware.com

---
+++ projects/control-service/projects/model/openapi-code-generator/generate-api.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+
+#
+# This script builds a Docker image that starts Control Plane Base
+#
+#
+
+set -e
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  SCRIPTDIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$SCRIPTDIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SCRIPTDIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+BRANCHROOT="$SCRIPTDIR/../../../"
+#set -x
+###################################################################################################
+
+
+java -jar $SCRIPTDIR/openapi-generator-cli.jar generate --generator-name spring --config $SCRIPTDIR/config-spring.json --output $BRANCHROOT/model -i $SCRIPTDIR/../apidefs/datajob-api/api.yaml
+rm -rf $BRANCHROOT/model/.openapi-generator*
+rm  $BRANCHROOT/model/README.md
+rm  $BRANCHROOT/model/pom.xml
