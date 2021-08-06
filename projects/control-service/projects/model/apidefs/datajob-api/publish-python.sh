90e7829e38db14ce69ecbd6578dd039d63bba52e
Miroslav Ivanov <40535952+mivanov1988@users.noreply.github.com>
2021-08-06 10:59:26 +0300

    control-service: model initial commit (#36)
    This is intial commit of model project into versatile-data-kit
    repository.
    
    Model project contains the definition of Versatile Data Kit APIs.
    
    Testing Done: pull request CICD.
    
    Signed-off-by: Miroslav Ivanov miroslavi@vmware.com

---
+++ projects/control-service/projects/model/apidefs/datajob-api/publish-python.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+# This script needs the following environment variables:
+# ARTIFACTORY_USER, ARTIFACTORY_PASS - Artifactory credentials
+
+cd build/python
+python3.7 -m venv venv
+source venv/bin/activate
+
+pip install -U pip setuptools wheel twine
+pip install -r requirements.txt
+
+rm -rf dist/
+python setup.py sdist --formats=gztar
+twine upload --repository-url "$PIP_REPO_UPLOAD_URL" -u "$PIP_REPO_UPLOAD_USER_NAME" -p "$PIP_REPO_UPLOAD_USER_PASSWORD" dist/*tar.gz
