2c654d2a4bbaffe0e73b78a5f68e5798df8e447f
Gabriel Georgiev <45939426+gageorgiev@users.noreply.github.com>
2021-08-12 14:36:18 +0300

    vdk-core: vdk-logging-json (#80)
    Implement plugin which changes the logging format to conform
    to JSON.
    
    Testing done: unit test, pipelines passed
    After installing, data job logs appear like so:
    ```
    {"@timestamp":"2021-08-04T12:51:11.532Z","created":"1628070671","jobname":"example-job","level":"DEBUG","modulename":"taurus.vdk.trino_connection","filename":"managed_connection_base.py","lineno":"69","funcname":"connect","attemptid":"1628070671-452613-739749","message":"Established <trino.dbapi.Connection object at 0x10b9b1d30>"}
    ```
    
    Signed-off-by: gageorgiev <gageorgiev@vmware.com>

---
+++ projects/vdk-core/plugins/vdk-logging-json/setup.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2021 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+import pathlib
+
+import setuptools
+
+__version__ = "0.1.0"
+
+setuptools.setup(
+    name="vdk-logging-json",
+    version=__version__,
+    url="https://github.com/vmware/versatile-data-kit",
+    description="Versatile Data Kit SDK plugin that changes logging output to JSON format.",
+    long_description=pathlib.Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    install_requires=["vdk-core"],
+    package_dir={"": "src"},
+    packages=setuptools.find_namespace_packages(where="src"),
+    entry_points={"vdk.plugin.run": ["vdk-logging-json = taurus.vdk.logging_json"]},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
