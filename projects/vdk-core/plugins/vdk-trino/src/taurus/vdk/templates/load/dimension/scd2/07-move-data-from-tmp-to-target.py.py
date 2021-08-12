4df4bda700a37afe2c7cc7ed21918c6c6472f32b
Yana Zhivkova <yzhivkova@vmware.com>
2021-08-12 21:01:08 +0300

    vdk-trino: Refactor SCD2 and fact snapshot templates
    Both templates have restore from backup step and safely (with backup)
    move data to target step. Therefore, their first and last steps share
    a lot of code.
    
    TrinoQueries class is renamed to TrinoTemplateQueries, since it is only
    used in templates and is also tightly related to them (ex. it reads its
    move data strategy from config called
    TRINO_TEMPLATES_DATA_TO_TARGET_STRATEGY). Two methods are added in it -
    ensure_target_exists_step and perform_safe_move_data_to_table_step.
    SCD2 and fact snapshot templates use them in their common steps.
    
    Templates unit tests.
    
    The commit is a non-breaking change which refactors two templates.
    
    Signed-off-by: Yana Zhivkova (yzhivkova@vmware.com)

--- projects/vdk-core/plugins/vdk-trino/src/taurus/vdk/templates/load/dimension/scd2/07-move-data-from-tmp-to-target.py.py
+++ projects/vdk-core/plugins/vdk-trino/src/taurus/vdk/templates/load/dimension/scd2/07-move-data-from-tmp-to-target.py.py
@@ -20,39 +20,19 @@ def run(job_input: IJobInput):
     5. If 3 fails, target table is lost, its content are in backup_target_table. Next job retry will try to
     recover target on its first step.
     6. If 2 succeeds, drop backup, we are ready.
-
-    Note: If there is no data in tmp_target_table, we are sure that the source table provided initially was empty,
-    so we do nothing, target (snapshot) remains unchanged and we drop the empty tmp_target_table.
     """
 
     args = job_input.get_arguments()
     target_schema = args.get("target_schema")
-    source_view = args.get("source_view")
     target_table = args.get("target_table")
     tmp_target_table = "tmp_" + target_table
     backup_target_table = "backup_" + target_table
     trino_queries = TrinoTemplateQueries(job_input)
 
-    log.debug("Check if tmp target has data.")
-    res = job_input.execute_query(
-        f"""
-        SELECT COUNT(*) FROM {target_schema}.{tmp_target_table}
-        """
+    trino_queries.perform_safe_move_data_to_table_step(
+        from_db=target_schema,
+        from_table_name=tmp_target_table,
+        to_db=target_schema,
+        to_table_name=target_table,
+        backup_table_name=backup_target_table,
     )
-    if res and res[0][0] > 0:
-        log.debug(
-            "Confirmed that tmp target has data, proceed with moving it to target."
-        )
-        trino_queries.perform_safe_move_data_to_table_step(
-            from_db=target_schema,
-            from_table_name=tmp_target_table,
-            to_db=target_schema,
-            to_table_name=target_table,
-            backup_table_name=backup_target_table,
-        )
-    else:
-        log.info(
-            f"Target table {target_schema}.{target_table} remains unchanged "
-            f"because source table {target_schema}.{source_view} was empty."
-        )
-        trino_queries.drop_table(target_schema, tmp_target_table)
