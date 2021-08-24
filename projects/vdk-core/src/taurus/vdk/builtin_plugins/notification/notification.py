7747f927069f363fe5a09bb13ae9bb6d6be252c7
Antoni Ivanov <aivanov@vmware.com>
2021-08-24 19:17:03 +0300

    vdk-core: refactor notification configuration (#128)
    Create separate class for notification configuration whcih encapusulates
    vdk-core Configuration class exposing only noification related
    configuration parameters.
    
    Testing Done: existing unit tests
    
    Signed-off-by: Antoni Ivanov <aivanov@vmware.com>

--- projects/vdk-core/src/taurus/vdk/builtin_plugins/notification/notification.py
+++ projects/vdk-core/src/taurus/vdk/builtin_plugins/notification/notification.py
@@ -6,6 +6,7 @@ from taurus.api.plugin.hook_markers import hookimpl
 from taurus.vdk.builtin_plugins.config.job_config import JobConfigKeys
 from taurus.vdk.builtin_plugins.config.vdk_config import LOG_CONFIG
 from taurus.vdk.builtin_plugins.notification import notification_base
+from taurus.vdk.builtin_plugins.notification import notification_configuration
 from taurus.vdk.builtin_plugins.run.execution_state import ExecutionStateStoreKeys
 from taurus.vdk.core import errors
 from taurus.vdk.core.config import ConfigurationBuilder
@@ -13,11 +14,6 @@ from taurus.vdk.core.context import CoreContext
 from taurus.vdk.core.statestore import CommonStoreKeys
 
 log = logging.getLogger(__name__)
-
-NOTIFICATION_JOB_LOG_URL_PATTERN = "NOTIFICATION_JOB_LOG_URL_PATTERN"
-NOTIFICATION_EMAIL_CC_LIST = "NOTIFICATION_EMAIL_CC_LIST"
-NOTIFICATION_SMTP_HOST = "NOTIFICATION_SMTP_HOST"
-NOTIFICATION_SENDER = "NOTIFICATION_SENDER"
 
 
 def _notify(error_overall, user_error, configuration, state):
@@ -27,10 +23,14 @@ def _notify(error_overall, user_error, configuration, state):
     job_name = state.get(ExecutionStateStoreKeys.JOB_NAME)
     op_id = state.get(CommonStoreKeys.OP_ID)
 
-    job_log_url_template = configuration.get_value(NOTIFICATION_JOB_LOG_URL_PATTERN)
-    cc = configuration.get_value(NOTIFICATION_EMAIL_CC_LIST)
-    smtp_host = configuration.get_value(NOTIFICATION_SMTP_HOST)
-    sender = configuration.get_value(NOTIFICATION_SENDER)
+    notification_cfg = notification_configuration.NotificationConfiguration(
+        configuration
+    )
+
+    job_log_url_template = notification_cfg.get_job_log_url_pattern()
+    cc = notification_cfg.get_email_cc_list()
+    smtp_host = notification_cfg.get_smtp_host()
+    sender = notification_cfg.get_sender()
 
     if not error_overall:
         recipients = configuration.get_value(JobConfigKeys.NOTIFIED_ON_JOB_SUCCESS)
@@ -74,33 +74,7 @@ def _notify(error_overall, user_error, configuration, state):
 class NotificationPlugin:
     @hookimpl
     def vdk_configure(self, config_builder: ConfigurationBuilder):
-        config_builder.add(
-            key=NOTIFICATION_JOB_LOG_URL_PATTERN,
-            default_value=(
-                "https://example-job-log-url.com/{op_id},{start_time_ms},{end_time_ms}"
-            ),
-            description=(
-                "The URL template used to find the full log of a particular job. "
-                "It is further parametrized with the job's op_id and start and end times "
-                "by replacing the bracketed parts of the url with the respective values. "
-                "(For example, '{op_id}' becomes the actual op_id of the job.)"
-            ),
-        )
-        config_builder.add(
-            key=NOTIFICATION_EMAIL_CC_LIST,
-            default_value="",
-            description="A comma separated string of email addresses to be CC'd in the notification email.",
-        )
-        config_builder.add(
-            key=NOTIFICATION_SMTP_HOST,
-            default_value="smtp.vmware.com",
-            description="The SMTP host used for to send the notification email.",
-        )
-        config_builder.add(
-            key=NOTIFICATION_SENDER,
-            default_value="data-pipelines@vmware.com",
-            description="The email address, from which notification emails are sent.",
-        )
+        notification_configuration.add_definitions(config_builder)
 
     @hookimpl
     def vdk_exit(self, context: CoreContext, exit_code: int):
