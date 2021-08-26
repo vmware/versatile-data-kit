4db9a438770192c38d379fc578127590753ac153
Antoni Ivanov <aivanov@vmware.com>
2021-08-26 12:07:54 +0300

    vdk-core: support full smtp configuration (#134)
    As our users can use variety of SMTP servers in order to enable
    notifications I decided to use gmail as a test to see we work well.
    But there was missing configuration option in order to make it work.
    Those options are added in this change.
    
    recipients were not being parsed correctly so mail notification were not
    working at all.
    
    Testing Done: set gmail as smpt host:
    export VDK_NOTIFICATION_SMTP_HOST=smtp.gmail.com
    export VDK_NOTIFICATION_SMTP_USE_TLS=true
    export VDK_NOTIFICATION_SMTP_LOGIN_USERNAME= 'me@gmail.com'
    export VDK_NOTIFICATION_SMTP_LOGIN_PASSWORD = "Ha, ha, did you really
    think
    I would tell you."
    export VDK_NOTIFICATION_SENDER = 'me@gmail.com'
    
    and sent notification successful to myself
    
    And added functional test that run testing data job and try to sent mail
    to mock smtp server using pytest plugin -
    https://github.com/bebleo/smtpdfix
    
    Signed-off-by: Antoni Ivanov <aivanov@vmware.com>

--- projects/vdk-core/src/taurus/vdk/builtin_plugins/notification/notification_configuration.py
+++ projects/vdk-core/src/taurus/vdk/builtin_plugins/notification/notification_configuration.py
@@ -6,10 +6,16 @@ from taurus.vdk.builtin_plugins.config.job_config import JobConfigKeys
 from taurus.vdk.core.config import Configuration
 from taurus.vdk.core.config import ConfigurationBuilder
 
+NOTIFICATION_ENABLED = "NOTIFICATION_ENABLED"
 NOTIFICATION_JOB_LOG_URL_PATTERN = "NOTIFICATION_JOB_LOG_URL_PATTERN"
 NOTIFICATION_EMAIL_CC_LIST = "NOTIFICATION_EMAIL_CC_LIST"
 NOTIFICATION_SENDER = "NOTIFICATION_SENDER"
 NOTIFICATION_SMTP_HOST = "NOTIFICATION_SMTP_HOST"
+NOTIFICATION_SMTP_PORT = "NOTIFICATION_SMTP_PORT"
+NOTIFICATION_SMTP_USE_TLS = "NOTIFICATION_SMTP_USE_TLS"
+NOTIFICATION_SMTP_LOGIN_USERNAME = "NOTIFICATION_SMTP_LOGIN_USERNAME"
+NOTIFICATION_SMTP_LOGIN_PASSWORD = "NOTIFICATION_SMTP_LOGIN_PASSWORD"
+NOTIFICATION_SMTP_DEBUG_LEVEL = "NOTIFICATION_SMTP_DEBUG_LEVEL"
 
 
 class NotificationConfiguration:
@@ -30,8 +36,31 @@ class NotificationConfiguration:
     def get_sender(self) -> str:
         return str(self.__config[NOTIFICATION_SENDER])
 
-    def get_smtp_host(self) -> str:
+    def get_notification_enabled(self):
+        return bool(self.__config[NOTIFICATION_ENABLED])
+
+
+class SmtpConfiguration:
+    def __init__(self, config: Configuration):
+        self.__config = config
+
+    def get_host(self) -> str:
         return str(self.__config[NOTIFICATION_SMTP_HOST])
+
+    def get_port(self) -> int:
+        return int(self.__config[NOTIFICATION_SMTP_PORT])
+
+    def get_use_tls(self) -> bool:
+        return bool(self.__config[NOTIFICATION_SMTP_USE_TLS])
+
+    def get_login_username(self) -> str:
+        return str(self.__config[NOTIFICATION_SMTP_LOGIN_USERNAME])
+
+    def get_login_password(self) -> str:
+        return str(self.__config[NOTIFICATION_SMTP_LOGIN_PASSWORD])
+
+    def get_debug_level(self) -> int:
+        return int(self.__config[NOTIFICATION_SMTP_DEBUG_LEVEL])
 
 
 def __add_job_notified_configuration_definitions(config_builder: ConfigurationBuilder):
@@ -47,7 +76,7 @@ def __add_job_notified_configuration_definitions(config_builder: ConfigurationBu
         default_value="",
         description="Semicolon-separated list of email addresses to be notified on job execution "
         "failure caused by user code or user configuration problem. "
-        " For example: if the job contains an SQL script with syntax error.",
+        "For example: if the job contains an SQL script with syntax error.",
     )
     config_builder.add(
         key=JobConfigKeys.NOTIFIED_ON_JOB_FAILURE_PLATFORM_ERROR,
@@ -73,7 +102,85 @@ def __add_job_notified_configuration_definitions(config_builder: ConfigurationBu
     )
 
 
+def __add_smtp_configuration_definitions(config_builder):
+    config_builder.add(
+        key=NOTIFICATION_SMTP_HOST,
+        default_value="smtp.vmware.com",
+        description="The SMTP host used for sending the notification email.",
+    )
+    config_builder.add(
+        key=NOTIFICATION_SMTP_PORT,
+        default_value=25,
+        description="The SMTP port used for to send the notification email.",
+    )
+    config_builder.add(
+        key=NOTIFICATION_SMTP_USE_TLS,
+        default_value=False,
+        description="Set to true if you want to use secure connection over TLS/SSL.",
+    )
+    config_builder.add(
+        key=NOTIFICATION_SMTP_LOGIN_USERNAME,
+        default_value="",
+        description="Login username to use when connecting to SMTP server. If empty no login is attempted.",
+    )
+    config_builder.add(
+        key=NOTIFICATION_SMTP_LOGIN_PASSWORD,
+        default_value="",
+        description="Login password to use when connecting to SMTP server.",
+    )
+    config_builder.add(
+        key=NOTIFICATION_SMTP_DEBUG_LEVEL,
+        default_value=1,
+        description="SMTP debug level. A non-zero value results in debug messages for connection "
+        "and for all messages sent to and received from the SMTP server.",
+    )
+
+
+def __add_job_notified_configuration_definitions(config_builder: ConfigurationBuilder):
+    config_builder.add(
+        key=JobConfigKeys.TEAM,
+        default_value="",
+        description="Specified which is the team that owns the data job. "
+        "Value is case-sensitive and must be an actual team name as specified during job creation."
+        "",
+    )
+    config_builder.add(
+        key=JobConfigKeys.NOTIFIED_ON_JOB_FAILURE_USER_ERROR,
+        default_value="",
+        description="Semicolon-separated list of email addresses to be notified on job execution "
+        "failure caused by user code or user configuration problem. "
+        "For example: if the job contains an SQL script with syntax error.",
+    )
+    config_builder.add(
+        key=JobConfigKeys.NOTIFIED_ON_JOB_FAILURE_PLATFORM_ERROR,
+        default_value="",
+        description="Semicolon-separated list of email addresses to be notified on job execution failure."
+        "caused by a platform problem, including job execution delays.",
+    )
+    config_builder.add(
+        key=JobConfigKeys.NOTIFIED_ON_JOB_SUCCESS,
+        default_value="",
+        description="Semicolon-separated list of email addresses to be notified on job execution success.",
+    )
+    config_builder.add(
+        key=JobConfigKeys.NOTIFIED_ON_JOB_DEPLOY,
+        default_value="",
+        description="Semicolon-separated list of email addresses to be notified of job deployment outcome.",
+    )
+    config_builder.add(
+        key=JobConfigKeys.ENABLE_ATTEMPT_NOTIFICATIONS,
+        default_value=False,
+        description="Flag to enable or disable the email notifications sent to the recipients listed above "
+        "for each data job run attempt.",
+    )
+
+
 def add_definitions(config_builder: ConfigurationBuilder):
+    config_builder.add(
+        key=NOTIFICATION_ENABLED,
+        default_value=False,
+        description="Set to true if you want to enable notifications regardless if it is run locally or not.",
+    )
     config_builder.add(
         key=NOTIFICATION_JOB_LOG_URL_PATTERN,
         default_value=(
@@ -96,9 +203,6 @@ def add_definitions(config_builder: ConfigurationBuilder):
         default_value="data-pipelines@vmware.com",
         description="The email address, from which notification emails are sent.",
     )
-    config_builder.add(
-        key=NOTIFICATION_SMTP_HOST,
-        default_value="smtp.vmware.com",
-        description="The SMTP host used for to send the notification email.",
-    )
+
+    __add_smtp_configuration_definitions(config_builder)
     __add_job_notified_configuration_definitions(config_builder)
