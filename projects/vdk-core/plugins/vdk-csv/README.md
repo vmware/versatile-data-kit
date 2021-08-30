2835614cd58e840f3025dd12783278eef39003c1
Antoni Ivanov <aivanov@vmware.com>
2021-08-30 13:46:02 +0300

    vdk-csv: add new plugin - vdk ingest-csv command
    Ingesting a CSV file (or excel or other tabular file that can be easily
    converted to csv)  into a database so that it can be analysized is a
    very frequent use-case. A lot of tools allow one-off
    export of data into csv which can be ingested one-off. Also excel or
    similar is used to store manually edited data which people may ingest
    for analysis.
    
    We want to make this as easy as possible for users to do so.
    
    We expose command vdk ingest-csv which user can point to csv file and we
    automatically will ingesting using Ingestion API so they can all
    benefitsfrom it - users can specify configruable destination, the
    ingestion is done in parallel with support for batching requests and so
    on.
    
    It allows to specify table name and extra options.
    We need table name so that users can customize the destination table. We
    infer it from file name if not specified.
    
    Extra options enable users to handle corner cases in ingesting csv file.
    Those extra options are same as pandas.read_csv. Our contract for
    reading csv file is the same as pandas.read_csv .
    And we enable the same customization as pandas.read_csv.
    
    Testing Done: added functional test and tested manually
    ```
    pip install vdk-ingest-file
    export VDK_INGEST_METHOD_DEFAULT=file
    vdk ingest-csv -f test.csv
    ```
    And verified that file test.xxx.json is created with "ingested" data.
    
    Signed-off-by: Antoni Ivanov <aivanov@vmware.com>

---
+++ projects/vdk-core/plugins/vdk-csv/README.md
@@ -0,0 +1,21 @@
+## Versatile Data Kit CSV Plugin
+
+This plugin provides functionality to ingest and export CSV files.
+
+### Usage
+
+To use the plugin, just install it using
+
+```bash
+pip install vdk-csv
+```
+
+Then run help to see what you can do.
+```bash
+vdk ingest-csv --help
+```
+
+For example you can ingest CSV file into tables.
+
+In order to configure ingestion method and target you can configure ingestion
+Run `vdk config-help` and look for options starting with VDK_INGEST
---
+++ projects/vdk-core/plugins/vdk-csv/tests/functional/test_csv_plugin.py
@@ -0,0 +1,79 @@
+# Copyright (c) 2021 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+import os
+from unittest import mock
+
+from click.testing import Result
+from taurus.vdk import csv_plugin
+from taurus.vdk.test_utils.util_funcs import cli_assert_equal
+from taurus.vdk.test_utils.util_funcs import CliEntryBasedTestRunner
+from taurus.vdk.test_utils.util_plugins import IngestIntoMemoryPlugin
+
+
+def _get_file(filename):
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), filename)
+
+
+@mock.patch.dict(
+    os.environ,
+    {"VDK_INGEST_METHOD_DEFAULT": "memory"},
+)
+def test_ingestion_csv():
+    ingest_plugin = IngestIntoMemoryPlugin()
+    runner = CliEntryBasedTestRunner(ingest_plugin, csv_plugin)
+
+    result: Result = runner.invoke(["ingest-csv", "-f", _get_file("test.csv")])
+    cli_assert_equal(0, result)
+
+    assert len(ingest_plugin.payloads) > 0
+    assert len(ingest_plugin.payloads[0].payload) == 4
+    expected_first_row = {
+        "@table": "test",
+        "Transaction_date": "01/02/2009 04:53",
+        "Product": "Product1",
+        "Price": 1200,
+        "Latitude": 39.195,
+        "Longitude": -94.68194,
+        "US Zip": 8056,
+    }
+    assert ingest_plugin.payloads[0].payload[0] == expected_first_row
+
+
+@mock.patch.dict(
+    os.environ,
+    {"VDK_INGEST_METHOD_DEFAULT": "memory"},
+)
+def test_ingestion_csv_with_target_table():
+    ingest_plugin = IngestIntoMemoryPlugin()
+    runner = CliEntryBasedTestRunner(ingest_plugin, csv_plugin)
+
+    result: Result = runner.invoke(
+        ["ingest-csv", "-f", _get_file("test.csv"), "-t", "table_name"]
+    )
+    cli_assert_equal(0, result)
+
+    assert len(ingest_plugin.payloads) > 0
+    assert ingest_plugin.payloads[0].destination_table == "table_name"
+
+
+@mock.patch.dict(
+    os.environ,
+    {"VDK_INGEST_METHOD_DEFAULT": "memory"},
+)
+def test_ingestion_csv_with_options():
+    ingest_plugin = IngestIntoMemoryPlugin()
+    runner = CliEntryBasedTestRunner(ingest_plugin, csv_plugin)
+
+    result: Result = runner.invoke(
+        [
+            "ingest-csv",
+            "-f",
+            _get_file("test.csv"),
+            "-o",
+            '{"dtype": {"US Zip": "str"}}',
+        ]
+    )
+    cli_assert_equal(0, result)
+
+    assert len(ingest_plugin.payloads) > 0
+    assert ingest_plugin.payloads[0].payload[0]["US Zip"] == "08056"
