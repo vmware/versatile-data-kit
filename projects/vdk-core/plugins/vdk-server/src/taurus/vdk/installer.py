1c15cbcec1394d4f92fc1e13cddfd615c432e8ca
tpalashki <87482487+tpalashki@users.noreply.github.com>
2021-09-02 16:04:53 +0300

    vdk-server: fix git server installation (#183)
    The initial implementation of the Git server installation had some flaws
    that are resolved as part of this commit.
    
    * The server configuration could fail if the server has not started yet
    even though the container was running;
    * The configuration request appears to fail because it automatically
    redirects to /user/login after installation;
    * The request for the Git repository creation was failing because the
    parameters were not passed in correctly;
    
    Testing done: locally running the plugin and verifying that it was running
    and the Git server was created and configured properly and the repository
    was created.
    
    Signed-off-by: Tsvetomir Palashki <tpalashki@vmware.com>

--- projects/vdk-core/plugins/vdk-server/src/taurus/vdk/installer.py
+++ projects/vdk-core/plugins/vdk-server/src/taurus/vdk/installer.py
@@ -1,4 +1,6 @@
 import base64
+import time
+
 import docker
 import logging
 import os
@@ -6,7 +8,6 @@ import subprocess
 import sys
 import requests
 from kubernetes import client, config, utils
-
 
 log = logging.getLogger(__name__)
 
@@ -22,7 +23,7 @@ class Installer(object):
     docker_registry_container_name = "vdk-docker-registry"
     # Currently changing the default Docker registry port does not work correctly,
     # i.e. subsequent image pulls from the registry fail
-    docker_registry_port = 5000
+    __docker_registry_port = 5000
     helm_installation_name = "vdk"
     helm_repo_url = "https://gitlab.com/api/v4/projects/28814611/packages/helm/stable"
     helm_chart_name = "vdk-gitlab/pipelines-control-service"
@@ -33,14 +34,13 @@ class Installer(object):
     git_server_admin_email = "vdkuser@vmware.com"
     git_server_repository_name = "vdk-git-repo"
 
-
     def install(self):
         """
         Installs all necessary components and configurations.
         """
         self.__create_docker_registry_container()
         if self.__create_git_server_container():
-            self.__configure_git_server()
+            self.__configure_git_server_with_error_handling()
             self.__create_git_repository()
             self.__restart_git_server_container()
         self.__create_kind_cluster()
@@ -73,19 +73,21 @@ class Installer(object):
             try:
                 # docker run -d --restart=always -p "127.0.0.1:${docker_registry_port}:5000" --name "${docker_registry_name}" registry:2
                 client.containers.run("registry:2",
-                                       detach=True,
-                                       restart_policy={"Name": "always"},
-                                       name=self.docker_registry_container_name,
-                                       ports={'5000/tcp': ('127.0.0.1', self.docker_registry_port)})
+                                      detach=True,
+                                      restart_policy={"Name": "always"},
+                                      name=self.docker_registry_container_name,
+                                      ports={'5000/tcp': ('127.0.0.1', self.__docker_registry_port)})
             except Exception as ex:
-                log.error(f"Error: Failed to create Docker registry container {self.docker_registry_container_name}. {str(ex)}")
+                log.error(
+                    f"Error: Failed to create Docker registry container {self.docker_registry_container_name}. {str(ex)}")
         finally:
             client.close
 
     def __delete_docker_registry_container(self):
         self.__delete_container(self.docker_registry_container_name)
 
-    def __delete_container(self, container_name: str):
+    @staticmethod
+    def __delete_container(container_name: str):
         """
         Deletes the Docker registry container with the specified name.
         """
@@ -102,7 +104,8 @@ class Installer(object):
     def __restart_git_server_container(self):
         self.__restart_container(self.git_server_container_name)
 
-    def __restart_container(self, container_name: str):
+    @staticmethod
+    def __restart_container(container_name: str):
         """
         Restarts the container with the specified name.
         """
@@ -133,9 +136,9 @@ class Installer(object):
             try:
                 # docker run --name=vdk-git-server -p 10022:22 -p 10080:3000 -p 10081:80 gogs/gogs:0.12
                 client.containers.run("gogs/gogs:0.12",
-                                       detach=True,
-                                       name=self.git_server_container_name,
-                                       ports={'22/tcp': '10022', '3000/tcp': '10080', '80/tcp': '10081'})
+                                      detach=True,
+                                      name=self.git_server_container_name,
+                                      ports={'22/tcp': '10022', '3000/tcp': '10080', '80/tcp': '10081'})
                 return True
             except Exception as ex:
                 log.error(f"Error: Failed to create Git server container {self.git_server_container_name}. {str(ex)}")
@@ -145,7 +148,8 @@ class Installer(object):
     def __delete_git_server_container(self):
         self.__delete_container(self.git_server_container_name)
 
-    def __connect_container_to_kind_network(self, container_name: str):
+    @staticmethod
+    def __connect_container_to_kind_network(container_name: str):
         """
         Connects a Docker container to the Kind cluster network.
         If the container is already connected, an info message is logged.
@@ -158,6 +162,35 @@ class Installer(object):
         finally:
             client.close
 
+    def __configure_git_server_with_error_handling(self):
+        """
+        Configures the Git server at localhost:10080 with the configurations
+        specified by the git_server_* variables.
+
+        The attempt may fail if the Git server is not yet running (even though the container was created)
+        with 'Connection reset by peer' error. In this case, the request will be retried with a back-off
+        several times before failing.
+
+        The request may fail even if the installation was successful. This happens due to automatic redirect
+        to /user/login after installation, which fails and in turn causes the entire request to fail.
+        """
+        attempt = 1
+        max_attempts = 5
+        while attempt <= max_attempts:
+            try:
+                self.__configure_git_server()
+            except Exception as ex:
+                ex_as_string = str(ex)
+                if "Connection reset by peer" in ex_as_string:
+                    log.debug(f"Failed to configure git server. Will reattempt in 2 seconds...")
+                    attempt += 1
+                    time.sleep(2)
+                    continue
+                if "/user/login" not in ex_as_string:
+                    log.error(f"Error: Failed to configure git server. {ex_as_string}")
+                    sys.exit(1)
+                break
+
     def __configure_git_server(self):
         """
         Configures the Git server at localhost:10080 with the configurations
@@ -165,34 +198,30 @@ class Installer(object):
         """
         # curl -X POST 'http://localhost:10080/install'  \
         # -d "db_type=SQLite3&db_path=data/gogs.db&app_name=Gogs&repo_root_path=/data/git/gogs-repositories&run_user=git&domain=localhost&ssh_port=22&http_port=80&app_url=http://localhost/&log_root_path=/app/gogs/log&smtp_host=&smtp_from=&smtp_user=&smtp_passwd=&disable_gravatar=true&enable_captcha=false&register_confirm=false&admin_name=vdkuser&admin_passwd=vdkpass&admin_confirm_passwd=vdkpass&admin_email=vdk_user@vmware.com"
-        try:
-            requests.post("http://localhost:10080/install",
-                          params={
-                              'db_type': 'SQLite3',
-                              'db_path': 'data/gogs.db',
-                              'app_name': 'Gogs',
-                              'repo_root_path': '/data/git/gogs-repositories',
-                              'run_user': 'git',
-                              'domain': 'localhost',
-                              'ssh_port': 22,
-                              'http_port': 80,
-                              'app_url': 'http://localhost/',
-                              'log_root_path': '/app/gogs/log',
-                              'smtp_host': '',
-                              'smtp_from': '',
-                              'smtp_user': '',
-                              'smtp_passwd': '',
-                              'disable_gravatar': 'true',
-                              'enable_captcha': 'false',
-                              'register_confirm': 'false',
-                              'admin_name': self.git_server_admin_user,
-                              'admin_passwd': self.git_server_admin_password,
-                              'admin_confirm_passwd': self.git_server_admin_password,
-                              'admin_email': self.git_server_admin_email
-                          })
-        except Exception as ex:
-            log.error(f"Error: Failed to configure git server. {str(ex)}")
-            sys.exit(1)
+        requests.post("http://localhost:10080/install",
+                      data={
+                          'db_type': 'SQLite3',
+                          'db_path': 'data/gogs.db',
+                          'app_name': 'Gogs',
+                          'repo_root_path': '/data/git/gogs-repositories',
+                          'run_user': 'git',
+                          'domain': 'localhost',
+                          'ssh_port': 22,
+                          'http_port': 80,
+                          'app_url': 'http://localhost/',
+                          'log_root_path': '/app/gogs/log',
+                          'smtp_host': '',
+                          'smtp_from': '',
+                          'smtp_user': '',
+                          'smtp_passwd': '',
+                          'disable_gravatar': 'true',
+                          'enable_captcha': 'false',
+                          'register_confirm': 'false',
+                          'admin_name': self.git_server_admin_user,
+                          'admin_passwd': self.git_server_admin_password,
+                          'admin_confirm_passwd': self.git_server_admin_password,
+                          'admin_email': self.git_server_admin_email
+                      })
 
     def __create_git_repository(self):
         """
@@ -209,23 +238,23 @@ class Installer(object):
         # }'
         try:
             credentials = f"{self.git_server_admin_user}:{self.git_server_admin_password}"
-            base64_credentials = base64.b64encode(credentials.encode("ascii"))
+            base64_credentials = base64.b64encode(credentials.encode("ascii")).decode("ascii")
             requests.post(f"http://localhost:10080/api/v1/admin/users/{self.git_server_admin_user}/repos",
                           headers={
                               'authorization': f'Basic {base64_credentials}',
                               'content-type': 'application/json'
                           },
-                          data={
+                          json={
                               'name': self.git_server_repository_name,
-                              'description': 'This is a VDK repository for storing the source code of data jobs',
-                              'private': 'false'
+                              'description': 'A repository for storing the source code of VDK data jobs',
+                              'private': False
                           })
         except Exception as ex:
             log.error(f"Error: Failed to create git repository. {str(ex)}")
             sys.exit(1)
 
-
-    def __transform_file(self, input_file_name, output_file_name, transformation):
+    @staticmethod
+    def __transform_file(input_file_name, output_file_name, transformation):
         try:
             with open(input_file_name, "r") as input_file:
                 content = input_file.read()
@@ -236,22 +265,23 @@ class Installer(object):
             log.error(f"Error: Failed to transform file {input_file_name} into {output_file_name}. {str(ex)}")
 
     def __transform_template(self, content: str) -> str:
-        return content.format(docker_registry_name = self.docker_registry_container_name,
-                              docker_registry_port = self.docker_registry_port)
+        return content.format(docker_registry_name=self.docker_registry_container_name,
+                              docker_registry_port=self.__docker_registry_port)
 
     def __create_kind_cluster(self):
         """
         Creates a kind cluster with the private Docker registry enabled in containerd.
         """
+        temp_file = "kind-cluster-config.yaml"
         self.__transform_file("kind-cluster-config-template.yaml",
-                              "kind-cluster-config.yaml",
+                              temp_file,
                               self.__transform_template)
         try:
             completed_process = subprocess.run(["kind", "create", "cluster",
-                                               "--config=kind-cluster-config.yaml",
-                                               "--name", self.kind_cluster_name])
-            if os.path.exists("kind-cluster-config.yaml"):
-                os.remove("kind-cluster-config.yaml")
+                                                "--config=kind-cluster-config.yaml",
+                                                "--name", self.kind_cluster_name])
+            if os.path.exists(temp_file):
+                os.remove(temp_file)
             if completed_process.returncode == 1:
                 sys.exit(1)
         except Exception as ex:
@@ -263,7 +293,7 @@ class Installer(object):
         """
         try:
             completed_process = subprocess.run(["kind", "delete", "cluster",
-                                               "--name", self.kind_cluster_name])
+                                                "--name", self.kind_cluster_name])
             if completed_process.returncode == 1:
                 sys.exit(1)
         except Exception as ex:
@@ -278,13 +308,14 @@ class Installer(object):
         config.load_kube_config()
         with client.ApiClient() as k8s_client:
             try:
+                temp_file = "configmap-local-registry-hosting.yaml"
                 self.__transform_file("configmap-local-registry-hosting-template.yaml",
-                                      "configmap-local-registry-hosting.yaml",
+                                      temp_file,
                                       self.__transform_template)
                 utils.create_from_yaml(
-                    k8s_client, "configmap-local-registry-hosting.yaml")
-                if os.path.exists("configmap-local-registry-hosting.yaml"):
-                    os.remove("configmap-local-registry-hosting.yaml")
+                    k8s_client, temp_file)
+                if os.path.exists(temp_file):
+                    os.remove(temp_file)
             except Exception as ex:
                 log.info(ex)
 
@@ -304,4 +335,3 @@ class Installer(object):
             subprocess.run(["helm", "uninstall", self.helm_installation_name])
         except Exception as ex:
             log.error(f"Error: Failed to uninstall Helm chart. Make sure you have Helm installed. {str(ex)}")
-
