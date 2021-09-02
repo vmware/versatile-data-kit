297101415b49df6fc7938d834103fb882d954461
YanaZhivkova <87722481+YanaZhivkova@users.noreply.github.com>
2021-09-02 13:04:49 +0300

    versatile-data-kit: From DB to DB Ingestion Example (#175)
    Versatile Data Kit project needs examples
    demonstrating basic functionality for new users.
    
    This change includes an example demonstrating
    how to ingest data from a CSV file to a
    target database, and the required job to
    do it. The CSV file is downloaded from a public AWS
     S3 bucket and its data is ingested in a local
    SQLite db.
    
    Executed steps from the example locally.
    
    Signed-off-by: yzhivkova@vmware.com

---
+++ examples/ingest-from-csv-example/ingest-from-csv-example-job/10_drop_table.sql
@@ -0,0 +1,1 @@
+DROP TABLE IF EXISTS noaa_ghcn_data_1763;
---
+++ examples/ingest-from-csv-example/ingest-from-csv-example-job/20_create_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE noaa_ghcn_data_1763 (
+    StationID    INTEGER,
+    Date         NVARCHAR,
+    Element      NVARCHAR,
+    ElementValue NVARCHAR,
+    DataValue    NVARCHAR,
+    MFlag        NVARCHAR,
+    QFlag        NVARCHAR,
+    SFlag        NVARCHAR,
+    ObsTime      NVARCHAR
+);
