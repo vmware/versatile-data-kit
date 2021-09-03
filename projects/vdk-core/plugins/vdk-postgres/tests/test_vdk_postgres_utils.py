18a29c1378a448570a16b8479987be24ed4fa7a0
ikoleva <ikoleva@vmware.com>
2021-09-03 16:01:37 +0300

    vdk-core: PostgreSQL support
    Capability to connect to PostgreSQL, and execute queries.
    
    A vdk-postgres plugin introduced, that starts a PostgreSQL instance in
    docker, then runs a sample data job to persist data. Tests verify the
    data has been populated, using the newly introduced vdk-core CLI
    command "postgres-query". README.md added, describing on how to use the
    plugin, enlist postgres-related vdk-core configurations, and test
    locally.
    
    Testing Done: test_vdk_postgres_utils added
    
    Signed-off-by: ikoleva <ikoleva@vmware.com>

---
+++ projects/vdk-core/plugins/vdk-postgres/tests/test_vdk_postgres_utils.py
@@ -0,0 +1,48 @@
+# Copyright 2021 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+import os
+import unittest
+from unittest import mock
+
+import pytest
+from click.testing import Result
+from taurus.vdk import postgres_plugin
+from taurus.vdk.test_utils.util_funcs import cli_assert_equal
+from taurus.vdk.test_utils.util_funcs import CliEntryBasedTestRunner
+from taurus.vdk.test_utils.util_funcs import jobs_path_from_caller_directory
+
+VDK_DB_DEFAULT_TYPE = "VDK_DB_DEFAULT_TYPE"
+VDK_POSTGRES_DBNAME = "VDK_POSTGRES_DBNAME"
+VDK_POSTGRES_USER = "VDK_POSTGRES_USER"
+VDK_POSTGRES_PASSWORD = "VDK_POSTGRES_PASSWORD"
+VDK_POSTGRES_HOST = "VDK_POSTGRES_HOST"
+VDK_POSTGRES_PORT = "VDK_POSTGRES_PORT"
+
+
+@pytest.mark.usefixtures("postgres_service")
+@mock.patch.dict(
+    os.environ,
+    {
+        VDK_DB_DEFAULT_TYPE: "POSTGRES",
+        VDK_POSTGRES_DBNAME: "postgres",
+        VDK_POSTGRES_USER: "postgres",
+        VDK_POSTGRES_PASSWORD: "postgres",
+        VDK_POSTGRES_HOST: "localhost",
+        VDK_POSTGRES_PORT: "5432",
+    },
+)
+class PostgresUtilsTests(unittest.TestCase):
+    def setUp(self) -> None:
+        self.__runner = CliEntryBasedTestRunner(postgres_plugin)
+
+    def test_execute_query(self) -> None:
+        result: Result = self.__runner.invoke(
+            ["run", jobs_path_from_caller_directory("sql-job")]
+        )
+        cli_assert_equal(0, result)
+
+        actual_rs: Result = self.__runner.invoke(
+            ["postgres-query", "--query", "SELECT * FROM stocks"]
+        )
+        cli_assert_equal(0, actual_rs)
+        assert "GOOG" in actual_rs.output
