7616cec6f59903cb114787a280398dd4622a8fda
Tsvetomir Palashki <tpalashki@vmware.com>
2021-09-16 14:01:12 +0300

    vdk-control-cli: provide option to disable authentication
    Extend the set-default command to provide an option (--authentication-disable)
    to disable authentication. If either this new option is set or the
    VDK_AUTHENTICATION_DISABLE environment variable is set to True,
    the authentication will be disabled.
    
    Testing done: Manually set the new --authentication-disable option
    and verified that authentication is indeed disabled in subsequent
    vdk commands that use authentication.
    
    Signed-off-by: Tsvetomir Palashki <tpalashki@vmware.com>

--- projects/vdk-control-cli/src/vdk/internal/control/command_groups/common_group/default.py
+++ projects/vdk-control-cli/src/vdk/internal/control/command_groups/common_group/default.py
@@ -1,10 +1,18 @@
 # Copyright 2021 VMware, Inc.
 # SPDX-License-Identifier: Apache-2.0
+from typing import Optional
+
 import click
+from vdk.internal.control.configuration.defaults_config import (
+    reset_default_authentication_disable,
+)
 from vdk.internal.control.configuration.defaults_config import (
     reset_default_rest_api_url,
 )
 from vdk.internal.control.configuration.defaults_config import reset_default_team_name
+from vdk.internal.control.configuration.defaults_config import (
+    write_default_authentication_disable,
+)
 from vdk.internal.control.configuration.defaults_config import (
     write_default_rest_api_url,
 )
@@ -30,12 +38,23 @@ from vdk.internal.control.configuration.defaults_config import write_default_tea
     type=click.STRING,
     help="Set the default REST API url that will be used in all the commands that require it.",
 )
+@click.option(
+    "--authentication-disable",
+    type=click.STRING,
+    is_flag=True,
+    default=None,
+    help="Disables authentication.",
+)
 @click.pass_context
-def set_default_command(ctx, team: str, rest_api_url: str):
+def set_default_command(
+    ctx, team: str, rest_api_url: str, authentication_disable: Optional[bool]
+):
     if team is not None:
         write_default_team_name(team)
     if rest_api_url is not None:
         write_default_rest_api_url(rest_api_url)
+    if authentication_disable is not None:
+        write_default_authentication_disable(str(authentication_disable))
 
 
 @click.command(
@@ -59,9 +78,20 @@ def set_default_command(ctx, team: str, rest_api_url: str):
     default=False,
     help="Reset the default REST API url.",
 )
+@click.option(
+    "--authentication-disable",
+    is_flag=True,
+    flag_value=True,
+    default=False,
+    help="Reset the disable authentication flag.",
+)
 @click.pass_context
-def reset_default_command(ctx, team: bool, rest_api_url: bool):
+def reset_default_command(
+    ctx, team: bool, rest_api_url: bool, authentication_disable: bool
+):
     if team:
         reset_default_team_name()
     if rest_api_url:
         reset_default_rest_api_url()
+    if authentication_disable:
+        reset_default_authentication_disable()
