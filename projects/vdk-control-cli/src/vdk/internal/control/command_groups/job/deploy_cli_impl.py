ea8d1b4b1fda920727c994559f8e3047ffbe0d88
gageorgiev <gageorgiev@vmware.com>
2021-09-24 17:12:06 +0300

    vdk-control-cli: Add deployment verification tip
    This change includes two helpful messages - one shown when
    executing `vdk deploy`, the other when executing
    `vdk deploy --show` - explaining how to use the printed
    version hash to verify that your job was deployed successfully.
    
    Testing done: executed `vdk deploy` and `vdk deploy --show` and
    verified logs were shown
    
    Signed-off-by: gageorgiev <gageorgiev@vmware.com>

--- projects/vdk-control-cli/src/vdk/internal/control/command_groups/job/deploy_cli_impl.py
+++ projects/vdk-control-cli/src/vdk/internal/control/command_groups/job/deploy_cli_impl.py
@@ -153,8 +153,8 @@ class JobDeploy:
                 f"Request to deploy Data Job {name} using version {job_version} finished successfully.\n"
                 f"It would take a few minutes for the Data Job to be deployed in the server.\n"
                 f"If notified_on_job_deploy option in config.ini is configured then "
-                f"notification will be sent on successful deploy or in case of an error.\n"
-                f"You can also execute `vdk deploy --show` and compare the printed version "
+                f"notification will be sent on successful deploy or in case of an error.\n\n"
+                f"You can also execute `vdk deploy --show -t {team} -n {name}` and compare the printed version "
                 f"to the one of the newly deployed job - {job_version} - to verify that the deployment "
                 f"was successful."
             )
@@ -215,6 +215,10 @@ class JobDeploy:
                     enabled=d.enabled,
                 ),
                 deployments,
+            )
+            click.echo(
+                "You can compare the version seen here to the one seen when "
+                "deploying to verify your deployment was successful."
             )
             if output == OutputFormat.TEXT.value:
                 click.echo("")
