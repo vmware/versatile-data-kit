855163728a7015f0494eb14073edaa6524224f88
Gabriel Georgiev <45939426+gageorgiev@users.noreply.github.com>
2021-09-24 19:26:51 +0300

    vdk-logging-json: Format logs before initializing job (#286)
    Before, the change of the log format happens in a initialize_job
    hook. This is suboptimal, as some logs are printed before a job
    is initialized.
    To address this, this change includes another hook invocation,
    namely vdk_start, to format logs as early as possible. The early
    logs have their jobname and attemptid fields empty, as those are
    part of the JobContext, which is unreachable at that point of the
    execution.
    
    Testing done: verified logs in local execution, pipelines passed
    
    Signed-off-by: gageorgiev gageorgiev@vmware.com

--- projects/vdk-core/plugins/vdk-logging-json/src/vdk/internal/logging_json.py
+++ projects/vdk-core/plugins/vdk-logging-json/src/vdk/internal/logging_json.py
@@ -1,8 +1,10 @@
 # Copyright 2021 VMware, Inc.
 # SPDX-License-Identifier: Apache-2.0
 import logging
+from typing import List
 
 from vdk.api.plugin.hook_markers import hookimpl
+from vdk.api.plugin.plugin_registry import IPluginRegistry
 from vdk.internal.builtin_plugins.run.job_context import JobContext
 from vdk.internal.core.statestore import CommonStoreKeys
 
@@ -13,13 +15,30 @@ format_template = (
     '"lineno":"%(lineno)s","funcname":"%(funcName)s","attemptid":"{attempt_id}","message":"%(message)s"'
 )
 
+
 # this class serves to escape newline characters in the log message
 # as according to https://stackoverflow.com/questions/42068/how-do-i-handle-newlines-in-json
 # it is currently experimental and might be removed
 class RemoveNewlinesFormatter(logging.Formatter):
+    def __init__(self, fmt):
+        super().__init__(fmt=fmt)
+        self.default_time_format = "%Y-%m-%dT%H:%M:%S"
+        self.default_msec_format = "%s.%03dZ"
+
     def format(self, record):
         record.msg = record.msg.replace("\n", "\\n")
         return super().format(record)
+
+
+@hookimpl(tryfirst=True)
+def vdk_start(plugin_registry: IPluginRegistry, command_line_args: List):
+    format_copy = (
+        "{" + format_template.format(job_name="", attempt_id="") + "}"
+    )  # appending the braces
+    for handler in logging.getLogger().handlers:
+        formatter = RemoveNewlinesFormatter(fmt=format_copy)
+
+        handler.setFormatter(formatter)
 
 
 @hookimpl(trylast=True)
@@ -27,16 +46,11 @@ def initialize_job(context: JobContext) -> None:
     attempt_id = context.core_context.state.get(CommonStoreKeys.ATTEMPT_ID)
     job_name = context.name
 
-    detailed_format = format_template.format(
-        job_name=job_name, attempt_id=attempt_id
-    )  # appending braces is separated due to issue with
     detailed_format = (
-        "{" + detailed_format + "}"
-    )  # formatting a string containing curly braces
+        "{" + format_template.format(job_name=job_name, attempt_id=attempt_id) + "}"
+    )  # appending the braces
 
     for handler in logging.getLogger().handlers:
         formatter = RemoveNewlinesFormatter(fmt=detailed_format)
-        formatter.default_time_format = "%Y-%m-%dT%H:%M:%S"
-        formatter.default_msec_format = "%s.%03dZ"
 
         handler.setFormatter(formatter)
