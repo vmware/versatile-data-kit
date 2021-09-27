e2e7c122d767a68c1092ddd1bbaf11e0cdcdd22e
gageorgiev <gageorgiev@vmware.com>
2021-09-27 12:27:17 +0300

    vdk-test-utils: Change namespace to vdk.plugin
    Since many other plugins depend on vdk-test-utils, we need to
    release this plugin first before the other plugins' CICD can pass.
    
    Testing done: pipelines passed
    
    Signed-off-by: gageorgiev <gageorgiev@vmware.com>

--- projects/vdk-core/plugins/vdk-test-utils/tests/test_vdk_test_utils.py
+++ projects/vdk-core/plugins/vdk-test-utils/tests/test_vdk_test_utils.py
@@ -1,7 +1,7 @@
 # Copyright 2021 VMware, Inc.
 # SPDX-License-Identifier: Apache-2.0
-from vdk.internal.test_utils import util_funcs
-from vdk.internal.test_utils import util_plugins
+from vdk.plugin.test_utils import util_funcs
+from vdk.plugin.test_utils import util_plugins
 
 
 def test_vdk_test_utils():
