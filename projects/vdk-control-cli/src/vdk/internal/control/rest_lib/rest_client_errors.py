3d4bbdaf2bf50e02bddc7090d99117e8f204e458
Antoni Ivanov <aivanov@vmware.com>
2021-10-03 18:37:29 +0300

    vdk-control-cli: clarify rest client errors
    We are making consequences configurable so that they are more precise on
    per case basis. And also explain a bit more about countermeasures in
    authorization error.
    
    Testing Done: existing unit tests
    
    Signed-off-by: Antoni Ivanov <aivanov@vmware.com>

--- projects/vdk-control-cli/src/vdk/internal/control/rest_lib/rest_client_errors.py
+++ projects/vdk-control-cli/src/vdk/internal/control/rest_lib/rest_client_errors.py
@@ -12,31 +12,38 @@ log = logging.getLogger(__name__)
 
 
 class ApiClientErrorDecorator:
-    def __init__(self, what="Control Service Error"):
-        self.what = what
+    def __init__(
+        self, what="Control Service Error", consequences="Operation cannot complete."
+    ):
+        self.__what = what
+        self.__consequences = consequences
 
     def _get_error(self, exception: ApiException):
-        # noinspection PyBroadException
         try:
             return json.loads(exception.body)
         except:
             error = {}
             if exception.status == 401:
                 error = dict(
-                    what=self.what,
-                    why="The request has not been applied because it lacks valid authentication credentials.",
-                    consequences="Operation cannot complete.",
-                    countermeasures="Try to login again using VDK CLI login command. "
-                    "Make sure you have permission to execute the given operation.",
+                    what=self.__what,
+                    why=f"The request has not been applied because it lacks valid authentication credentials. "
+                    f"Error returned by control service is {exception.body}",
+                    consequences=self.__consequences,
+                    countermeasures="Try to login again using VDK CLI login command."
+                    "Or set correct api token configuration (see vdk config-help)."
+                    "Make sure you have permission to execute the given operation."
+                    "Investigate (google) the error returned by control service."
+                    "And if all fails try to contact the support team.",
                 )
             if exception.status == 404:
                 error = dict(
-                    what=self.what,
+                    what=self.__what,
                     why="The requested resource cannot be found. "
                     "You may have a spelling mistake or the data job has not been created",
-                    consequences="Operation cannot complete.",
-                    countermeasures="Make sure that the data job name and team name  are spelled correctly (it is is case-sensitive) . "
-                    "Make sure you have run vdk create before doing any other operations on a data job.",
+                    consequences=self.__consequences,
+                    countermeasures="Make sure that the data job name and team name are spelled correctly "
+                    "(it is case-sensitive) . "
+                    "Make sure you have run vdk create --cloud before doing this operation on a data job.",
                 )
 
             return error
@@ -55,11 +62,9 @@ class ApiClientErrorDecorator:
 
                 body = self._get_error(ex)
                 vdk_ex = VDKException(
-                    what=body.get("what", self.what),
+                    what=body.get("what", self.__what),
                     why=body.get("why", f"Http error: status: {ex.status} - {ex.body}"),
-                    consequence=body.get(
-                        "consequences", "Operation cannot complete successfully."
-                    ),
+                    consequence=body.get("consequences", self.__consequences),
                     countermeasure=body.get(
                         "countermeasures",
                         "See error and try to resolve it or open ticket to SRE team.",
@@ -73,9 +78,9 @@ class ApiClientErrorDecorator:
                 )
 
                 vdk_ex = VDKException(
-                    what=self.what,
+                    what=self.__what,
                     why=str(ex),
-                    consequence="Operation cannot complete successfully.",
+                    consequence=self.__consequences,
                     countermeasure="Verify that the provided url (--rest-api-url) is valid "
                     "and points to the correct host.",
                 )
