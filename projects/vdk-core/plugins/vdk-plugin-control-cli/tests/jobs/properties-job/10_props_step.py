7ab1d968ca8bbcbd870e1371808ea142b6e57594
Antoni Ivanov <aivanov@vmware.com>
2021-10-03 23:04:21 +0300

    vdk-control-cli: set rest api url for properties api
    I accidentally merged older version of the code which did not have REST
    API URL set for Properties API plugin.
    
    This was not caught by test though so I added a functional test which
    executes testing job (it's often used pattern now in the project)
    
    Testing Done: the newly added test
    
    Signed-off-by: Antoni Ivanov <aivanov@vmware.com>

---
+++ projects/vdk-core/plugins/vdk-plugin-control-cli/tests/jobs/properties-job/10_props_step.py
@@ -0,0 +1,10 @@
+# Copyright 2021 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+from vdk.api.job_input import IJobInput
+
+
+def run(job_input: IJobInput):
+
+    props = job_input.get_all_properties()
+    props["new"] = props["original"] + 1
+    job_input.set_all_properties(props)
