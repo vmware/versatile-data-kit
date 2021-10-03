073dc3fd7834d937c892d209366d5b19e14b98b7
Antoni Ivanov <aivanov@vmware.com>
2021-10-03 23:04:21 +0300

    vdk-control-cli:  properties plugin reusing control service api client
    We had custom implementation with custom configuration of
    PropertiesServiceClient plugin which caused unnecessary issues.
    For example, in order to setup properties user had to configure 3 extra
    configuration options which duplicated when with those of
    vdk-control-cli.
    Also, user executing vdk login successfully still had to set them even
    though the login info is the same.
    
    Now vdk login authentication is used if vdk login has been successful.
    Thus in interactive use of vdk (e.g local development) user would have
    no issues.
    In non-interactive use (e.g cloud execution) admins or users can set
    VDK_API_TOKEN to authenticate as necessary.
    
    Testing Done: existing tests passed to verify no regressions
    
    Signed-off-by: Antoni Ivanov <aivanov@vmware.com>

---
+++ projects/vdk-core/plugins/vdk-plugin-control-cli/src/vdk/plugin/control_cli_plugin/control_service_properties_client.py
@@ -0,0 +1,42 @@
+# Copyright 2021 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+import json
+import logging
+from abc import ABC
+from abc import abstractmethod
+from typing import Dict
+from typing import Optional
+
+import requests
+from requests.adapters import HTTPAdapter
+from requests.adapters import Retry
+from requests.auth import AuthBase
+from vdk.api.plugin.plugin_input import IPropertiesServiceClient
+from vdk.internal.control.rest_lib.factory import ApiClientFactory
+from vdk.internal.core import errors
+
+log = logging.getLogger(__name__)
+
+
+class ControlServicePropertiesServiceClient(IPropertiesServiceClient):
+    """
+    Implementation of PropertiesServiceClient which connects to Pipelines Control Service Properties API
+    """
+
+    def __init__(self, rest_api_url: str):
+        self.__properties_api = ApiClientFactory(rest_api_url).get_properties_api()
+        log.debug(f"Initialized Properties against {rest_api_url}.")
+
+    def read_properties(self, job_name: str, team_name: str):
+        data = self.__properties_api.data_job_properties_read(
+            team_name=team_name, job_name=job_name, deployment_id="todo"
+        )
+        return data
+
+    def write_properties(self, job_name: str, team_name: str, properties: Dict):
+        self.__properties_api.data_job_properties_update(
+            team_name=team_name,
+            job_name=job_name,
+            deployment_id="todo",
+            request_body=properties,
+        )
