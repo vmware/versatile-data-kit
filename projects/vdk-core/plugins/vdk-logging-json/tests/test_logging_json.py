340dba1ab48593710abd8987606e2635a4133510
gageorgiev <gageorgiev@vmware.com>
2021-10-05 14:09:30 +0300

    vdk-logging-json: Escape double quotes in message
    Double quotes in the log message would mess up the parsing, as
    JSON separates fields and values based on double quote pairs.
    This change escapes the double quotes in the message string.
    
    Testing done: verified that the formatted log with double quotes
    in the message is valid JSON
    
    Signed-off-by: gageorgiev <gageorgiev@vmware.com>

--- projects/vdk-core/plugins/vdk-logging-json/tests/test_logging_json.py
+++ projects/vdk-core/plugins/vdk-logging-json/tests/test_logging_json.py
@@ -2,9 +2,32 @@
 # SPDX-License-Identifier: Apache-2.0
 import logging
 
+from vdk.plugin.logging_json.logging_json import EscapeNewlinesAndQuotesFormatter
 from vdk.plugin.logging_json.logging_json import format_template
 
 
 def test_json_logging():
     for handler in logging.getLogger(__name__).handlers:
         assert handler.formatter._fmt == format_template
+
+
+def test_formatter():
+    formatter = EscapeNewlinesAndQuotesFormatter(fmt="")
+
+    log_record = logging.LogRecord(
+        name="",
+        level=1,
+        pathname="",
+        lineno=1,
+        msg="test-string",
+        args=(),
+        exc_info=None,
+    )
+
+    assert formatter.format(log_record) == "test-string"
+
+    log_record.msg = "test\nstring\nwith\nnewlines"
+    assert formatter.format(log_record) == "test\\nstring\\nwith\\nnewlines"
+
+    log_record.msg = 'test"string"with"double"quotes'
+    assert formatter.format(log_record) == 'test\\"string\\"with\\"double\\"quotes'
