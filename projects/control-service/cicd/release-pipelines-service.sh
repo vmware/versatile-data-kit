05b25bcdd8bc39f13c89e329d6c444353e82067f
Tsvetomir Palashki <tpalashki@vmware.com>
2021-10-07 18:12:11 +0300

    control-service: change CI/CD release new version on each PR merge
    Up until now Control Service releases had to be done manually
    by making changes to several files and then making a new PR
    and merge it.
    
    With this commit a new version of the Control Service is
    released automatically every time a new image of the Control Service
    is built, which happens every time a PR with changes to the code
    base of the Control Service is merged. The MAJOR and MINOR components
    of the version are taken from the version.txt file. The PATCH
    component is automatically generated.
    
    The CONTRIBUTING.md file was updated to reflect the new release
    process. The CHANGELOG.md file was cleaned up.
    
    Testing done: none
    
    Signed-off-by: Tsvetomir Palashki <tpalashki@vmware.com>

--- projects/control-service/cicd/release-pipelines-service.sh
+++ projects/control-service/cicd/release-pipelines-service.sh
@@ -7,13 +7,13 @@ export CHART_NAME=pipelines-control-service
 export CHART_VERSION=$(cat $CHART_NAME/version.txt)
 export HELM_REPO=versatile-data-kit-helm-registry
 # TODO: sign chart
-helm package --version $CHART_VERSION --app-version $CHART_VERSION --dependency-update $CHART_NAME
+helm pack --version "$CHART_VERSION" --app-version "$CHART_VERSION" --dependency-update "$CHART_NAME" --set image.tag="$TAG"
 echo "Upload $CHART_NAME version $CHART_VERSION (if version already exists upload will fail)"
-helm repo add $HELM_REPO $VDK_HELM_REGISTRY_URL \
-      --username $VDK_HELM_REGISTRY_USERNAME \
-      --password $VDK_HELM_REGISTRY_PASSWORD
+helm repo add $HELM_REPO "$VDK_HELM_REGISTRY_URL" \
+      --username "$VDK_HELM_REGISTRY_USERNAME" \
+      --password "$VDK_HELM_REGISTRY_PASSWORD"
 # in v 0.10.0 the name of the push command was changed to cm-push
-helm cm-push $CHART_NAME-$CHART_VERSION.tgz $HELM_REPO
+helm cm-push "$CHART_NAME-$CHART_VERSION.tgz" $HELM_REPO
 echo "Check helm repo and new chart is accessible"
 helm repo update
-helm search repo $CHART_NAME --version $CHART_VERSION
+helm search repo $CHART_NAME --version "$CHART_VERSION"
