93317b4b41ba88f4534d882da98dd2a40c28fe28
Tsvetomir Palashki <tpalashki@vmware.com>
2021-10-08 12:29:51 +0300

    control-service: additional fixes
    Signed-off-by: Tsvetomir Palashki <tpalashki@vmware.com>

--- projects/control-service/cicd/release-pipelines-service.sh
+++ projects/control-service/cicd/release-pipelines-service.sh
@@ -3,11 +3,9 @@
 # Copyright 2021 VMware, Inc.
 # SPDX-License-Identifier: Apache-2.0
 
-export CHART_NAME=pipelines-control-service
-export CHART_VERSION=$(cat $CHART_NAME/version.txt)
 export HELM_REPO=versatile-data-kit-helm-registry
 # TODO: sign chart
-helm pack --version "$CHART_VERSION" --app-version "$CHART_VERSION" --dependency-update "$CHART_NAME" --set image.tag="$TAG"
+helm pack --version "$CHART_VERSION" --app-version "$CHART_VERSION" --dependency-update "$CHART_NAME" --set image.tag="$IMAGE_TAG"
 echo "Upload $CHART_NAME version $CHART_VERSION (if version already exists upload will fail)"
 helm repo add $HELM_REPO "$VDK_HELM_REGISTRY_URL" \
       --username "$VDK_HELM_REGISTRY_USERNAME" \
@@ -16,4 +14,4 @@ helm repo add $HELM_REPO "$VDK_HELM_REGISTRY_URL" \
 helm cm-push "$CHART_NAME-$CHART_VERSION.tgz" $HELM_REPO
 echo "Check helm repo and new chart is accessible"
 helm repo update
-helm search repo $CHART_NAME --version "$CHART_VERSION"
+helm search repo "$CHART_NAME" --version "$CHART_VERSION"
