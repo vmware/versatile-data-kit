41e5511502bdf1e0d4b7892b418e25d505d2054e
gageorgiev <gageorgiev@vmware.com>
2021-10-11 15:56:51 +0300

    vdk-logging-json: Allow for logging of the stack trace
    Until now, formatting a stack trace log was not possible, as it
    uses a separate function formatException() instead of format().
    This change fixes that by overriding the aformentioned function,
    capturing the stack trace, replacing the newlines and double quotes,
    formatting it in JSON and logging it.
    
    Testing done: ran job locally and verified the stack trace is JSON
    formatted
    
    Signed-off-by: gageorgiev <gageorgiev@vmware.com>

--- projects/vdk-core/plugins/vdk-logging-json/setup.py
+++ projects/vdk-core/plugins/vdk-logging-json/setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     description="Versatile Data Kit SDK plugin that changes logging output to JSON format.",
     long_description=pathlib.Path("README.md").read_text(),
     long_description_content_type="text/markdown",
-    install_requires=["vdk-core"],
+    install_requires=["vdk-core", "ecs-logging"],
     package_dir={"": "src"},
     packages=setuptools.find_namespace_packages(where="src"),
     entry_points={
--- projects/vdk-core/plugins/vdk-logging-json/src/vdk/plugin/logging_json/logging_json.py
+++ projects/vdk-core/plugins/vdk-logging-json/src/vdk/plugin/logging_json/logging_json.py
@@ -1,44 +1,60 @@
 # Copyright 2021 VMware, Inc.
 # SPDX-License-Identifier: Apache-2.0
+import json
 import logging
 from typing import List
 
+from ecs_logging import StdlibFormatter
 from vdk.api.plugin.hook_markers import hookimpl
 from vdk.api.plugin.plugin_registry import IPluginRegistry
 from vdk.internal.builtin_plugins.run.job_context import JobContext
 from vdk.internal.core.statestore import CommonStoreKeys
 
-# the labels follow the labelling recommendations found here: http://ltsv.org/
-format_template = (
-    '"@timestamp":"%(asctime)s","created":"%(created)f","jobname":"{job_name}",'
-    '"level":"%(levelname)s","modulename":"%(name)s","filename":"%(filename)s",'
-    '"lineno":"%(lineno)s","funcname":"%(funcName)s","attemptid":"{attempt_id}","message":"%(message)s"'
-)
 
-
-# this class serves to escape newline characters in the log message
-# as according to https://stackoverflow.com/questions/42068/how-do-i-handle-newlines-in-json
-# it is currently experimental and might be removed
-class EscapeNewlinesAndQuotesFormatter(logging.Formatter):
-    def __init__(self, fmt):
-        super().__init__(fmt=fmt)
-        self.default_time_format = "%Y-%m-%dT%H:%M:%S"
-        self.default_msec_format = "%s.%03dZ"
+class EcsJsonFormatter(StdlibFormatter):
+    def __init__(self, job_name, attempt_id, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+        self.job_name = job_name
+        self.attempt_id = attempt_id
 
     def format(self, record):
-        record.msg = record.msg.replace("\n", "\\n").replace('"', '\\"')
-        return super().format(record)
+        result = self.format_to_ecs(record)
+
+        # fix the names of some fields and then remove their duplicates
+        result["level"] = result["log"]["level"].upper()
+        result["lineno"] = result["log"]["origin"]["file"]["line"]
+        result["filename"] = result["log"]["origin"]["file"]["name"]
+        result["modulename"] = result["log"]["logger"]
+        result["funcname"] = result["log"]["origin"]["function"]
+        if "error" in result:
+            result["error.message"] = result["error"]["message"]
+            result["error.stack_trace"] = result["error"]["stack_trace"]
+            result["error.type"] = result["error"]["type"]
+
+            result.pop("error")
+
+        result.pop("log")
+        result.pop("process")
+
+        # bind extra fields
+        result["jobname"] = self.job_name
+        result["attemptid"] = self.attempt_id
+
+        return json.dumps(result)
 
 
 @hookimpl(tryfirst=True)
 def vdk_start(plugin_registry: IPluginRegistry, command_line_args: List):
-    format_copy = (
-        "{" + format_template.format(job_name="", attempt_id="") + "}"
-    )  # appending the braces
     for handler in logging.getLogger().handlers:
-        formatter = EscapeNewlinesAndQuotesFormatter(fmt=format_copy)
-
-        handler.setFormatter(formatter)
+        handler.setFormatter(
+            EcsJsonFormatter(
+                job_name="",
+                attempt_id="",
+                exclude_fields=[
+                    "ecs",
+                ],
+            )
+        )
 
 
 @hookimpl(trylast=True)
@@ -46,11 +62,13 @@ def initialize_job(context: JobContext) -> None:
     attempt_id = context.core_context.state.get(CommonStoreKeys.ATTEMPT_ID)
     job_name = context.name
 
-    detailed_format = (
-        "{" + format_template.format(job_name=job_name, attempt_id=attempt_id) + "}"
-    )  # appending the braces
-
     for handler in logging.getLogger().handlers:
-        formatter = EscapeNewlinesAndQuotesFormatter(fmt=detailed_format)
-
-        handler.setFormatter(formatter)
+        handler.setFormatter(
+            EcsJsonFormatter(
+                job_name=job_name,
+                attempt_id=attempt_id,
+                exclude_fields=[
+                    "ecs",
+                ],
+            )
+        )
