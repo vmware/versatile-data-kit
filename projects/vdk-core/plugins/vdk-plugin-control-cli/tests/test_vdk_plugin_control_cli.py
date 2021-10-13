85e87f06f9fe79d4d9e1357fde319841906b6576
Antoni Ivanov <aivanov@vmware.com>
2021-10-13 11:54:07 +0300

    vdk-plugin-control-cli: fix unit tests
    We were using hard-coded config names in the test from vdk-control-cli
    but when we change them , the test broke. So now we use variable names
    
    Testing Done: locally ran unit test of the plugin
    
    Signed-off-by: Antoni Ivanov <aivanov@vmware.com>

--- projects/vdk-core/plugins/vdk-plugin-control-cli/tests/test_vdk_plugin_control_cli.py
+++ projects/vdk-core/plugins/vdk-plugin-control-cli/tests/test_vdk_plugin_control_cli.py
@@ -1,6 +1,12 @@
 # Copyright 2021 VMware, Inc.
 # SPDX-License-Identifier: Apache-2.0
 from vdk.plugin.control_cli_plugin import vdk_plugin_control_cli
+from vdk.plugin.control_cli_plugin.control_service_configuration import (
+    CONTROL_SAMPLE_JOB_DIRECTORY,
+)
+from vdk.plugin.control_cli_plugin.control_service_configuration import (
+    CONTROL_SERVICE_REST_API_URL,
+)
 from vdk.plugin.test_utils.util_funcs import cli_assert_equal
 from vdk.plugin.test_utils.util_funcs import CliEntryBasedTestRunner
 
@@ -18,8 +24,8 @@ def test_vdk_plugin_control_cli_configuration_defined():
 
     cli_assert_equal(0, result)
     assert (
-        "CONTROL_SAMPLE_JOB_DIRECTORY" in result.output
-        and "CONTROL_SERVICE_REST_API_URL" in result.output
+        CONTROL_SAMPLE_JOB_DIRECTORY.lower() in str(result.output).lower()
+        and CONTROL_SERVICE_REST_API_URL.lower() in str(result.output).lower()
     ), (
         "Expected to find definitions of CONTROL_SERVICE_REST_API_URL & CONTROL_SAMPLE_JOB_DIRECTORY in result output "
         f"but did not. result output is: {result.output}"
