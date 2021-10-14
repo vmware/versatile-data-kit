ac94d58055b95d2b6a2bd51c60f7e8a91559be04
mrMoZ1 <mzhivkov@vmware.com>
2021-10-14 15:05:41 +0300

    comments
    Signed-off-by: mrMoZ1 <mzhivkov@vmware.com>

--- projects/vdk-core/tests/vdk/internal/builtin_plugins/test_builtin_hook_impl.py
+++ projects/vdk-core/tests/vdk/internal/builtin_plugins/test_builtin_hook_impl.py
@@ -1,6 +1,5 @@
 # Copyright 2021 VMware, Inc.
 # SPDX-License-Identifier: Apache-2.0
-import unittest
 from unittest.mock import MagicMock
 
 from vdk.api.plugin.plugin_registry import IPluginRegistry
@@ -11,9 +10,9 @@ from vdk.internal.core.statestore import CommonStoreKeys
 from vdk.internal.core.statestore import StateStore
 
 
-class TestLocalIdSetting(unittest.TestCase):
+class TestLocalIdSet:
     @classmethod
-    def setUpClass(cls):
+    def setup_method(cls):
         cls.configuration = ConfigurationBuilder().build()
         cls.plugin_registry = MagicMock(spec=IPluginRegistry)
         cls.state = StateStore()
@@ -46,9 +45,9 @@ class TestLocalIdSetting(unittest.TestCase):
         assert len(self.context.state.get(CommonStoreKeys.ATTEMPT_ID)) == 53
 
 
-class TestExternalId(unittest.TestCase):
+class TestExternalIdSet:
     @classmethod
-    def setUpClass(cls):
+    def setup_method(cls):
 
         cls.configuration = (
             ConfigurationBuilder()
@@ -78,4 +77,49 @@ class TestExternalId(unittest.TestCase):
         assert (
             self.context.state.get(CommonStoreKeys.ATTEMPT_ID)
             == "mzhivkov-test-job-1234567890-akmvy"
+        )
+
+
+class TestExecutionIdSet:
+    @classmethod
+    def setup_method(cls):
+
+        cls.configuration = (
+            ConfigurationBuilder()
+            .add("EXECUTION_ID", "mzhivkov-test-job-1234567890")
+            .build()
+        )
+        cls.plugin_registry = MagicMock(spec=IPluginRegistry)
+        cls.state = StateStore()
+
+        cls.context = CoreContext(cls.plugin_registry, cls.configuration, cls.state)
+        cls.initializer = builtin_hook_impl.RuntimeStateInitializePlugin()
+        cls.initializer.vdk_initialize(cls.context)
+
+    def test_execution_id_set(self):
+        assert (
+            self.context.state.get(CommonStoreKeys.EXECUTION_ID)
+            == "mzhivkov-test-job-1234567890"
+        )
+
+    def test_attempt_id(self):
+        assert self.context.state.get(CommonStoreKeys.ATTEMPT_ID)
+
+    def test_op_id(self):
+        assert self.context.state.get(CommonStoreKeys.OP_ID) == self.context.state.get(
+            CommonStoreKeys.EXECUTION_ID
+        )
+
+    def test_attempt_id_startswith_execution_id(self):
+        # In case Attempt id is not set we append "-" plus 5 random chars to the end of execution id and assign the result to attempt id
+        assert self.context.state.get(CommonStoreKeys.ATTEMPT_ID).startswith(
+            self.context.state.get(CommonStoreKeys.EXECUTION_ID)
+        )
+
+    def test_attempt_id_longer_than_execution_id(self):
+        # In case Attempt id is not set we append "-" plus 5 random chars to the end of execution id and assign the result to attempt id
+        assert (
+            len(self.context.state.get(CommonStoreKeys.ATTEMPT_ID))
+            - len(self.context.state.get(CommonStoreKeys.EXECUTION_ID))
+            == 6
         )
