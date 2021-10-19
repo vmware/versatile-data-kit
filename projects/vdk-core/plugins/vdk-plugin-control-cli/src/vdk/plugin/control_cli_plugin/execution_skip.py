vmware/versatile-data-kit

commit 582ccaecd2162a2873d6a5135fd5b060ac3cd68a

vdk-core: extend pod termination message (#347)
* vdk-core: extend pod termination massage

In order to be able to use the K8S Pod termination message
to transfer various data (such as vdk version) we need to switch
from plain text to json format of termination message.

Testing: unit tests

Signed-off-by: Miroslav Ivanov miroslavi@vmware.com

* [pre-commit.ci] auto fixes from pre-commit.com hooks

for more information, see https://pre-commit.ci

Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

projects/vdk-core/plugins/vdk-plugin-control-cli/src/vdk/plugin/control_cli_plugin/execution_skip.py
@@ -9,12 +9,12 @@ from vdk.api.plugin.hook_markers import hookimpl
 from vdk.internal.builtin_plugins.config import vdk_config
 from vdk.internal.builtin_plugins.config.job_config import JobConfigKeys
 from vdk.internal.builtin_plugins.run.job_context import JobContext
-from vdk.internal.builtin_plugins.termination_message.action import WriteToFileAction
-from vdk.internal.builtin_plugins.termination_message.writer_configuration import (
-    WriterConfiguration,
+from vdk.internal.builtin_plugins.termination_message.writer import (
+    TerminationMessageWriterPlugin,
 )
 from vdk.internal.control.configuration.vdk_config import VDKConfig
 from vdk.internal.control.rest_lib.factory import ApiClientFactory
+from vdk.internal.core.config import Configuration
 from vdk.internal.core.config import ConfigurationBuilder
 from vdk.internal.core.statestore import CommonStoreKeys
 
@@ -99,7 +99,7 @@ def _skip_job_if_necessary(
     job_name: str,
     execution_id: str,
     job_team: str,
-    action: WriteToFileAction,
+    configuration: Configuration,
 ):
     try:
         log.info("Checking if job should be skipped:")
@@ -119,7 +119,10 @@ def _skip_job_if_necessary(
 
         if job_running:
             log.info(f"Skipping job {job_name}")
-            action.skipped()
+            writer_plugin = TerminationMessageWriterPlugin()
+            writer_plugin.write_termination_message(
+                configuration=configuration, execution_skipped=True
+            )
             _skip_job_run(job_name)  # calls os._exit(0)
             return 1  # All other branches return None
     except Exception as exc:
@@ -153,13 +156,10 @@ def run_job(context: JobContext) -> None:
         log_config_type = configuration.get_value(vdk_config.LOG_CONFIG)
         job_name = context.name
         job_team = configuration.get_value(JobConfigKeys.TEAM)
-
-        execution_id = context.core_context.state.get(CommonStoreKeys.EXECUTION_ID)
-        writer_config = WriterConfiguration(configuration)
-        action = WriteToFileAction(writer_config.get_output_file())
+        execution_id = configuration.get_value(vdk_config.EXECUTION_ID)
 
         return _skip_job_if_necessary(
-            log_config_type, job_name, execution_id, job_team, action
+            log_config_type, job_name, execution_id, job_team, configuration
         )
     except Exception as exc:
         log.warning(f"Error while setting up arguments: {str(exc)}")

projects/vdk-core/plugins/vdk-plugin-control-cli/tests/test_execution_skip.py
@@ -5,8 +5,8 @@ from unittest.mock import Mock
 from unittest.mock import patch
 
 from taurus_datajob_api import DataJobExecution
-from vdk.internal.builtin_plugins.termination_message.action import WriteToFileAction
 from vdk.internal.control.rest_lib.factory import DataJobsExecutionApi
+from vdk.internal.core.config import ConfigurationBuilder
 from vdk.plugin.control_cli_plugin.execution_skip import _skip_job_if_necessary
 from vdk.plugin.control_cli_plugin.execution_skip import ConcurrentExecutionChecker
 
@@ -182,7 +182,21 @@ class ExecutionSkipTest(unittest.TestCase):
 
         api_factory.return_value = None
         get_api.return_value = None
-        action = WriteToFileAction("test_skip_file.txt")
+
+        configuration = (
+            ConfigurationBuilder()
+            .add(
+                key="TERMINATION_MESSAGE_WRITER_OUTPUT_FILE",
+                default_value="filename.txt",
+            )
+            .add(
+                key="TERMINATION_MESSAGE_WRITER_ENABLED",
+                default_value=True,
+            )
+            .build()
+        )
 
         # Check if method executes. Test suceeds if no exception thrown
-        _skip_job_if_necessary("CLOUD", "test-job", "test-id", "test-team", action)
+        _skip_job_if_necessary(
+            "CLOUD", "test-job", "test-id", "test-team", configuration
+        )

projects/vdk-core/src/vdk/internal/builtin_plugins/termination_message/action.py
@@ -1,73 +1,0 @@
-# Copyright 2021 VMware, Inc.
-# SPDX-License-Identifier: Apache-2.0
-import logging
-from abc import ABC
-from abc import abstractmethod
-
-log = logging.getLogger(__name__)
-
-
-class ITerminationAction(ABC):
-    """
-    A base class representing an action that is performed upon data job completion, depending on the result.
-    """
-
-    @abstractmethod
-    def success(self):
-        """
-        Called when the job execution was successful.
-        """
-        pass
-
-    @abstractmethod
-    def user_error(self):
-        """
-        Called when the job execution failed with an user error.
-        """
-        pass
-
-    @abstractmethod
-    def platform_error(self):
-        """
-        Called when the job execution failed with a platform error.
-        """
-        pass
-
-
-class WriteToFileAction(ITerminationAction):
-    """
-    Writes a string message to a specified file upon completion of a data job execution.
-
-    This is intended to provide a termination status of a data job in a Kubernetes environment
-    (see https://kubernetes.io/docs/tasks/debug-application-cluster/determine-reason-pod-failure/).
-    Keep in mind that there is a limit to the length of the termination message.
-    """
-
-    def __init__(self, filename, show_log_messages=True):
-        self.filename = filename
-        self.show_log_messages = show_log_messages
-
-    def _append_to_file(self, message):
-        if not self.filename:
-            return
-
-        try:
-            with open(self.filename, "a") as file:
-                file.write(message)
-        except OSError as e:
-            if self.show_log_messages:
-                log.debug(
-                    f'Unable to write termination message to file "{self.filename}". {e}'
-                )
-
-    def success(self):
-        self._append_to_file("Success\n")
-
-    def user_error(self):
-        self._append_to_file("User error\n")
-
-    def platform_error(self):
-        self._append_to_file("Platform error\n")
-
-    def skipped(self):
-        self._append_to_file("Skipped\n")

projects/vdk-core/src/vdk/internal/builtin_plugins/termination_message/file_util.py
@@ -1,0 +1,32 @@
+# Copyright 2021 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class WriteToFileAction:
+    """
+    Writes a string message to a specified file upon completion of a data job execution.
+
+    This is intended to provide a termination status of a data job in a Kubernetes environment
+    (see https://kubernetes.io/docs/tasks/debug-application-cluster/determine-reason-pod-failure/).
+    Keep in mind that there is a limit to the length of the termination message.
+    """
+
+    def __init__(self, filename, show_log_messages=True):
+        self.filename = filename
+        self.show_log_messages = show_log_messages
+
+    def append_to_file(self, message):
+        if not self.filename:
+            return
+
+        try:
+            with open(self.filename, "a") as file:
+                file.write(message)
+        except OSError as e:
+            if self.show_log_messages:
+                log.debug(
+                    f'Unable to write termination message to file "{self.filename}". {e}'
+                )

projects/vdk-core/src/vdk/internal/builtin_plugins/termination_message/writer.py
@@ -1,16 +1,20 @@
 # Copyright 2021 VMware, Inc.
 # SPDX-License-Identifier: Apache-2.0
+import json
 import logging
 
 from vdk.api.plugin.hook_markers import hookimpl
 from vdk.internal.builtin_plugins.config.vdk_config import LOG_CONFIG
-from vdk.internal.builtin_plugins.termination_message import action
 from vdk.internal.builtin_plugins.termination_message import (
     writer_configuration,
+)
+from vdk.internal.builtin_plugins.termination_message.file_util import (
+    WriteToFileAction,
 )
 from vdk.internal.builtin_plugins.termination_message.writer_configuration import (
     WriterConfiguration,
 )
+from vdk.internal.builtin_plugins.version.version import get_version
 from vdk.internal.core import errors
 from vdk.internal.core.config import ConfigurationBuilder
 from vdk.internal.core.context import CoreContext
@@ -25,13 +29,15 @@ class TerminationMessageWriterPlugin:
 
     @hookimpl
     def vdk_exit(self, context: CoreContext, exit_code: int):
-        self._write_termination_message(
+        self.write_termination_message(
             errors.get_blamee_overall(),  # TODO: get this from context
             errors.get_blamee_overall_user_error(),  # TODO: get this from context
             context.configuration,
         )
 
-    def _write_termination_message(self, error_overall, user_error, configuration):
+    def write_termination_message(
+        self, error_overall, user_error, configuration, execution_skipped=False
+    ):
         termination_message_writer_cfg = WriterConfiguration(configuration)
 
         try:
@@ -47,7 +53,7 @@ class TerminationMessageWriterPlugin:
                 show_termination_log_messages = (
                     configuration.get_value(LOG_CONFIG).lower() != "local"
                 )
-                termination_action = action.WriteToFileAction(
+                file_util = WriteToFileAction(
                     termination_message_output_file, show_termination_log_messages
                 )
 
@@ -56,20 +62,26 @@ class TerminationMessageWriterPlugin:
                         f'Writing termination message to file "{termination_message_output_file}"'
                     )
                 self._execute_termination_action(
-                    termination_action, error_overall, user_error
+                    file_util, error_overall, user_error, execution_skipped
                 )
-        except:
-            log.exception("Failed to write termination message.")
+        except Exception as e:
+            log.exception("Failed to write termination message. See exception: ", e)
 
     @staticmethod
     def _execute_termination_action(
-        action, error_overall, user_error, execution_skipped=False
+        file_util, error_overall, user_error, execution_skipped
     ):
+
+        termination_message = {"vdk_version": get_version()}
+
         if execution_skipped:
-            action.skipped()
+            status = "Skipped"
         elif not error_overall:
-            action.success()
+            status = "Success"
         elif user_error:
-            action.user_error()
+            status = "User error"
         else:
-            action.platform_error()
+            status = "Platform error"
+
+        termination_message["status"] = status
+        file_util.append_to_file(json.dumps(termination_message))

projects/vdk-core/src/vdk/internal/builtin_plugins/version/version.py
@@ -16,8 +16,9 @@ except ImportError as e:
 
 log = logging.getLogger(__name__)
 
+
 # Returns the version of the package named dist_name
-def _version(
+def get_version(
     dist_name="vdk-core",
 ):  # Change default value if project is renamed and does not equal the setuptools metadata.name
     try:
@@ -61,7 +62,7 @@ def list_installed_plugins_versions():
     dist_plugin_pairs = list_installed_plugins()
 
     plugin_list = [
-        plugin + " (from package " + dist + ", version " + _version(dist) + ")"
+        plugin + " (from package " + dist + ", version " + get_version(dist) + ")"
         for dist, plugin in dist_plugin_pairs
     ]
     return "\n".join(plugin_list) if len(plugin_list) > 0 else "None"
@@ -69,7 +70,7 @@ def list_installed_plugins_versions():
 
 def get_version_info():
     return (
-        f"Version: {_version()}{os.linesep}"
+        f"Version: {get_version()}{os.linesep}"
         f"Build details: {build_details()}{os.linesep}{os.linesep}"
         f"Installed plugins:{os.linesep}{list_installed_plugins_versions()}"
     )

projects/vdk-core/tests/vdk/internal/builtin_plugins/termination_message/test_action.py
@@ -1,55 +1,0 @@
-# Copyright 2021 VMware, Inc.
-# SPDX-License-Identifier: Apache-2.0
-import unittest
-from unittest.mock import MagicMock
-from unittest.mock import patch
-
-from vdk.internal.builtin_plugins.termination_message.action import WriteToFileAction
-
-
-class TerminationActionTest(unittest.TestCase):
-    @patch("builtins.open")
-    def test_write_to_file_action_with_empty_file(self, mock_open):
-        action = WriteToFileAction("")
-        action.success()
-        mock_open.assert_not_called()
-
-    @patch("builtins.open")
-    def test_write_to_file_action_with_success(self, mock_open):
-        mock_file = MagicMock()
-        mock_open.return_value = mock_file
-        action = WriteToFileAction("somefile")
-        action.success()
-        # Assert that the write() method of the file returned by the open() mocked method is called once
-        mock_file.__enter__.return_value.write.assert_called_once_with("Success\n")
-
-    @patch("builtins.open")
-    def test_write_to_file_action_with_user_error(self, mock_open):
-        mock_file = MagicMock()
-        mock_open.return_value = mock_file
-        action = WriteToFileAction("somefile")
-        action.user_error()
-        # Assert that the write() method of the file returned by the open() mocked method is called once
-        mock_file.__enter__.return_value.write.assert_called_once_with("User error\n")
-
-    @patch("builtins.open")
-    def test_write_to_file_action_with_platform_error(self, mock_open):
-        mock_file = MagicMock()
-        mock_open.return_value = mock_file
-        action = WriteToFileAction("somefile")
-        action.platform_error()
-        # Assert that the write() method of the file returned by the open() mocked method is called once
-        mock_file.__enter__.return_value.write.assert_called_once_with(
-            "Platform error\n"
-        )
-
-    @patch("builtins.open")
-    @patch("logging.Logger.debug")
-    def test_write_to_file_when_error_happens(self, mock_logger_debug, mock_open):
-        mock_file = MagicMock()
-        mock_file.__enter__.return_value.write.side_effect = OSError()
-        mock_open.return_value = mock_file
-        action = WriteToFileAction("somefile")
-        action.platform_error()
-        # Assert that the mocked logger method is called once
-        mock_logger_debug.expect_to_be_called_once()

projects/vdk-core/tests/vdk/internal/builtin_plugins/termination_message/test_writer.py
@@ -9,6 +9,7 @@ from vdk.internal.builtin_plugins.config import vdk_config
 from vdk.internal.builtin_plugins.termination_message.writer import (
     TerminationMessageWriterPlugin,
 )
+from vdk.internal.builtin_plugins.version.version import get_version
 from vdk.internal.core.config import ConfigurationBuilder
 from vdk.internal.core.context import CoreContext
 from vdk.internal.core.errors import get_blamee_overall
@@ -48,4 +49,6 @@ class WriterTest(unittest.TestCase):
         vdk.vdk_exit(context, 0)
 
         # Assert that the write() method of the file returned by the open() mocked method is called once
-        mock_file.__enter__.return_value.write.assert_called_once_with("Success\n")
+        mock_file.__enter__.return_value.write.assert_called_once_with(
+            '{"vdk_version": "%s", "status": "Success"}' % get_version()
+        )

commit b6113e50c9082822eae57da63a740b92bebb6ba6

vdk-control-cli-plugin: Fix regression
why: A regression was introduced with a recent change:
582ccae
(L159 in the execution_skip.py file)
which overrode how we extract the execution id in the skip logic.

what: Changed it to the correct configuration variable.

testing: Local run of a data job, made sure we are
using the correct variable.

Signed-off-by: Momchil Zhivkov mzhivkov@vmware.com

projects/vdk-core/plugins/vdk-plugin-control-cli/src/vdk/plugin/control_cli_plugin/execution_skip.py
@@ -156,7 +156,7 @@ def run_job(context: JobContext) -> None:
         log_config_type = configuration.get_value(vdk_config.LOG_CONFIG)
         job_name = context.name
         job_team = configuration.get_value(JobConfigKeys.TEAM)
-        execution_id = configuration.get_value(vdk_config.EXECUTION_ID)
+        execution_id = context.core_context.state.get(CommonStoreKeys.EXECUTION_ID)
 
         return _skip_job_if_necessary(
             log_config_type, job_name, execution_id, job_team, configuration