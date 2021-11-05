971d1ae63d21a233f1464c97ae438a664e1ea155
Tsvetomir Palashki <tpalashki@vmware.com>
2021-11-05 11:22:31 +0200

    control-service: address review comments
    Signed-off-by: Tsvetomir Palashki <tpalashki@vmware.com>

--- projects/control-service/projects/pipelines_control_service/src/main/resources/db/migration/V20213110120000__add_last_execution_info_data_job.sql
+++ projects/control-service/projects/pipelines_control_service/src/main/resources/db/migration/V20213110120000__add_last_execution_info_data_job.sql
@@ -1,4 +1,4 @@
-alter table data_job
-    add last_execution_status smallint,
-    add last_execution_end_time timestamp,
-    add last_execution_duration int;
+alter table if exists data_job
+    add column if not exists last_execution_status smallint,
+    add column if not exists last_execution_end_time timestamp,
+    add column if not exists last_execution_duration int;
