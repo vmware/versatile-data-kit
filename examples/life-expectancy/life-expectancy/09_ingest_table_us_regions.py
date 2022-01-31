b0d14dd3259c727ef81716c819c2f999f0e860b9
Angelica Lo Duca <angelica.loduca@iit.cnr.it>
2022-01-31 17:12:04 +0100

    Moved le_utils within life-expectancy and removed import os block code
    - Moved le_utils within life-expectancy
    - removed import os block code from .py codes

--- examples/life-expectancy/life-expectancy/09_ingest_table_us_regions.py
+++ examples/life-expectancy/life-expectancy/09_ingest_table_us_regions.py
@@ -9,10 +9,6 @@ import numpy as np
 import pandas as pd
 
 
-current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parent_dir = os.path.dirname(current_dir)
-sys.path.insert(0, parent_dir)
-
 from le_utils.utils import build_insert_query
 
 from vdk.api.job_input import IJobInput
