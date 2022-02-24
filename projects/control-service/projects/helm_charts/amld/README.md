24b1fed99d7fa4de726fb0f62d3a798f70d6cc12
Antoni Ivanov <aivanov@vmware.com>
2022-02-24 17:42:31 +0200

    versatile-data-kit: add scripts for AMLD workshop env setup
    I will not be submitting the scripts to main branch. They will sit in
    this branch for reference purposes and sharing.
    
    The env is deployed in  Kubernetes used by CICD -
    https://github.com/vmware/versatile-data-kit/wiki/Gitlab-CICD
    
    In namespace vdk-amld
    
    Signed-off-by: Antoni Ivanov <aivanov@vmware.com>

---
+++ projects/control-service/projects/helm_charts/amld/README.md
@@ -0,0 +1,16 @@
+# Setup environment for AMLD workshop
+
+Here are scripts that preapre the backend enviornment to be used in AMLD workshop
+
+The entry point is the setup-env.sh script.
+It will
+- Install Pipelins Control Service
+- Install trino with mysql as backend catalog
+
+Aside from that because my binder allows connections only on port 80 or 443 (and a few more: https://github.com/jupyterhub/mybinder.org-deploy/blob/master/mybinder/values.yaml#L46)
+we use API gateway to expose those :
+
+https://us-west-1.console.aws.amazon.com/apigateway/main/develop/auth/attach?api=iaclqhm5xk&integration=qbji9io&region=us-west-1&routeKey=&routes=taal8qu&stage=$default
+
+1. Go to API Gateway -> Click Create API -> Follow instructions
+2. Then make sure to set in routes configuration . "$default" so that all paths are proxied
---
+++ projects/control-service/projects/helm_charts/amld/setup-env.sh
@@ -0,0 +1,22 @@
+
+# Copyright 2021 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+source env.sh || echo "Provide env.sh with exported environment variables: VDK_API_TOKEN and GIT_TOKEN"
+
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm upgrade --install --wait --timeout 10m0s  vdk-mysql bitnami/mysql
+
+
+
+envsubst < values.yaml > values-secret.yaml
+
+helm repo add vdk-gitlab https://gitlab.com/api/v4/projects/28814611/packages/helm/stable
+helm upgrade --install --wait --timeout 10m0s amld-vdk vdk-gitlab/pipelines-control-service -f values-secret.yaml
+
+
+export  MYSQL_ROOT_PASSWORD=$(kubectl get secret --namespace vdk-amld vdk-mysql -o jsonpath="{.data.mysql-root-password}" | base64 --decode)
+envsubst < values-trino.yaml > values-trino-secret.yaml
+
+helm repo add valeriano-manassero https://valeriano-manassero.github.io/helm-charts
+helm upgrade --install test-trino valeriano-manassero/trino --version 1.11.0 -f  values-trino-secret.yaml
