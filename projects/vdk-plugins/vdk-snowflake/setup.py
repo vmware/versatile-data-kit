acae3c8d33a789262bfdafd6208395df527e883c
Gabriel Georgiev <45939426+gageorgiev@users.noreply.github.com>
2022-03-01 17:24:49 +0200

    vdk-snowflake: Enable support for Python 3.10 (#746)
    Support for Python 3.10 was disabled due to the fact that the
    python-snowflake-connector had not been updated to support it.
    It has been now so we can enable support for vdk-snowflake as well.
    
    Signed-off-by: Gabriel Georgiev <gageorgiev@vmware.com>

--- projects/vdk-plugins/vdk-snowflake/setup.py
+++ projects/vdk-plugins/vdk-snowflake/setup.py
@@ -26,6 +26,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        #        "Programming Language :: Python :: 3.10", - not supported yet as the connector hasn't been updated
+        "Programming Language :: Python :: 3.10",
     ],
 )
