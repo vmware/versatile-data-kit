105a89c041057aabb14ab622e1e70dc9a6f2fe9e
Momchil Z <mzhivkov@vmware.com>
2022-03-11 14:21:22 +0200

    control-service: add kerberos auth provider (#755)
    what: Added a new AuthProviderConfig which should work with kerberos.
    
    why: users requested Kerberos authentication in parallel with OAuth2
    
    todo: Expose configuration properties against env variables/helm charts
    we need to decide which ones to expose and on default values.
    
    testing: Locally ran control service, made sure enable/disable flag works.
    Ran requests on a locally running instance. Verified that: both OAuth2 and
    SPNEGO filters are used when making requests and that users can authenticate
    with either OAuth2 or Kerberos credentials. Requests were made with an
    authenticated vdkcli:
    vdk execute --list -t supercollider -n job -u http://localhost:8092
    and with a curl command for the kerberos authentication:
    curl -vvv --negotiate -u : "http://localhost:8092/data-jobs/for-team/supercollider/jobs/smoke-test/executions"
    
    Signed-off-by: Momchil Zhivkov mzhivkov@vmware.com

---
+++ projects/control-service/projects/base/src/main/java/com/vmware/taurus/security/KerberosConfig.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2021 VMware, Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.vmware.taurus.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.kerberos.authentication.sun.GlobalSunJaasKerberosConfig;
+
+@Configuration
+@ConditionalOnProperty(value = "datajobs.security.kerberos.enabled")
+public class KerberosConfig {
+
+   @Value("${datajobs.security.kerberos.krb5ConfigLocation}")
+   private String krb5ConfigLocation;
+
+   @Bean
+   public GlobalSunJaasKerberosConfig globalSunJaasKerberosConfig() {
+      var config = new GlobalSunJaasKerberosConfig();
+      config.setDebug(true);
+      config.setKrbConfLocation(krb5ConfigLocation);
+      return config;
+   }
+}
