6ef12a090777604b69623f0ce2d8c17e1a069dfa
Antoni Ivanov <aivanov@vmware.com>
2022-03-20 12:25:14 +0200

    vdk-plugins: make sure build script upgrades requiremennts
    If build-plugin.sh is used locally and we have stale requirements , they
    will not be upgraded. This is changing it so new versions are always
    fetched eagerly
    
    Testing Done: ran the script locally
    
    Signed-off-by: Antoni Ivanov <aivanov@vmware.com>

--- projects/vdk-plugins/build-plugin.sh
+++ projects/vdk-plugins/build-plugin.sh
@@ -11,8 +11,8 @@ PIP_EXTRA_INDEX_URL=${PIP_EXTRA_INDEX_URL:-'https://pypi.org/simple'}
 pip install -U pip setuptools pre-commit
 pre-commit install --hook-type commit-msg --hook-type pre-commit
 
-pip install --extra-index-url $PIP_EXTRA_INDEX_URL -r requirements.txt
-pip install -e . --extra-index-url $PIP_EXTRA_INDEX_URL
+pip install --upgrade --extra-index-url $PIP_EXTRA_INDEX_URL -r requirements.txt
+pip install --upgrade --upgrade-strategy eager -e . --extra-index-url $PIP_EXTRA_INDEX_URL
 
 # List exceptions to below check here.
 # Those are not technically plugins so they would not have entry point defined.
