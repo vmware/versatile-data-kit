0712329377952843a3f9f6b18e02f6381b32163f
Gabriel Georgiev <gageorgiev@vmware.com>
2022-03-23 12:33:11 +0200

    make it configurable
    Signed-off-by: Gabriel Georgiev <gageorgiev@vmware.com>

--- projects/control-service/projects/pipelines_control_service/src/main/java/com/vmware/taurus/service/KubernetesService.java
+++ projects/control-service/projects/pipelines_control_service/src/main/java/com/vmware/taurus/service/KubernetesService.java
@@ -172,6 +172,9 @@ public abstract class KubernetesService implements InitializingBean {
     @org.springframework.beans.factory.annotation.Value("${datajobs.control.k8s.data.job.template.file}")
     private String datajobTemplateFileLocation;
 
+    @org.springframework.beans.factory.annotation.Value("${datajobs.control.k8s.cronJobTTLAfterFinished}")
+    private int cronJobTTLAfterFinished;
+
     private String namespace;
     private String kubeconfig;
     private Logger log;
@@ -546,7 +549,7 @@ public abstract class KubernetesService implements InitializingBean {
                 .map(V1beta1JobTemplateSpec::getSpec)
                 .orElseThrow(() -> new ApiException(String.format("K8S Cron Job '%s' does not exist or is not properly defined.", cronJobName)));
 
-        jobSpec.setTtlSecondsAfterFinished(3600);
+        jobSpec.setTtlSecondsAfterFinished(cronJobTTLAfterFinished);
 
         V1Container v1Container = Optional.ofNullable(jobSpec.getTemplate())
                 .map(V1PodTemplateSpec::getSpec)
@@ -590,7 +593,7 @@ public abstract class KubernetesService implements InitializingBean {
               .map(V1JobTemplateSpec::getSpec)
               .orElseThrow(() -> new ApiException(String.format("K8S Cron Job '%s' does not exist or is not properly defined.", cronJobName)));
 
-        jobSpec.setTtlSecondsAfterFinished(3600);
+        jobSpec.setTtlSecondsAfterFinished(cronJobTTLAfterFinished);
 
         V1Container v1Container = Optional.ofNullable(jobSpec.getTemplate())
               .map(V1PodTemplateSpec::getSpec)
@@ -818,7 +821,7 @@ public abstract class KubernetesService implements InitializingBean {
                 .build();
         var spec = new V1JobSpecBuilder()
                 .withBackoffLimit(3) //TODO configure
-                .withTtlSecondsAfterFinished(3600)  //TODO configure
+                .withTtlSecondsAfterFinished(cronJobTTLAfterFinished)  //TODO configure
                 .withTemplate(template)
                 .build();
         createNewJob(name, spec, Collections.emptyMap(), Collections.emptyMap());
