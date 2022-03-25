d6969212183535b06f5b446d137698bef3f5cf5c
Gabriel Georgiev <gageorgiev@vmware.com>
2022-03-25 16:35:58 +0200

    VDKHook interface, followed the airflow-provider-sample template
    Signed-off-by: Gabriel Georgiev <gageorgiev@vmware.com>

---
+++ support/airflow-provider-vdk/setup.py
@@ -0,0 +1,27 @@
+# Copyright 2021 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+from setuptools import find_packages
+from setuptools import setup
+
+with open("README.md") as fh:
+    long_description = fh.read()
+
+"""Perform the package airflow-provider-vdk setup."""
+setup(
+    name="airflow-provider-vdk",
+    version="0.0.1",
+    description="Airflow provider for Versatile Data Kit.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    entry_points={
+        "apache_airflow_provider": ["provider_info=vdk.__init__:get_provider_info"]
+    },
+    license="Apache License 2.0",
+    packages=["vdk", "vdk.hooks", "vdk.sensors", "vdk.operators"],
+    install_requires=["apache-airflow>=2.0", "tenacity>=6.2.0", "vdk-core"],
+    setup_requires=["setuptools", "wheel"],
+    author="Versatile Data Kit Development Team",
+    author_email="versatile-data-kit@vmware.com",
+    url="https://github.com/vmware/versatile-data-kit",
+    python_requires="~=3.7",
+)
---
+++ support/airflow-provider-vdk/vdk/hooks/vdk.py
@@ -0,0 +1,62 @@
+# Copyright 2021 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+from airflow.providers.http.hooks.http import HttpHook
+from tenacity import sleep
+from tenacity import stop_after_attempt
+from vdk.internal.builtin_plugins.run.run_status import ExecutionStatus
+
+
+class VDKHook(HttpHook):
+    def __init__(
+        self,
+        conn_id,
+        job_name,
+        team_name,
+        timeout=1,  # TODO: Set reasonable defaults
+        retry_limit=1,
+        retry_delay=1,
+    ):
+        super().__init__(http_conn_id=conn_id)
+
+        self.job_name = job_name
+        self.team_name = team_name
+        self.timeout = timeout
+        self.deployment_id = "production"  # currently multiple deployments are not supported so this remains hardcoded
+        # hook methods will use HttpHook's `run_with_advanced_retry` method and pass the _retry_dict as a parameter
+        self._retry_dict = {
+            "stop": stop_after_attempt(retry_limit),
+            "sleep": sleep(retry_delay),
+        }
+
+    def start_job_execution(self) -> None:
+        """
+        API path: POST /data-jobs/for-team/{team_name}/jobs/{job_name}/deployments/{deployment_id}/executions
+
+        """
+        pass
+
+    def cancel_job_execution(self, execution_id: str) -> None:
+        """
+        API path: DELETE /data-jobs/for-team/{team_name}/jobs/{job_name}/executions/{execution_id}
+
+        :param execution_id: ID of the job execution
+        """
+        pass
+
+    def get_job_execution_log(self, execution_id: str) -> str:
+        """
+        API path: GET /data-jobs/for-team/{team_name}/jobs/{job_name}/executions/{execution_id}/logs
+
+        :param execution_id: ID of the job execution
+        :return: job execution logs
+        """
+        pass
+
+    def get_job_execution_status(self, execution_id: str) -> ExecutionStatus:
+        """
+        API path: GET /data-jobs/for-team/{team_name}/jobs/{job_name}/deployments/{deployment_id}/executions
+
+        :param execution_id: ID of the job execution
+        :return: Execution status; either SUCCESS, NOT_RUNNABLE or ERROR
+        """
+        pass
