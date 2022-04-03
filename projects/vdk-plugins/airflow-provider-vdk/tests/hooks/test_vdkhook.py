11a98cb123db525876c7222af575c6e405960ab8
Gabriel Georgiev <gageorgiev@vmware.com>
2022-04-03 11:15:35 +0300

    restructure testing class
    Signed-off-by: Gabriel Georgiev <gageorgiev@vmware.com>

--- projects/vdk-plugins/airflow-provider-vdk/tests/hooks/test_vdkhook.py
+++ projects/vdk-plugins/airflow-provider-vdk/tests/hooks/test_vdkhook.py
@@ -16,32 +16,29 @@ log = logging.getLogger(__name__)
     "os.environ", AIRFLOW_CONN_CONN_VDK="http://https%3A%2F%2Fwww.vdk-endpoint.org"
 )
 class TestVDKHook(unittest.TestCase):
-    @staticmethod
+    def __init__(self):
+        self.hook = VDKHook(
+            conn_id="conn_vdk", job_name="test_job", team_name="test_team"
+        )
+
     @requests_mock.mock()
-    def test_start_job_execution(m):
+    def test_start_job_execution(self, m):
         request_url = "https://www.vdk-endpoint.org/data-jobs/for-team/test_team/jobs/test_job/deployments/production/executions"
         # Mock endpoint
         m.post(request_url)
 
-        # Instantiate hook
-        hook = VDKHook(conn_id="conn_vdk", job_name="test_job", team_name="test_team")
-
-        hook.start_job_execution()
+        self.hook.start_job_execution()
 
         assert m.request_history[0].method == "POST"
         assert m.request_history[0].url == request_url
 
-    @staticmethod
     @requests_mock.mock()
-    def test_cancel_job_execution(m):
+    def test_cancel_job_execution(self, m):
         request_url = "https://www.vdk-endpoint.org/data-jobs/for-team/test_team/jobs/test_job/executions/test_execution_id"
         # Mock endpoint
         m.delete(request_url)
 
-        # Instantiate hook
-        hook = VDKHook(conn_id="conn_vdk", job_name="test_job", team_name="test_team")
-
-        hook.cancel_job_execution("test_execution_id")
+        self.hook.cancel_job_execution("test_execution_id")
 
         assert m.request_history[0].method == "DELETE"
         assert m.request_history[0].url == request_url
