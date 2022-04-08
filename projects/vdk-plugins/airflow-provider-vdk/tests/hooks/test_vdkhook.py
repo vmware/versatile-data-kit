78016b0a4514ded61a035974c10e3cff7e66c85b
Gabriel Georgiev <45939426+gageorgiev@users.noreply.github.com>
2022-04-08 14:27:03 +0300

    airflow-provider-vdk: Start and cancel job execution methods (#778)
    This change allows job executions to be started and cancelled
    through the VDK connection hook object.
    
    Testing done: included two unit tests, tested locally against
    deployed control-service
    
    Signed-off-by: Gabriel Georgiev <gageorgiev@vmware.com>

--- projects/vdk-plugins/airflow-provider-vdk/tests/hooks/test_vdkhook.py
+++ projects/vdk-plugins/airflow-provider-vdk/tests/hooks/test_vdkhook.py
@@ -1,9 +1,35 @@
 # Copyright 2021 VMware, Inc.
 # SPDX-License-Identifier: Apache-2.0
+import logging
+import unittest
+from unittest import mock
+
 from vdk_provider.hooks.vdk import VDKHook
 
+log = logging.getLogger(__name__)
 
-def test_dummy():
-    hook = VDKHook("conn_id", "job_name", "team_name")
 
-    assert hook
+class TestVDKHook(unittest.TestCase):
+    @mock.patch.dict(
+        "os.environ", AIRFLOW_CONN_CONN_VDK="http://https%3A%2F%2Fwww.vdk-endpoint.org"
+    )
+    def setUp(self):
+        self.hook = VDKHook(
+            conn_id="conn_vdk", job_name="test_job", team_name="test_team"
+        )
+
+    @mock.patch("taurus_datajob_api.api_client.ApiClient.request")
+    def test_start_job_execution(self, mocked_api_client_request):
+        request_url = "https://www.vdk-endpoint.org/data-jobs/for-team/test_team/jobs/test_job/deployments/production/executions"
+
+        self.hook.start_job_execution()
+
+        assert mocked_api_client_request.call_args_list[0][0] == ("POST", request_url)
+
+    @mock.patch("taurus_datajob_api.api_client.ApiClient.request")
+    def test_cancel_job_execution(self, mocked_api_client_request):
+        request_url = "https://www.vdk-endpoint.org/data-jobs/for-team/test_team/jobs/test_job/executions/test_execution_id"
+
+        self.hook.cancel_job_execution("test_execution_id")
+
+        assert mocked_api_client_request.call_args_list[0][0] == ("DELETE", request_url)
