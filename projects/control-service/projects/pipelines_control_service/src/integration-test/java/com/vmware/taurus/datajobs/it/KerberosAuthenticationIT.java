1cbed3562ccccd8f666133e45c0d1f3f44c27351
mrMoZ1 <mzhivkov@vmware.com>
2022-04-13 20:45:02 +0300

    kerberos auth it
    Signed-off-by: mrMoZ1 <mzhivkov@vmware.com>

--- projects/control-service/projects/pipelines_control_service/src/integration-test/java/com/vmware/taurus/datajobs/it/KerberosAuthenticationIT.java
+++ projects/control-service/projects/pipelines_control_service/src/integration-test/java/com/vmware/taurus/datajobs/it/KerberosAuthenticationIT.java
@@ -2,171 +2,126 @@
  * Copyright 2021 VMware, Inc.
  * SPDX-License-Identifier: Apache-2.0
  */
-
 package com.vmware.taurus.datajobs.it;
 
-import com.google.common.io.Files;
-import com.sun.security.auth.callback.TextCallbackHandler;
-import com.vmware.taurus.ServiceApp;
-import org.junit.Assert;
+import com.kerb4j.client.SpnegoClient;
+import com.kerb4j.client.SpnegoContext;
+import com.vmware.taurus.ControlplaneApplication;
+import com.vmware.taurus.service.JobsRepository;
+import com.vmware.taurus.service.model.DataJob;
+import com.vmware.taurus.service.model.JobConfig;
+import org.apache.kerby.kerberos.kerb.server.SimpleKdcServer;
+import org.apache.kerby.util.NetworkUtil;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.security.kerberos.client.KerberosRestTemplate;
-import org.springframework.security.kerberos.test.KerberosSecurityTestcase;
-import org.springframework.security.kerberos.test.MiniKdc;
+import org.springframework.core.Ordered;
+import org.springframework.test.context.TestContext;
+import org.springframework.test.context.TestExecutionListener;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import javax.security.auth.Subject;
-import javax.security.auth.kerberos.KerberosPrincipal;
-import javax.security.auth.login.AppConfigurationEntry;
-import javax.security.auth.login.Configuration;
-import javax.security.auth.login.LoginContext;
 import java.io.File;
-import java.security.Principal;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Properties;
-import java.util.Set;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-
+@TestExecutionListeners(value = {KerberosAuthenticationIT.KdcExecutionListener.class}, mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
 @TestPropertySource(properties = {
-      "datajobs.security.kerberos.enabled=true",
+      "test.dir=target",
       "featureflag.security.enabled=true",
-      "datajobs.security.kerberos.kerberosPrincipal=\"pa__view_foo\"",
+      "datajobs.security.kerberos.enabled=true",
+      "featureflag.authorization.enabled=false",
+      "datajobs.security.kerberos.kerberosPrincipal=HTTP/localhost",
       "datajobs.security.kerberos.keytabFileLocation=target/foo.keytab",
-      "datajobs.security.kerberos.krb5ConfigLocation=target/krb5.conf"
+      "datajobs.security.kerberos.krb5ConfigLocation=target/krb5.conf",
+      "java.security.krb5.conf=target/krb5.conf",
+      "sun.security.krb5.debug=true",
+      "datajobs.authorization.authorized-roles="
 })
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = ServiceApp.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = ControlplaneApplication.class)
 @ExtendWith(SpringExtension.class)
-//@AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class KerberosAuthenticationIT {
+   // composition in favour of inheritance, due to KDC instance is used via TestExecutionListeners and Test both (shared)
+   private static final String WORK_DIR = "target";
+   private static final File KDC_WORK_DIR = new File(WORK_DIR);
+   private static final File KEYTAB = new File(WORK_DIR + "/foo.keytab");
+   private static final String CLIENT_PRINCIPAL = "client/localhost";
+   private static final String TGT_PRINCIPAL = "HTTP/localhost";
 
-   private static KerberosSecurityTestcase kerberosSecurityTestcase;
-   private static Properties kdcConf;
-   private static File krb5;
-   private static Set<Principal> principals = new HashSet<Principal>();
-   private static File keytab;
-   private static MiniKdc kdc;
-   private static String principal = "pa__view_foo";
+   private static SimpleKdcServer simpleKdcServer;
 
 
-//   @Autowired
-//   protected MockMvc mockMvc;
+   @Autowired
+   private JobsRepository jobsRepository;
+
+   public static class KdcExecutionListener implements TestExecutionListener, Ordered {
+      @Override
+      public int getOrder() {
+         return 0;
+      }
+
+      @Override
+      public void beforeTestClass(final TestContext testContext) throws Exception {
+         // Initialize the KDC server
+         simpleKdcServer = new SimpleKdcServer();
+         simpleKdcServer.setWorkDir(KDC_WORK_DIR);
+         simpleKdcServer.setAllowTcp(true);
+         simpleKdcServer.setAllowUdp(true);
+         simpleKdcServer.setKdcUdpPort(NetworkUtil.getServerPort());
+         // Start the KDC server
+         simpleKdcServer.init();
+         simpleKdcServer.start();
+         simpleKdcServer.createAndExportPrincipals(KEYTAB, CLIENT_PRINCIPAL, TGT_PRINCIPAL);
+      }
+   }
 
    @LocalServerPort
    int randomPort;
 
-   static {
-      try {
-         kerberosSecurityTestcase = new KerberosSecurityTestcase();
-         kerberosSecurityTestcase.getConf();
-         kerberosSecurityTestcase.startMiniKdc();
-         kdc = kerberosSecurityTestcase.getKdc();
-         kdcConf = kerberosSecurityTestcase.getConf();
-         File workDir = kerberosSecurityTestcase.getWorkDir();
-         krb5 = kdc.getKrb5conf();
-
-         keytab = new File(workDir, "foo.keytab");
-         File configurationKrb = new File(workDir, "krb5.conf");
-         kdc.createPrincipal(keytab, principal);
-
-         principals.add(new KerberosPrincipal(principal));
-         Files.copy(krb5, configurationKrb);
-         System.setProperty("java.security.krb5.conf", krb5.getAbsolutePath());
-         System.setProperty("sun.security.krb5.debug", "true");
-      } catch (Exception e) {
-         throw new RuntimeException("Unable to start mini KDC service for Integration Test.", e);
-      }
-   }
-
-   @BeforeEach
-   public void setup() {
-
+   @BeforeAll
+   public void addTestJob() {
+      // Add a dummy data job so that endpoint call returns 200 instead of 404
+      var dataJob = new DataJob();
+      var config = new JobConfig();
+      config.setTeam("test");
+      dataJob.setName("test");
+      dataJob.setJobConfig(config);
+      jobsRepository.save(dataJob);
    }
 
    @Test
-   public void test() throws Exception {
+   public void testAuthenticatedCall() throws Exception {
 
-      Subject subject = new Subject(false, principals, new HashSet<Object>(), new HashSet<Object>());
-      var loginContext = new LoginContext("", subject, new TextCallbackHandler(), KerberosConfiguration.createClientConfig(principal,
-            keytab));
-      loginContext.login();
-      subject = loginContext.getSubject();
-      Assert.assertEquals(1, subject.getPrincipals().size());
-      Assert.assertEquals(KerberosPrincipal.class, subject.getPrincipals().iterator().next().getClass());
-      Assert.assertEquals(principal + "@" + kdc.getRealm(), subject.getPrincipals().iterator().next().getName());
+      SpnegoClient spnegoClient = SpnegoClient.loginWithKeyTab(CLIENT_PRINCIPAL, KEYTAB.getPath());
+      URL url = new URL("http://127.0.0.1:" + randomPort + "/data-jobs/for-team/test/jobs/test");
+      var krb5 = simpleKdcServer.getKrbClient().getKrbConfig();
+      SpnegoContext context = spnegoClient.createContext(new URL("http://" + krb5.getKdcHost() + "/" + krb5.getKdcPort()));
+      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+      conn.setRequestProperty("Authorization", context.createTokenAsAuthroizationHeader());
+      var res = conn.getResponseCode();
 
-      // Get ticket from login context
-//
-//      KerberosTicket kerberosTicket = null;
-//
-//      for (var object : subject.getPrivateCredentials()) {
-//         if (object instanceof KerberosTicket) {
-//            kerberosTicket = (KerberosTicket) object;
-//         }
-//      }
+      Assertions.assertEquals(200, res);
+   }
 
-      KerberosRestTemplate kerberosRestTemplate = new KerberosRestTemplate(keytab.getPath(), principal);
-      var response = kerberosRestTemplate.getForObject("http://localhost:" + randomPort + "/data-jobs/for-team/supercollider/jobs", String.class);
-
+   @Test
+   public void testUnauthenticatedCall() throws Exception {
+      URL url = new URL("http://127.0.0.1:" + randomPort + "/data-jobs/for-team/test/jobs/test");
+      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+      var res = conn.getResponseCode();
+      Assertions.assertEquals(401, res);
    }
 
    @AfterAll
-   public static void cleanup() {
-      kerberosSecurityTestcase.stopMiniKdc();
-   }
-
-   private static class KerberosConfiguration extends Configuration {
-      private String principal;
-      private String keytab;
-      private boolean isInitiator;
-
-      private KerberosConfiguration(String principal, File keytab, boolean client) {
-         this.principal = principal;
-         this.keytab = keytab.getAbsolutePath();
-         this.isInitiator = client;
-      }
-
-      public static Configuration createClientConfig(String principal, File keytab) {
-         return new KerberosConfiguration(principal, keytab, true);
-      }
-
-      public static Configuration createServerConfig(String principal, File keytab) {
-         return new KerberosConfiguration(principal, keytab, false);
-      }
-
-      private static String getKrb5LoginModuleName() {
-         return System.getProperty("java.vendor").contains("IBM") ? "com.ibm.security.auth.module.Krb5LoginModule"
-               : "com.sun.security.auth.module.Krb5LoginModule";
-      }
-
-      @Override
-      public AppConfigurationEntry[] getAppConfigurationEntry(String name) {
-         Map<String, String> options = new HashMap<String, String>();
-         options.put("keyTab", keytab);
-         options.put("principal", principal);
-         options.put("useKeyTab", "true");
-         options.put("storeKey", "true");
-         options.put("doNotPrompt", "true");
-         options.put("useTicketCache", "true");
-         options.put("renewTGT", "true");
-         options.put("refreshKrb5Config", "true");
-         options.put("isInitiator", Boolean.toString(isInitiator));
-         String ticketCache = System.getenv("KRB5CCNAME");
-         if (ticketCache != null) {
-            options.put("ticketCache", ticketCache);
-         }
-         options.put("debug", "true");
-
-         return new AppConfigurationEntry[]{new AppConfigurationEntry(getKrb5LoginModuleName(),
-               AppConfigurationEntry.LoginModuleControlFlag.REQUIRED, options)};
-      }
+   public void cleanup() {
+      KDC_WORK_DIR.deleteOnExit();
    }
 
 }
