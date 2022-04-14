36b2782c241bcd16c37b8ac3dfe5e638d9de2204
Antoni Ivanov <aivanov@vmware.com>
2022-04-14 16:22:04 +0300

    vdk-lineage: introducing POC (pre-alpha) implementation (#783)
    VDK Lineage plugin provides lineage data (input data -> job -> output
    data) information and send it to pre-configured
    destination.
    
    At POC level currently (Pre-Alpha release).
    It collects lineage information for each job run and for each executed
    query.
    Query execution is currently before it's executed (so not query status
    is logged) as we lack the necessary hooks.
    
    There are plenty of TODOs in the code that need to be addressed before
    moving maturity level and add more tests.
    
    Testing Done: there are some tests and also manually:
    ```
    vdk marquez-server --start
    vdk run some-job
    vdk marquez-server --stop
    ```
    
    Signed-off-by: Antoni Ivanov <aivanov@vmware.com>

---
+++ projects/vdk-plugins/vdk-lineage/setup.py
@@ -0,0 +1,45 @@
+# Copyright 2021 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+import pathlib
+
+import setuptools
+
+"""
+Builds a package with the help of setuptools in order for this package to be imported in other projects
+"""
+
+__version__ = "0.1.5"
+
+setuptools.setup(
+    name="vdk-lineage",
+    version=__version__,
+    url="https://github.com/vmware/versatile-data-kit",
+    description="VDK Lineage plugin collects lineage (input -> job -> output) information "
+    "and send it to a pre-configured destination.",
+    long_description=pathlib.Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "vdk-core",
+        "sqlparse",
+        "sqllineage",
+        "openlineage-integration-common",
+        "openlineage-python",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_namespace_packages(where="src"),
+    include_package_data=True,
+    entry_points={
+        "vdk.plugin.run": [
+            "vdk-lineage = vdk.plugin.lineage.plugin_lineage",
+            "vdk-marquez = vdk.plugin.marquez.marquez_plugin",
+        ]
+    },
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
---
+++ projects/vdk-plugins/vdk-lineage/tests/jobs/sql-job/30_copy_table.sql
@@ -0,0 +1,1 @@
+create table stocks2 as select * from stocks
