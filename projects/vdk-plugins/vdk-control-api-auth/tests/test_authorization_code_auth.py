ecee32d4312a78f442535b4074db0fb5c7a497c3
Andon Andonov <andonova@vmware.com>
2022-05-13 00:10:48 +0300

    [vdk-plugins] vdk-control-api-auth: Add authorization code flow
    As part of the ongoing work to build a stand-alone authentication plugin
    library, which is to be used by all components of Versatile Data Kit that
    rely on authentication, we need to move and generalize the existing logic
    from vdk-control-cli.
    
    This change introduces the authorization code flow as defined in
    https://datatracker.ietf.org/doc/html/rfc6749#section-4.1 The implementation
    of this flow for the purposes of Versatile Data Kit also relies on the use
    of Proof Key for Code Exchange or pkce.
    
    Testing Done: Unit tests.
    
    Signed-off-by: Andon Andonov <andonova@vmware.com>

---
+++ projects/vdk-plugins/vdk-control-api-auth/tests/test_authorization_code_auth.py
@@ -0,0 +1,74 @@
+# Copyright 2021 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+import pytest
+from pytest_httpserver.pytest_plugin import PluginHTTPServer
+from test_core_auth import allow_oauthlib_insecure_transport
+from test_core_auth import get_json_response_mock
+from vdk.plugin.control_api_auth.auth_exception import VDKAuthException
+from vdk.plugin.control_api_auth.authentication import Authentication
+from vdk.plugin.control_api_auth.autorization_code_auth import LoginHandler
+from vdk.plugin.control_api_auth.autorization_code_auth import RedirectAuthentication
+from vdk.plugin.control_api_auth.base_auth import BaseAuth
+from vdk.plugin.control_api_auth.base_auth import InMemAuthConfiguration
+
+
+def test_verify_redirect_url():
+    allow_oauthlib_insecure_transport()
+    in_mem_conf = InMemAuthConfiguration()
+    auth = BaseAuth(in_mem_conf)
+
+    auth = RedirectAuthentication(
+        client_id="client-id",
+        client_secret="client-secret",
+        oauth2_discovery_url="http://discovery-url",
+        oauth2_exchange_url="http://exchange-url",
+        auth=auth,
+        redirect_uri="http://127.0.0.1",
+        redirect_uri_default_port=9999,
+    )
+    authorization_url = auth._create_authorization_redirect_url()
+    assert (
+        authorization_url[0]
+        == "http://discovery-url?response_type=code&client_id=client-id&redirect_uri=http%3A%2F%2F127.0.0.1%3A9999&state=requested&prompt=login"
+    )
+    assert authorization_url[1] == "requested"
+
+
+def test_login_handler_exception(httpserver: PluginHTTPServer):
+    allow_oauthlib_insecure_transport()
+    httpserver.expect_request("/foo").respond_with_json(get_json_response_mock())
+    in_mem_conf = InMemAuthConfiguration()
+    auth = BaseAuth(in_mem_conf)
+
+    handler = LoginHandler(
+        client_id=None,
+        client_secret=None,
+        exchange_endpoint=httpserver.url_for("/foo"),
+        redirect_uri=None,
+        code_verifier=None,
+        auth=auth,
+    )
+
+    with pytest.raises(VDKAuthException) as exc_info:
+        handler.login_with_authorization_code("dummy-path")
+
+    raised_exception = exc_info.value
+    assert "Failed to login." in raised_exception.message
+    assert "Possibly the request was intercepted." in raised_exception.message
+
+
+def test_authorization_code_no_secret(httpserver: PluginHTTPServer):
+    allow_oauthlib_insecure_transport()
+    httpserver.expect_request("/foo").respond_with_json(get_json_response_mock())
+
+    auth = Authentication(
+        token="apitoken",
+        authorization_url=httpserver.url_for("/foo"),
+        auth_type="credentials",
+    )
+    with pytest.raises(VDKAuthException) as exc_info:
+        auth.authenticate()
+
+    raised_exception = exc_info.value
+    assert "Unable to log in." in raised_exception.message
+    assert "Specify client_id and auth_discovery_url" in raised_exception.message
