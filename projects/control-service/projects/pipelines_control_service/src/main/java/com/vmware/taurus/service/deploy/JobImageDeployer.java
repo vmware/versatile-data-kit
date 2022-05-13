4b4e4520f68b5a7c6a5229301a3eb04948b4ab83
Gabriel Georgiev <gageorgiev@vmware.com>
2022-05-13 18:42:01 +0300

    dropped unused import
    Signed-off-by: Gabriel Georgiev <gageorgiev@vmware.com>

--- projects/control-service/projects/pipelines_control_service/src/main/java/com/vmware/taurus/service/deploy/JobImageDeployer.java
+++ projects/control-service/projects/pipelines_control_service/src/main/java/com/vmware/taurus/service/deploy/JobImageDeployer.java
@@ -21,7 +21,6 @@ import org.apache.commons.lang3.Validate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.print.DocFlavor;
 import java.time.OffsetDateTime;
 import java.util.*;
 
