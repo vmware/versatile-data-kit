c1bd7aeeebc530fc08f3f51f8b1f6f428968fd80
Gabriel Georgiev <gageorgiev@vmware.com>
2022-06-01 11:24:56 +0300

    airflow-provider-vdk: Add comments to DAG
    This change addresses missed comments in the previous PR.
    It adds comments explaining the DAG.
    
    Signed-off-by: Gabriel Georgiev <gageorgiev@vmware.com>

--- projects/vdk-plugins/airflow-provider-vdk/vdk_provider/example_dags/example_vdk.py
+++ projects/vdk-plugins/airflow-provider-vdk/vdk_provider/example_dags/example_vdk.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from airflow import DAG
 from vdk_provider.operators.vdk import VDKOperator
 
+# instantiate the dag
 with DAG(
     "example_vdk",
     schedule_interval=None,
@@ -12,18 +13,22 @@ with DAG(
     catchup=False,
     tags=["example"],
 ) as dag:
+    # run data job example_vdk_job1 belonging to team example_team_name
     job1 = VDKOperator(
         conn_id="example_vdk_connection",
         job_name="example_vdk_job1",
-        team_name="team_name",
+        team_name="example_team_name",
         task_id="job1",
     )
 
+    # run data job example_vdk_job2 belonging to team example_team_name
     job2 = VDKOperator(
-        conn_id="tms-example_vdk_connection",
-        job_name="gg-example_vdk_job2",
+        conn_id="example_vdk_connection",
+        job_name="example_vdk_job2",
         team_name="team_name",
         task_id="job2",
     )
 
+    # declare job dependencies
+    # example_vdk_job1 is executed first, and if it succeeds, example_vdk_job2 is executed
     job1 >> job2
