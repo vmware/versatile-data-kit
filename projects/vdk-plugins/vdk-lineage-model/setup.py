3350b31c0c2cd1401398d3ed2d587da19aacea8a
VladimirPetkov1 <37925189+VladimirPetkov1@users.noreply.github.com>
2022-07-06 16:42:22 +0300

    vdk-lineage-model: Extract Lineage Model in separate plugin (#896)
    Currently there is code duplication in vdk-trino and vdk-linage plugins and
    also any new plugin that wants to emit lineage data will have to copy-paste the
    lineage model and logger interface.
    Now that the lineage model is spearate plugin/lib it can be consumed by
    different plugins like vdk-trino, vdk-lineage and for e.g. vdk-impala without
    introducing code duplication.
    After this change gets released will remove code duplication in vdk-lineage and
    vdk-trino plugins in separate change.
    
    Signed-off-by: Vladimir Petkov <petkovv@vmware.com>

---
+++ projects/vdk-plugins/vdk-lineage-model/setup.py
@@ -0,0 +1,33 @@
+# Copyright 2021 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+import pathlib
+
+import setuptools
+
+"""
+Builds a package with the help of setuptools in order for this package to be imported in other projects
+"""
+
+__version__ = "0.0.1"
+
+setuptools.setup(
+    name="vdk-lineage-model",
+    version=__version__,
+    url="https://github.com/vmware/versatile-data-kit",
+    description="VDK Lineage Model plugin defines common lineage model and classes used for managing lineage"
+    "information in other VDK plugins.",
+    long_description=pathlib.Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    install_requires=[],
+    package_dir={"": "src"},
+    packages=setuptools.find_namespace_packages(where="src"),
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
