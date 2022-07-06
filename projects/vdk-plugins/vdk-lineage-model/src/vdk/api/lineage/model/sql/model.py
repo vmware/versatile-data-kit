492994245a904f79529a4e08457702e60cbaec4a
Vladimir Petkov <petkovv@vmware.com>
2022-07-06 08:58:42 +0300

    vdk-lineage-model: Extract Lineage Model in separate plugin
    Currently there is code duplication in vdk-trino and vdk-linage plugins and
    also any new plugin that wants to emit lineage data will have to copy-paste the
    lineage model and logger interface.
    Now that the lineage model is spearate plugin/lib it can be consumed by
    different plugins like vdk-trino, vdk-lineage and for e.g. vdk-impala without
    introducing code duplication.
    After this change gets released will remove code duplication in vdk-lineage and
    vdk-trino plugins in separate change.
    
    Signed-off-by: Vladimir Petkov <petkovv@vmware.com>

---
+++ projects/vdk-plugins/vdk-lineage-model/src/vdk/api/lineage/model/sql/model.py
@@ -0,0 +1,57 @@
+# Copyright 2021 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+from dataclasses import dataclass
+from typing import List
+from typing import Optional
+
+
+@dataclass(frozen=True)
+class LineageTable:
+    """
+    Defines the LineageTable object contract
+
+    Attributes
+    catalog: str
+        A string holding the catalog where the table lives
+    schema: str
+        A string holding the schema where the table lives
+    table: str
+        A string holding the table name
+    """
+
+    catalog: Optional[str]
+    schema: Optional[str]
+    table: str
+
+    def __str__(self):
+        s = ""
+        if self.catalog:
+            s = f"{self.catalog}."
+        if self.schema:
+            s = f"{s}{self.schema}."
+        return f"{s}{self.table}"
+
+
+@dataclass(frozen=True)
+class LineageData:
+    """
+    Defines the LineageData contract
+
+    Attributes
+    query: str
+        The original query
+    query_type: str
+        Type of operation
+    query_status: str
+        'OK' or 'EXCEPTION'
+    input_tables: List[LineageTable]
+        An array of LineageTable objects (see LineageTable)
+    output_table: LineageTable
+        A LineageTable object (see LineageTable)
+    """
+
+    query: str
+    query_type: str
+    query_status: str
+    input_tables: List[LineageTable]
+    output_table: Optional[LineageTable]
