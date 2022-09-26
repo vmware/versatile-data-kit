18a62fd83f29fa8fffc8b598501cfca8cb38c2d1
Momchil Z <mzhivkov@vmware.com>
2022-09-26 18:24:45 +0300

    vdk-core: add ability to cancel remaining job steps (#1188)
    what: Added the ability to cancel the remaining job/template steps through the job_input interface.
    
    why: This feature is required by users in certain cases e.g: if a data job depends on processing data from a source which has indicated no new entries since last run, then we can skip the execution.
    
    testing: Tested locally with data job/templates and added functional tests.
    
    Signed-off-by: Momchil Zhivkov mzhivkov@vmware.com

---
+++ projects/vdk-core/tests/functional/run/test_run_job_cancel.py
@@ -0,0 +1,31 @@
+# Copyright 2021 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+from click.testing import Result
+from functional.run import util
+from vdk.plugin.test_utils.util_funcs import cli_assert_equal
+from vdk.plugin.test_utils.util_funcs import CliEntryBasedTestRunner
+
+
+def test_run():
+    """
+    Test that runs a sample data job consisting of 3 steps.
+    Each step logs <Step 1,2> etc. However after the second step's log call
+    the cancel_job_execution method is invoked which should cancel
+    the job execution. Test checks:
+    execution is canceled and final step isn't executed.
+    proper exit code is set.
+    proper message is logged confirming job was cancelled from cancel_job_execution() method.
+    :return:
+    """
+    test_runner = CliEntryBasedTestRunner()
+
+    result: Result = test_runner.invoke(["run", util.job_path("cancel-job")])
+
+    assert "Step Cancel 1." in result.output
+    assert "Step Cancel 2." in result.output
+    assert "Step Cancel 3." not in result.output
+    assert (
+        "Job/template execution was skipped from job/template step code."
+        in result.output
+    )
+    cli_assert_equal(0, result)
