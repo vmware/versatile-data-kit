fc424ce4f9d82e344a8e07fe31d505ea24d4054e
ivakoleva <ikoleva@vmware.com>
2022-09-27 14:34:29 +0300

    vdk-properties-fs: new plugin for local FS properties storage (#1190)
    * vdk-properties-fs: new plugin for local FS properties storage
    
    We need a simplistic plugin, that allows a developer or presenter
    to quickly store secrets on the local FS during a dev/demo session,
    that does not require a prerequisite of the entire Control Service
    installed and running.
    
    Added a FS properties client, that uses a customizable dir and filename
    on the local file system to store the properties key-values. It does
    support multiple teams and data jobs, the properties of those are nested
    under a dedicated key for deduplication.
    JSON format is used, enabling nested data structures.
    
    Testing Done: added `test_fs_properties_plugin.py`, that covers multiple
    data jobs using matching keys, and default/customized directory and
    filename for file storage on the local FS.
    
    Signed-off-by: ivakoleva <iva.koleva@clearcode.bg>
    
    Signed-off-by: ivakoleva <iva.koleva@clearcode.bg>
    Co-authored-by: Gabriel Georgiev <45939426+gageorgiev@users.noreply.github.com>

---
+++ projects/vdk-plugins/vdk-properties-fs/setup.py
@@ -0,0 +1,34 @@
+# Copyright 2021 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+import pathlib
+
+import setuptools
+
+
+__version__ = "0.0.1"
+
+setuptools.setup(
+    name="vdk-properties-fs",
+    version=__version__,
+    url="https://github.com/vmware/versatile-data-kit",
+    description="Versatile Data Kit SDK plugin provides support for Properties API client that uses local FS storage.",
+    long_description=pathlib.Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    install_requires=["vdk-core"],
+    package_dir={"": "src"},
+    packages=setuptools.find_namespace_packages(where="src"),
+    include_package_data=True,
+    entry_points={
+        "vdk.plugin.run": [
+            "vdk-properties-fs = vdk.plugin.properties_fs.fs_properties_plugin"
+        ]
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
