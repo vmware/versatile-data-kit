4667f5479075a574469a2508a17e09a52e5499f5
murphp15 <murphp15@tcd.ie>
2022-10-05 21:27:46 +0100

    improve integration test docs and also re-enable assertion leading to flaxey tests.
    Signed-off-by: murphp15 <murphp15@tcd.ie>

--- projects/control-service/projects/pipelines_control_service/src/integration-test/java/com/vmware/taurus/datajobs/it/DataJobTerminationStatusIT.java
+++ projects/control-service/projects/pipelines_control_service/src/integration-test/java/com/vmware/taurus/datajobs/it/DataJobTerminationStatusIT.java
@@ -264,7 +264,11 @@ public class DataJobTerminationStatusIT extends BaseDataJobDeploymentIT {
                   objectMapper.readValue(
                       dataJobExecutionResult.getResponse().getContentAsString(),
                       DataJobExecution.class);
-
+              if(dataJobExecution.length<1){
+                log.info("No response from server");
+              }else{
+                log.info("Response from server  " + dataJobExecution[0].getStatus());
+              }
               return dataJobExecution[0] != null
                   && executionStatus.equals(dataJobExecution[0].getStatus());
             });
