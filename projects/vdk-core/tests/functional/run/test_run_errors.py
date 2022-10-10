7f6e273319ce8255606de0f7d98c3e2b38889ae5
Antoni Ivanov <aivanov@vmware.com>
2022-10-10 19:10:15 +0300

    vdk-core: errors occurred and the state (handled or not) context missing (#1212)
    Re submit the change -
    https://github.com/vmware/versatile-data-kit/pull/1182 - again. The only
    difference is that I renamed _get_eception_message to
    get_exception_message as not to break backwords compatability. I will
    merge this after
    
    --
    
    User code may trigger a SDK feature in a way that raises a user error.
    In such a use case, the error could be handled by the cautious user, in
    runtime. The VDK platform should be aware if user errors are handled
    in runtime or not, so that affects the data job termination status.
    
    The old errors.BLAMEES (experimental data structure for
    keeping track of responsibles for errors priorly logged) should be
    refactored:
    * a resolvable context abstraction introduced
    * it is memory-managed (singleton, reasoning documented)
    * populated by the 3 user-facing `errors.log_*()` methods - in favour of
    a method designed to populate ErrorMessage entries
    * keeps track if the actual exception, along with all properties like
    who is the actual respobsible persona (user or platform, that was
    free-form str before), resolved by a data job step or not, etc.
    
    Testing Done: added a functional test covering the
     `simple-query-failed-handled` scenario, added singleton verification
     for the resolvable context; ci/cd
    
    Signed-off-by: Antoni Ivanov <aivanov@vmware.com>

--- projects/vdk-core/tests/functional/run/test_run_errors.py
+++ projects/vdk-core/tests/functional/run/test_run_errors.py
@@ -14,9 +14,14 @@ from vdk.api.plugin.plugin_input import IPropertiesServiceClient
 from vdk.internal.builtin_plugins.connection.execution_cursor import ExecutionCursor
 from vdk.internal.builtin_plugins.run.job_context import JobContext
 from vdk.internal.core import errors
+from vdk.internal.core.errors import ResolvableByActual
 from vdk.plugin.test_utils.util_funcs import cli_assert_equal
 from vdk.plugin.test_utils.util_funcs import CliEntryBasedTestRunner
+from vdk.plugin.test_utils.util_plugins import DB_TYPE_SQLITE_MEMORY
+from vdk.plugin.test_utils.util_plugins import SqLite3MemoryDbPlugin
 from vdk.plugin.test_utils.util_plugins import TestPropertiesPlugin
+
+VDK_DB_DEFAULT_TYPE = "VDK_DB_DEFAULT_TYPE"
 
 
 @pytest.fixture(autouse=True)
@@ -33,7 +38,7 @@ def tmp_termination_msg_file(tmpdir) -> pathlib.Path:
 
 
 def test_initialize_step_user_error(tmp_termination_msg_file):
-    errors.clear_intermediate_errors()
+    errors.resolvable_context().clear()
     runner = CliEntryBasedTestRunner()
 
     result: Result = runner.invoke(["run", util.job_path("syntax-error-job")])
@@ -42,16 +47,22 @@ def test_initialize_step_user_error(tmp_termination_msg_file):
 
 
 def test_run_user_error(tmp_termination_msg_file):
-    errors.clear_intermediate_errors()
+    errors.resolvable_context().clear()
     runner = CliEntryBasedTestRunner()
 
     result: Result = runner.invoke(["run", util.job_path("fail-job")])
     cli_assert_equal(1, result)
     assert _get_job_status(tmp_termination_msg_file) == "User error"
 
+    assert errors.get_blamee_overall() == ResolvableByActual.USER
+    actual_error = errors.resolvable_context().resolvables.get(ResolvableByActual.USER)[
+        0
+    ]
+    assert actual_error.resolved is False
+
 
 def test_run_user_error_fail_job_library(tmp_termination_msg_file):
-    errors.clear_intermediate_errors()
+    errors.resolvable_context().clear()
     runner = CliEntryBasedTestRunner()
 
     result: Result = runner.invoke(["run", util.job_path("fail-job-indirect-library")])
@@ -60,7 +71,7 @@ def test_run_user_error_fail_job_library(tmp_termination_msg_file):
 
 
 def test_run_user_error_fail_job_ingest_iterator(tmp_termination_msg_file):
-    errors.clear_intermediate_errors()
+    errors.resolvable_context().clear()
     runner = CliEntryBasedTestRunner()
 
     result: Result = runner.invoke(["run", util.job_path("fail-job-ingest-iterator")])
@@ -69,7 +80,7 @@ def test_run_user_error_fail_job_ingest_iterator(tmp_termination_msg_file):
 
 
 def test_run_init_fails(tmp_termination_msg_file: pathlib.Path):
-    errors.clear_intermediate_errors()
+    errors.resolvable_context().clear()
 
     class InitFailsPlugin:
         @staticmethod
@@ -83,9 +94,15 @@ def test_run_init_fails(tmp_termination_msg_file: pathlib.Path):
     cli_assert_equal(1, result)
     assert _get_job_status(tmp_termination_msg_file) == "Platform error"
 
+    assert errors.get_blamee_overall() == ResolvableByActual.PLATFORM
+    actual_error = errors.resolvable_context().resolvables.get(
+        ResolvableByActual.PLATFORM
+    )[0]
+    assert actual_error.resolved is False
+
 
 def test_run_exception_handled(tmp_termination_msg_file: pathlib.Path):
-    errors.clear_intermediate_errors()
+    errors.resolvable_context().clear()
 
     class ExceptionHandler:
         @staticmethod
@@ -100,7 +117,7 @@ def test_run_exception_handled(tmp_termination_msg_file: pathlib.Path):
 
 
 def test_run_job_plugin_fails(tmp_termination_msg_file):
-    errors.clear_intermediate_errors()
+    errors.resolvable_context().clear()
 
     class RunJobFailsPlugin:
         @staticmethod
@@ -116,7 +133,7 @@ def test_run_job_plugin_fails(tmp_termination_msg_file):
 
 
 def test_run_platform_error_properties(tmp_termination_msg_file):
-    errors.clear_intermediate_errors()
+    errors.resolvable_context().clear()
 
     class FailingPropertiesServiceClient(IPropertiesServiceClient):
         def read_properties(self, job_name: str, team_name: str) -> Dict:
@@ -139,7 +156,7 @@ def test_run_platform_error_properties(tmp_termination_msg_file):
 
 
 def test_run_platform_error_sql(tmp_termination_msg_file):
-    errors.clear_intermediate_errors()
+    errors.resolvable_context().clear()
 
     class QueryFailingPlugin:
         @hookimpl
@@ -155,3 +172,16 @@ def test_run_platform_error_sql(tmp_termination_msg_file):
 
 def _get_job_status(tmp_termination_msg_file):
     return json.loads(tmp_termination_msg_file.read_text())["status"]
+
+
+@mock.patch.dict(os.environ, {VDK_DB_DEFAULT_TYPE: DB_TYPE_SQLITE_MEMORY})
+def test_user_error_handled(tmp_termination_msg_file):
+    errors.resolvable_context().clear()
+    db_plugin = SqLite3MemoryDbPlugin()
+    runner = CliEntryBasedTestRunner(db_plugin)
+
+    result: Result = runner.invoke(
+        ["run", util.job_path("simple-query-failed-handled")]
+    )
+    cli_assert_equal(0, result)
+    assert (json.loads(tmp_termination_msg_file.read_text())["status"]) == "Success"
