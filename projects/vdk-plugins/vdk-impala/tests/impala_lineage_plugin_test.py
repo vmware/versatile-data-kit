5f74d783f22c50a0451c831da67bb6733b55d158
Plamen Kostov <kostovw@gmail.com>
2022-10-18 12:39:22 +0300

    vdk-impala: Refactor query classifier for data lineage (#1239)
    vdk-impala: Refactor query classifier for data lineage
    
    # Why?
    Analysing the SQL query is not so trivial task, as there are number of variations of the DDL, DML and DDL with DML combinations. Current avoidance of single line comments does not work properly and skips queries that are eligible for lineage collection.
    
    # What?
    Added sqlparse library in order to externalise comment-cleaning task, now it leaves us only with the query and there isn't any need to analyse whole query. What we do now is we check if query starts with blacklisted keyword, if so - query is not eligible for lineage. Although some keywords blacklisted might contain lineage data, like CREATE statement, when table is created from another with SELECT statement. In this case blacklisted word needs to be whitelisted in some conditions.
    
    As sql statements are complex, we need to get which queries definitely has data lineage, instead of searching which query have for sure and for sure not data lineage. False positives queries for lineage, ie queries that does not have lineage information provided from the profile, but are marked as having one, will not break the logic at all, instead we optimize load on the impala server with a simple approach
    
    # How has this been tested?
    Unit and integration tests, also with production-grade queries
    
    # What type of change are you making?
    Bug-fixing
    
    Signed-off-by: Plamen Kostov <pkostov@vmware.com>
    Co-authored-by: Plamen Kostov <pkostov@vmware.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- projects/vdk-plugins/vdk-impala/tests/impala_lineage_plugin_test.py
+++ projects/vdk-plugins/vdk-impala/tests/impala_lineage_plugin_test.py
@@ -15,12 +15,12 @@ class ImpalaLineagePluginTest(unittest.TestCase):
     def test_get_lineage_table_from_table_name_none(self):
         self.assertIsNone(ImpalaLineagePlugin._get_lineage_table_from_table_name(None))
 
-    def test_is_query_have_lineage(self):
+    def test_does_query_have_lineage(self):
         self.assertTrue(
-            ImpalaLineagePlugin._is_query_have_lineage("SELECT * FROM table")
+            ImpalaLineagePlugin._does_query_have_lineage("SELECT * FROM table")
         )
         self.assertTrue(
-            ImpalaLineagePlugin._is_query_have_lineage(
+            ImpalaLineagePlugin._does_query_have_lineage(
                 "WITH temporaryTable(avgVal) as"
                 "(SELECT avg(Salary)"
                 "from Employee)"
@@ -30,14 +30,14 @@ class ImpalaLineagePluginTest(unittest.TestCase):
             )
         )
         self.assertTrue(
-            ImpalaLineagePlugin._is_query_have_lineage(
+            ImpalaLineagePlugin._does_query_have_lineage(
                 "-- job_name: a-job\n-- op_id: an-op\nINSERT "
                 "INTO TABLE schema.table  /* +SHUFFLE */\n "
                 "SELECT t1.* FROM schema.table"
             )
         )
         self.assertTrue(
-            ImpalaLineagePlugin._is_query_have_lineage(
+            ImpalaLineagePlugin._does_query_have_lineage(
                 "-- job_name: a-job\n"
                 "-- /* +SHUFFLE */ below is a query hint to "
                 "Impala. Do not remove!\n "
@@ -46,30 +46,178 @@ class ImpalaLineagePluginTest(unittest.TestCase):
             )
         )
 
-        self.assertFalse(ImpalaLineagePlugin._is_query_have_lineage("USE database;"))
+        self.assertFalse(ImpalaLineagePlugin._does_query_have_lineage("USE database;"))
         self.assertFalse(
-            ImpalaLineagePlugin._is_query_have_lineage("DROP TABLE table;")
+            ImpalaLineagePlugin._does_query_have_lineage("DROP TABLE table;")
         )
         self.assertFalse(
-            ImpalaLineagePlugin._is_query_have_lineage(
+            ImpalaLineagePlugin._does_query_have_lineage(
                 "select 1 -- testing if connection is alive."
             )
         )
         self.assertFalse(
-            ImpalaLineagePlugin._is_query_have_lineage(
+            ImpalaLineagePlugin._does_query_have_lineage(
                 "alter table d2.mobile rename to d3.mobile;"
             )
         )
         self.assertFalse(
-            ImpalaLineagePlugin._is_query_have_lineage(
+            ImpalaLineagePlugin._does_query_have_lineage(
                 "-- job_name: a-job\n"
                 "-- op_id: an-op\n"
                 "select 1 -- Testing if connection is alive."
             )
         )
         self.assertFalse(
-            ImpalaLineagePlugin._is_query_have_lineage(
-                "-- job_name: a-job\n" "-- op_id: an-op\n" "DESCRIBE schema.table"
+            ImpalaLineagePlugin._does_query_have_lineage(
+                "-- job_name: a-job\n" "-- op_id: an-op\n" " DESCRIBE schema.table"
+            )
+        )
+        self.assertFalse(
+            ImpalaLineagePlugin._does_query_have_lineage(
+                "-- job_name: a-job\n" "-- op_id: an-op\n" " REFRESH schema.table"
+            )
+        )
+        self.assertTrue(
+            ImpalaLineagePlugin._does_query_have_lineage(
+                "CREATE TABLE database_one.table_for_prod "
+                "STORED AS PARQUET AS SELECT * FROM  database_two.table_for_prod;"
+            )
+        )
+        self.assertTrue(
+            ImpalaLineagePlugin._does_query_have_lineage(
+                "CREATE TABLE database_one.table_for_prod "
+                "STORED AS PARQUET AS SELECT\n"
+                "* FROM  database_two.table_for_prod;"
+            )
+        )
+        self.assertTrue(
+            ImpalaLineagePlugin._does_query_have_lineage(
+                "CREATE TABLE database_one.table_for_prod "
+                "STORED AS PARQUET AS"
+                "WITH temporaryTable(avgVal) as"
+                "(SELECT avg(Salary)"
+                "from Employee)"
+                "SELECT EmployeeID,Name, Salary"
+                "FROM Employee, temporaryTable"
+                "WHERE Employee.Salary > temporaryTable.avgVal;"
+            )
+        )
+        self.assertFalse(
+            ImpalaLineagePlugin._does_query_have_lineage(
+                "CREATE TABLE database_one.table_for_prod " "STORED AS PARQUET"
+            )
+        )
+        self.assertFalse(
+            ImpalaLineagePlugin._does_query_have_lineage(
+                "\n"
+                "-- job_name: job-name\n"
+                "-- op_id: job-name-1665673200-v9nth\n"
+                "-- template: template-complex-name\n"
+                "COMPUTE STATS shop1.users;\n"
+            )
+        )
+
+        self.assertTrue(
+            ImpalaLineagePlugin._does_query_have_lineage(
+                "\n"
+                "/***\n"
+                "show create table database.incident;\n"
+                "\n"
+                "-- drop table if exists database.incident;\n"
+                "\n"
+                "-- invalidate metadata database.incident;\n"
+                "\n"
+                "CREATE TABLE database.incident\n"
+                "(\n"
+                "  id STRING,\n"
+                "  incident STRING,\n"
+                ")\n"
+                "STORED AS PARQUET\n"
+                "-- LOCATION check parquet files\n"
+                ";\n"
+                "***/\n"
+                "\n"
+                "with\n"
+                "---------------------\n"
+                "indent_tabs_enums\n"
+                "---------------------\n"
+                "as\n"
+                "(\n"
+                "  with\n"
+                "  -------------------------\n"
+                "  indent_tabs\n"
+                "  -------------------------\n"
+                "  as\n"
+                "  (\n"
+                "    select distinct trim( regexp_extract( reason, '\\d+', 0 )) issuenum\n"
+                "    from   database.tasks\n"
+                "    where  resource_type               in ('bundle' )\n"
+                "    and    start_time                  >= '2020-06-01'\n"
+                "    and    lower( trim( task_status )) in ( 'finished', 'succeeded' )\n"
+                "    and    user_name                   =  'auto' \n"
+                "  )\n"
+                ")\n"
+                "select    issue.id\n"
+                "where     issue.id > '2019-02-01'\n"
+                "                "
+            )
+        )
+        self.assertTrue(
+            ImpalaLineagePlugin._does_query_have_lineage(
+                "\n"
+                "-- job_name: just-a-job\n"
+                "-- op_id: just-a-job-1665673200-crhqw\n"
+                "-- template: vdk.templates.load.dimension\n"
+                "/* TO DO DROP AND RECREATE TARGET TABLE ON FULL RELOAD OR DATA TYPE CHANGE */\n"
+                "\n"
+                "-- DROP TABLE database_staging.a_table;\n"
+                "-- CREATE TABLE database_staging.a_table  STORED AS PARQUET \n"
+                "--    AS SELECT * FROM  database_production.a_table;\n"
+                "\n"
+                "-- /* +SHUFFLE */ below is a query hint to Impala. Do not remove!\n"
+                "-- See https://www.cloudera.com/documentation/enterprise/5-9-x/topics/impala_hints.html for details.\n"
+                "INSERT OVERWRITE TABLE database_staging.a_table  /* +SHUFFLE */\n"
+                "SELECT * FROM database_production.a_table;\n"
+            )
+        )
+        self.assertTrue(
+            ImpalaLineagePlugin._does_query_have_lineage(
+                "\n"
+                "/***\n"
+                "show create table database_one.table_incidents;\n"
+                "\n"
+                "-- drop table if exists database_one.table_incidents;\n"
+                "\n"
+                "CREATE TABLE database_one.table_incidents\n"
+                "(\n"
+                "  id             STRING,\n"
+                "  component      STRING,\n"
+                "  category_group STRING,\n"
+                "  category       STRING\n"
+                ")\n"
+                "STORED AS PARQUET\n"
+                ";\n"
+                "\n"
+                "***/\n"
+                "\n"
+                "---------------------------------------------------\n"
+                "insert overwrite table database_one.table_incidents\n"
+                "---------------------------------------------------\n"
+                "(\n"
+                "  id,\n"
+                "  component,\n"
+                "  category_group,\n"
+                "  category\n"
+                ")\n"
+                "select  isc.id\n"
+                ",       isc.component\n"
+                ",       'cat_group' category_group\n"
+                ",       cfo.customvalue    category\n"
+                "from    database_one.table_components isc\n"
+                "join    database_two.value cfv on  isc.id = cfv.issue\n"
+                "join    database_two.option cfo on cfv.f1 = cfo.f2\n"
+                "   and cfv.stringvalue = cfo.id    \n"
+                "where isc.component_group = 'Components' -- (6 of 12)\n"
             )
         )
 
