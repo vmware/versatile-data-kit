cd76ffe1fefad79ce3afcfd9356e7c5fb4caae67
murphp15 <murphp15@tcd.ie>
2022-11-07 20:05:49 +0000

    vdk-control-service: fix helm template
    Signed-off-by: murphp15 <murphp15@tcd.ie>

--- projects/control-service/cicd/test-testing-pipelines-service.sh
+++ projects/control-service/cicd/test-testing-pipelines-service.sh
@@ -37,25 +37,4 @@ helm dependency update --kubeconfig="$KUBECONFIG"
 #
 # image.tag is fixed during release. It is set here to deploy using latest change in source code.
 # We are using here embedded database, and we need to set the storageclass since in our test k8s no default storage class is not set.
-helm template "$RELEASE_NAME" . \
-      --set image.tag="$TAG" \
-      --set resources.limits.memory=2G \
-      --set credentials.repository="EMPTY" \
-      --set-file vdkOptions="$VDK_OPTIONS_SUBSTITUTED" \
-      --set deploymentGitUrl="$CICD_GIT_URI" \
-      --set deploymentGitUsername="$CICD_GIT_USER" \
-      --set deploymentGitPassword="$CICD_GIT_PASSWORD" \
-      --set uploadGitReadWriteUsername="$CICD_GIT_USER" \
-      --set uploadGitReadWritePassword="$CICD_GIT_PASSWORD" \
-      --set deploymentDockerRegistryType=generic \
-      --set deploymentDockerRegistryUsername="$CICD_CONTAINER_REGISTRY_USER_NAME" \
-      --set deploymentDockerRegistryPassword="$CICD_CONTAINER_REGISTRY_USER_PASSWORD" \
-      --set deploymentDockerRepository="$CICD_CONTAINER_REGISTRY_URI/cicd-data-jobs/$RELEASE_NAME" \
-      --set proxyRepositoryURL="$CICD_CONTAINER_REGISTRY_URI/cicd-data-jobs/$RELEASE_NAME" \
-      --set security.enabled=true \
-      --set security.oauth2.jwtJwkSetUri=https://console-stg.cloud.vmware.com/csp/gateway/am/api/auth/token-public-key?format=jwks \
-      --set security.oauth2.jwtIssuerUrl=https://gaz-preview.csp-vidm-prod.com \
-      --set security.authorizationEnabled=true \
-      --set security.authorization.webhookUri=https://httpbin.org/post \
-      --set extraEnvVars.LOGGING_LEVEL_COM_VMWARE_TAURUS=DEBUG \
-      --set extraEnvVars.DATAJOBS_TELEMETRY_WEBHOOK_ENDPOINT="https://vcsa.vmware.com/ph-stg/api/hyper/send?_c=taurus.v0&_i=cicd-control-service"
+helm template "$RELEASE_NAME" .
