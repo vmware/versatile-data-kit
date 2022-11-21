3e59c3e2b99cbc9c234acda8d1f719f8e753b23c
murphp15 <murphp15@tcd.ie>
2022-11-21 16:40:11 +0000

    vdk-pipelines-control-service: integration test Signed-off-by: murphp15 <murphp15@tcd.ie>

---
+++ projects/control-service/projects/pipelines_control_service/src/integration-test/java/com/vmware/taurus/datajobs/it/common/JobExecutionUtil.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2021 VMware, Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.vmware.taurus.datajobs.it.common;
+
+import java.time.OffsetDateTime;
+
+import com.vmware.taurus.service.JobExecutionRepository;
+import com.vmware.taurus.service.model.DataJob;
+import com.vmware.taurus.service.model.DataJobExecution;
+import com.vmware.taurus.service.model.ExecutionStatus;
+import com.vmware.taurus.service.model.ExecutionType;
+
+public class JobExecutionUtil {
+
+  public static DataJobExecution createDataJobExecution(
+      JobExecutionRepository jobExecutionRepository,
+      String executionId,
+      DataJob dataJob,
+      OffsetDateTime startTime,
+      OffsetDateTime endTime,
+      ExecutionStatus executionStatus) {
+
+    var jobExecution =
+        DataJobExecution.builder()
+            .id(executionId)
+            .dataJob(dataJob)
+            .startTime(startTime)
+            .endTime(endTime)
+            .type(ExecutionType.MANUAL)
+            .status(executionStatus)
+            .resourcesCpuRequest(1F)
+            .resourcesCpuLimit(2F)
+            .resourcesMemoryRequest(500)
+            .resourcesMemoryLimit(1000)
+            .message("message")
+            .lastDeployedBy("test_user")
+            .lastDeployedDate(OffsetDateTime.now())
+            .jobVersion("test_version")
+            .jobSchedule("*/5 * * * *")
+            .opId("test_op_id")
+            .vdkVersion("test_vdk_version")
+            .build();
+
+    return jobExecutionRepository.save(jobExecution);
+  }
+}
