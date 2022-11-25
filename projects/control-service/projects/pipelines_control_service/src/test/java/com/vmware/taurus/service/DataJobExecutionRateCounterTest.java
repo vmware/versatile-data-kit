abac10a26fd298815f67356fbde8158089ad9b8e
murphp15 <murphp15@tcd.ie>
2022-11-25 09:09:37 +0000

    control-service: cleanup tests
    1. Remove unused method
    2. don't support empty namespace
    
    Signed-off-by: murphp15 <murphp15@tcd.ie>

--- projects/control-service/projects/pipelines_control_service/src/test/java/com/vmware/taurus/service/DataJobExecutionRateCounterTest.java
+++ projects/control-service/projects/pipelines_control_service/src/test/java/com/vmware/taurus/service/DataJobExecutionRateCounterTest.java
@@ -17,6 +17,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.OffsetDateTime;
@@ -25,6 +26,7 @@ import java.util.List;
 
 @SpringBootTest(classes = ServiceApp.class)
 @ExtendWith(SpringExtension.class)
+@ActiveProfiles({"unittest"})
 public class DataJobExecutionRateCounterTest {
 
   @Autowired private JobExecutionRepository jobExecutionRepository;
