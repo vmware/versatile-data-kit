1befcb65bb0e449da6916801abf1b802be9574d0
murphp15 <murphp15@tcd.ie>
2022-11-25 10:08:20 +0000

    Revert "Revert "control-service: cleanup tests""
    This reverts commit c5fb8d05dc41e6706578d696fde38c8e7f49a824.

--- projects/control-service/projects/pipelines_control_service/src/test/java/com/vmware/taurus/service/JobExecutionFilterSpecIT.java
+++ projects/control-service/projects/pipelines_control_service/src/test/java/com/vmware/taurus/service/JobExecutionFilterSpecIT.java
@@ -16,11 +16,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.OffsetDateTime;
 import java.util.List;
 
 @SpringBootTest(classes = ControlplaneApplication.class)
+@ActiveProfiles({"unittest"})
 public class JobExecutionFilterSpecIT {
 
   @Autowired private JobsRepository jobsRepository;
--- projects/control-service/projects/pipelines_control_service/src/test/java/com/vmware/taurus/service/notification/EmailNotificationTest.java
+++ projects/control-service/projects/pipelines_control_service/src/test/java/com/vmware/taurus/service/notification/EmailNotificationTest.java
@@ -13,8 +13,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.vmware.taurus.ControlplaneApplication;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = ControlplaneApplication.class)
+@ActiveProfiles({"unittest"})
 public class EmailNotificationTest {
 
   @Autowired private EmailNotification emailNotification;
