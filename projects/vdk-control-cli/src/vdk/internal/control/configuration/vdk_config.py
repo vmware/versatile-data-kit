60ed6340cb821cf1b06b52570799c696e9d353fb
murphp15 <murphp15@tcd.ie>
2022-12-05 18:02:51 +0000

    vdk-control-cli: allow cli users to explicitly set the user agent tag
    Signed-off-by: murphp15 <murphp15@tcd.ie>

--- projects/vdk-control-cli/src/vdk/internal/control/configuration/vdk_config.py
+++ projects/vdk-control-cli/src/vdk/internal/control/configuration/vdk_config.py
@@ -50,7 +50,6 @@ class VDKConfig:
     def user_agent(self) -> str:
         """
         user agent that is used to make easier troubleshooting.
-
         This value won't appear in trace logs but will be included in events set to supercollider
         """
         return VDKConfig._user_agent
