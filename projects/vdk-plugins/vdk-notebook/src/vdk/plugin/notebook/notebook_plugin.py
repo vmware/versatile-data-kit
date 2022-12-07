5f58a028f9e94721c46ae002f762c236df8845f6
Duygu Hasan <duyguhsnhsn@gmail.com>
2022-12-07 17:25:11 +0200

    Add class for notebook plugin

--- projects/vdk-plugins/vdk-notebook/src/vdk/plugin/notebook/notebook_plugin.py
+++ projects/vdk-plugins/vdk-notebook/src/vdk/plugin/notebook/notebook_plugin.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 import logging
 
 from vdk.api.plugin.hook_markers import hookimpl
+from vdk.api.plugin.plugin_registry import IPluginRegistry
 from vdk.internal.builtin_plugins.run.job_context import JobContext
 from vdk.plugin.notebook.notebook import JobNotebookLocator
 from vdk.plugin.notebook.notebook import Notebook
@@ -12,11 +13,17 @@ from vdk.plugin.notebook.notebook import Notebook
 log = logging.getLogger(__name__)
 
 
-@hookimpl(trylast=True)
-def initialize_job(context: JobContext):
-    file_locator: JobNotebookLocator = JobNotebookLocator()
-    notebook_files = file_locator.get_notebook_files(context.job_directory)
-    if len(notebook_files) >= 1:
-        for file_path in notebook_files:
-            nb: Notebook = Notebook(file_path)
-            nb.extract_notebook_steps(context)
+class NotebookPlugin:
+    @hookimpl(trylast=True)
+    def initialize_job(self, context: JobContext):
+        file_locator: JobNotebookLocator = JobNotebookLocator()
+        notebook_files = file_locator.get_notebook_files(context.job_directory)
+        if len(notebook_files) >= 1:
+            for file_path in notebook_files:
+                nb: Notebook = Notebook(file_path)
+                nb.extract_notebook_steps(context)
+
+
+@hookimpl
+def vdk_start(plugin_registry: IPluginRegistry):
+    plugin_registry.load_plugin_with_hooks_impl(NotebookPlugin(), "notebook-plugin")
