3929c9cdda4ea81e7c2ef7c636bee6549cf70923
sbuldeev <sbuldeev@vmware.com>
2022-12-16 15:29:26 +0200

    vdk-impala/vdk-plugins: Introduce checks for scd1 template

--- projects/vdk-plugins/vdk-impala/src/vdk/plugin/impala/templates/load/dimension/scd1/00-dimension-scd1-definition.py
+++ projects/vdk-plugins/vdk-impala/src/vdk/plugin/impala/templates/load/dimension/scd1/00-dimension-scd1-definition.py
@@ -2,6 +2,8 @@
 # SPDX-License-Identifier: Apache-2.0
 from pydantic import BaseModel
 from vdk.api.job_input import IJobInput
+from collections.abc import Callable
+from typing import Optional
 from vdk.plugin.impala.templates.template_arguments_validator import (
     TemplateArgumentsValidator,
 )
@@ -12,6 +14,7 @@ class SlowlyChangingDimensionTypeOverwriteParams(BaseModel):
     target_table: str
     source_schema: str
     source_view: str
+    check: Optional[Callable[[str], bool]]
 
 
 class SlowlyChangingDimensionTypeOverwrite(TemplateArgumentsValidator):
