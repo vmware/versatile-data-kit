9e04bfeafd758a33f7742674ba481e4864d2d919
Duygu Hasan <duyguhsnhsn@gmail.com>
2023-01-05 14:37:58 +0200

    Add ipython extension

---
+++ projects/vdk-plugins/vdk-ipython-ext/src/vdk_ipython_ext.py
@@ -0,0 +1,44 @@
+import os
+import pathlib
+from pathlib import Path
+
+from IPython import get_ipython
+from IPython.core.magic import register_line_magic
+from vdk.internal.builtin_plugins.run.standalone_data_job import StandaloneDataJobFactory
+from IPython.core.magic_arguments import argument, magic_arguments, parse_argstring
+
+def load_ipython_extension(ipython):
+    """
+    IPython will look for this function specifically.
+    See https://ipython.readthedocs.io/en/stable/config/extensions/index.html
+    """
+    ipython.register_magic_function(magic_load_job, magic_name="intialize_vdk_job")
+
+
+@magic_arguments()
+@argument("-p", "--path", type=str, default=None)
+@argument("-a", "--arguments", type=str, default=None)
+@argument("-t", "--template", type=str, default=None)
+def magic_load_job(line: str):
+    """
+    You can use %initialize_vdk_job line magic within your Notebook to reload the job_input variable
+    for your current job
+    See more for line magic: https://ipython.readthedocs.io/en/stable/interactive/magics.html
+    """
+    args = parse_argstring(magic_load_job, line)
+    load_job(args.path, args.arguments, args.template)
+
+
+def load_job(path: str = None, arguments:str = None, template: str = None):
+    if path:
+        path = pathlib.Path(path)
+    with StandaloneDataJobFactory.create(
+            data_job_directory=path,
+            job_args=arguments,
+            template_name=template
+    ) as job_input:
+        get_ipython().push(
+            variables={
+                "job_input": job_input
+            }
+        )
---
+++ projects/vdk-plugins/vdk-ipython-ext/tests/jobs/job-using-a-plugin/10_dummy.py
@@ -0,0 +1,9 @@
+import logging
+
+from vdk.api.job_input import IJobInput
+
+log = logging.getLogger(__name__)
+
+
+def run(job_input: IJobInput):
+    log.info(f"Dummy arguments {job_input.get_arguments()}")
