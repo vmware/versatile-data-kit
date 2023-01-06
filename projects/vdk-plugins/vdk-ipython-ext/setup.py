50531c859474147661637d4035514e5818dcd66c
Duygu Hasan <duyguhsnhsn@gmail.com>
2023-01-06 13:41:51 +0200

    Change development status, delete unnedded testjobs, small changes on README

--- projects/vdk-plugins/vdk-ipython-ext/setup.py
+++ projects/vdk-plugins/vdk-ipython-ext/setup.py
@@ -4,9 +4,6 @@ import pathlib
 
 import setuptools
 
-"""
-Builds a package with the help of setuptools in order for this package to be imported in other projects
-"""
 
 __version__ = "0.1.0"
 
@@ -20,11 +17,9 @@ setuptools.setup(
     install_requires=["vdk-core"],
     package_dir={"": "src"},
     packages=setuptools.find_namespace_packages(where="src"),
-    # This is the only vdk plugin specifc part
-    # Define entry point called "vdk.plugin.run" with name of plugin and module to act as entry point.
     entry_points={"vdk.plugin.run": ["ipython-ext = vdk_ipython_ext"]},
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 2 - Pre-Alpha",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
