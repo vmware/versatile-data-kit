a86b75379b6f93758e331724161cec698f3bbb8c
Duygu Hasan <duyguhsnhsn@gmail.com>
2023-01-10 10:36:22 +0200

    fixes with naming and add iPython to setup dependencies

--- projects/vdk-plugins/vdk-ipython/setup.py
+++ projects/vdk-plugins/vdk-ipython/setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     description="Ipython extension for VDK",
     long_description=pathlib.Path("README.md").read_text(),
     long_description_content_type="text/markdown",
-    install_requires=["vdk-core"],
+    install_requires=["vdk-core", "iPython"],
     package_dir={"": "src"},
     packages=setuptools.find_namespace_packages(where="src"),
     entry_points={"vdk.plugin.run": ["ipython = vdk_ipython"]},
