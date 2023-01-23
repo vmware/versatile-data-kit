871ff4401095d3e84993f0ba6ca5c49cd89e3b4c
murphp15 <murphp15@tcd.ie>
2023-01-23 11:07:53 +0000

    Merge branch 'main' into person/murphp15/java_17

--- projects/control-service/projects/pipelines_control_service/src/integration-test/java/com/vmware/taurus/datajobs/it/common/MiniKdcCredentialsRepository.java
+++ projects/control-service/projects/pipelines_control_service/src/integration-test/java/com/vmware/taurus/datajobs/it/common/MiniKdcCredentialsRepository.java
@@ -6,12 +6,14 @@
 package com.vmware.taurus.datajobs.it.common;
 
 import com.vmware.taurus.service.credentials.KerberosCredentialsRepository;
-import lombok.Setter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.util.Optional;
 import org.apache.kerby.kerberos.kerb.server.SimpleKdcServer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
 
 /**
  * This class can be used to replace {@link KerberosCredentialsRepository} for testing purposes. It
@@ -21,14 +23,17 @@ import org.apache.kerby.kerberos.kerb.server.SimpleKdcServer;
  * (tsvetkovt@vmware.com) was not able to connect to MiniKdc or to a * dockerized KDC with kadmin.
  * TODO we should think about moving away from MiniKDC as it's not maintained.
  */
-@Setter
+@Component
+@Primary
 public class MiniKdcCredentialsRepository extends KerberosCredentialsRepository {
   private static final Logger log = LoggerFactory.getLogger(MiniKdcCredentialsRepository.class);
 
   private SimpleKdcServer miniKdc;
 
-  public MiniKdcCredentialsRepository() {
+  @Autowired
+  public MiniKdcCredentialsRepository(SimpleKdcServer miniKdc) {
     super("test", "test");
+    this.miniKdc = miniKdc;
   }
 
   @Override
