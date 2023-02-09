9b548d7196d8c9a0cc9757d454ca88f239be7bcf
Dilyan Marinov <mdilyan@vmware.com>
2023-02-09 14:15:31 +0200

    vdk/frontend: Open source shared components library
    https://jira.eng.vmware.com/browse/TAUR-5878
    
    What
    
    - replace references to taurus, supercollider, vmw, vmware and tms with vdk
    - change license to Apache-2.0
    - upade copyright headers to reflect license change
    - regenerate package-lock.json
    - remove e2e tests
    - change package versions to 0.0.1
    - update zone.js version to fix broken async test
    - remove author tag from docs
    
    How has this been tested
    
    Followed instructions to build and test here https://github.com/vmware/versatile-data-kit/blob/person/mdilyan/os-shared-components/projects/frontend/shared-components/gui/README.md
    
    https://user-images.githubusercontent.com/91800778/218074170-89ea2cca-1676-47b7-9df2-bad81d2b1ff9.mov
    
    What type of change are you making
    
    New feature, non-breaking
    
    Signed-off-by: Dilyan Marinov <mdilyan@vmware.com>

---
+++ projects/frontend/shared-components/gui/projects/shared/src/lib/commons/ngx-components/copy-to-clipboard-button/copy-to-clipboard-button.l10n.ts
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2021-2023 VMware, Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export const TRANSLATIONS = {
+    'en': {
+        'copied': 'Copied'
+    },
+    'de': {
+        'copied': 'Kopiert'
+    },
+    'es': {
+        'copied': 'Copiado'
+    },
+    'fr': {
+        'copied': 'Copié'
+    },
+    'ja': {
+        'copied': 'コピー'
+    },
+    'ko': {
+        'copied': '복사'
+    },
+    'zh_TW': {
+        'copied': '已复制'
+    },
+    'zh_CN': {
+        'copied': '已復制'
+    },
+    'it': {
+        'copied': 'Copiato'
+    },
+    'nl': {
+        'copied': 'Gekopieerd'
+    },
+    'pt': {
+        'copied': 'Copiado'
+    },
+    'ru': {
+        'copied': 'Скопировано'
+    },
+};
---
+++ projects/frontend/shared-components/gui/projects/shared/src/lib/commons/ngx-utils/simple-translate-service/simple-translate.pipe.ts
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2021-2023 VMware, Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { Pipe, PipeTransform } from '@angular/core';
+
+import { VdkSimpleTranslateService } from './simple-translate.service';
+
+@Pipe({
+    name: 'simpleTranslate',
+    pure: false,
+})
+export class VdkSimpleTranslatePipe implements PipeTransform {
+    constructor(private simpleTranslate: VdkSimpleTranslateService) {}
+
+    transform(text: string, ...args: any[]) {
+        return this.simpleTranslate.translate(text, ...args);
+    }
+}
