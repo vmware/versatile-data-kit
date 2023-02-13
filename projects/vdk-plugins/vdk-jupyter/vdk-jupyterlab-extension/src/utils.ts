beddad46e2a2e3e5fc68c6cfd37dd764bef75c18
Duygu Hasan <87015481+duyguHsnHsn@users.noreply.github.com>
2023-02-13 14:27:16 +0200

    vdk-jupyter: ui end-to-end testing (#1617)
    What:
    Introduced tests for currently existing ui components - vdk menu and its
    buttons.
    
    Why:
    Only manual tests were done so far.
    
    Signed-off-by: Duygu Hasan [hduygu@vmware.com](mailto:hduygu@vmware.com)
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- projects/vdk-plugins/vdk-jupyter/vdk-jupyterlab-extension/src/utils.ts
+++ projects/vdk-plugins/vdk-jupyter/vdk-jupyterlab-extension/src/utils.ts
@@ -1,12 +1,28 @@
 
 
 export function removeCreateJobDataFromSessionStorage(){
-    sessionStorage.removeItem('create-job-name')
-    sessionStorage.removeItem('create-job-team')
-    sessionStorage.removeItem('create-job-rest-api-url')
-    sessionStorage.removeItem('create-job-path')
-    sessionStorage.removeItem('local')
-    sessionStorage.removeItem('cloud')
+    sessionStorage.removeItem('create-job-name');
+    sessionStorage.removeItem('create-job-team');
+    sessionStorage.removeItem('create-job-rest-api-url');
+    sessionStorage.removeItem('create-job-path');
+    sessionStorage.removeItem('local');
+    sessionStorage.removeItem('cloud');
 }
 
-// TODO: add functions for removing data for other operations, too
+export function removeRunJobDataFromSessionStorage(){
+    sessionStorage.removeItem('job-path');
+    sessionStorage.removeItem('job-args');
+}
+
+export function removeDeleteJobDataFromSessionStorage(){
+    sessionStorage.removeItem('delete-job-name');
+    sessionStorage.removeItem('delete-job-team');
+    sessionStorage.removeItem('delete-job-rest-api-url');
+}
+
+export function removeDownloadJobDataFromSessionStorage(){
+    sessionStorage.removeItem('download-job-name');
+    sessionStorage.removeItem('download-job-team');
+    sessionStorage.removeItem('download-job-rest-api-url');
+    sessionStorage.removeItem('download-job-path');
+}
