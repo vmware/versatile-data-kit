23c90cb9bd8574bab27d4dd8fec4eb2cb72e7003
ivakoleva <iva.koleva@clearcode.bg>
2023-02-23 19:19:55 +0200

    frontend: data-pipelines build and test running CI/CD
    We need to build and run unit tests of data-pipelines library and
    ui application that uses it. The component is dependent on
    shared-components library.
    
    Added Bash scripts to
    (1) link if any shared-components rebuild was made and is about to be
    published, or otherwise default to latest already published one
    (2) build the data-pipelines library and link @vdk/data-pipelines
    (3) build the UI application
    (4) lint the subprojects to verify format
    (5) run the unit tests
    One bash script is meant to be used to satisfy build prerequisites,
    the other one to rebuild and relink continuously, needed during
    development.
    The proposed implementation triggers a data-pipelines rebuild (+test),
    upon any shared-components change, as well as data-pipelines changes.
    A few functional import fixes were made, to import the correctly renamed
    class.
    A README.md link fix to the VEP.
    
    Testing done: run the install script (that evaluates the build script)
    on a clean setup, and verified the unit tests were successful. Also,
    did start and browse the application manually, to check its operational.
    
    Signed-off-by: ivakoleva <iva.koleva@clearcode.bg>

---
+++ projects/frontend/cicd/build_data_pipelines.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -e
+
+# Copyright 2021-2023 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+###
+# For installing the build prerequisites, consider `install_data_pipelines.sh` script.
+# This script is meant to be used, to quickly
+# rebuild and link the the @vdk/data-pipelines in an already bootstrapped development setup.
+###
+
+if ! which npm >/dev/null 2>&1 ; then
+  echo "ERROR:"
+  echo "Please install npm 8.5.5+. Build cannot continue without it."
+  exit 1
+fi
+echo "Logging npm engines version..."
+npm version
+
+cd "$(dirname $0)" || exit 1
+cd ../data-pipelines/gui
+
+echo "Build the Data Pipelines Library ..."
+npm run build
+
+echo "Going to the dir of the data-pipelines build to create a symlink to the local repo ..."
+cd dist/data-pipelines
+
+echo "Link the library to local npm in order to test it ..."
+npm link
+
+echo "Going to the GUI root ..."
+cd ../../
+
+echo "Use the local repo symlink to link it to node_modules ..."
+npm link @vdk/data-pipelines
+
+echo "Logging global npm packages"
+npm ls -g
+
+echo "Logging local npm packages"
+npm ls
---
+++ projects/frontend/cicd/install_data_pipelines.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -e
+
+# Copyright 2021-2023 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+###
+# This script generates the dependencies tree, installs the packages needed,
+# evaluates the build_data_pipelines_sh script, then
+# lints them to verify format, builds the UI application and runs the unit tests.
+###
+
+if ! which npm >/dev/null 2>&1 ; then
+  echo "ERROR:"
+  echo "Please install npm 8.5.5+. Build cannot continue without it."
+  exit 1
+fi
+echo "Logging npm engines version..."
+npm version
+
+cd "$(dirname $0)" || exit 1
+cd ../data-pipelines/gui
+
+echo "Removing package lock if available..."
+rm -f package-lock.json
+
+shared_dist_dir="../../shared-components/gui/dist/shared"
+if [ -d "$shared_dist_dir" ]
+then
+  echo "Linking the shared-components dist rebuild found..."
+  npm link "$shared_dist_dir"
+else
+  echo "No shared-components dist rebuild found."
+fi
+
+echo "Install Dependencies..."
+npm install
+
+echo "Running linking script..."
+sh ../../cicd/link_data_pipelines.sh
+
+echo "Lint all projects & sub-projects..."
+npm run lint
+
+echo "Build the UI Wrapper Project"
+npm run build:ui
+
+echo "Test all projects & sub-projects ... (--watch=false to put exit code 0 after finishing)"
+npm run test:headless
