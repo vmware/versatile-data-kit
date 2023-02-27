99940fd7057c4901cef24e7d76f80f02ac53c606
Duygu Hasan <duyguhsnhsn@gmail.com>
2023-02-27 17:17:20 +0200

    Add new wrapper class for job initialization

--- projects/vdk-plugins/vdk-ipython/src/vdk_ipython.py
+++ projects/vdk-plugins/vdk-ipython/src/vdk_ipython.py
@@ -7,9 +7,28 @@ from IPython import get_ipython
 from IPython.core.magic_arguments import argument
 from IPython.core.magic_arguments import magic_arguments
 from IPython.core.magic_arguments import parse_argstring
-from vdk.internal.builtin_plugins.run.standalone_data_job import (
-    StandaloneDataJobFactory,
-)
+from vdk.internal.builtin_plugins.run import standalone_data_job
+
+
+class JobControl:
+    """
+    The main idea of thi class is to make it easier to  work with IStandaloneDataJob instances
+    Since the user will be using this class
+    """
+
+    def __init__(self, path: str = None, name: str = None, arguments: str = None, template: str = None):
+        path = pathlib.Path(path) if path else pathlib.Path(os.getcwd())
+        self.job = standalone_data_job.StandaloneDataJob(
+            name=name,
+            job_args=arguments,
+            template_name=template,
+            data_job_directory=path)
+
+    def get_initialized_job_input(self):
+        return self.job.__enter__()
+
+    def finalise(self):
+        self.job.__exit__(None, None, None)
 
 
 def load_ipython_extension(ipython):
@@ -17,9 +36,10 @@ def load_ipython_extension(ipython):
     IPython will look for this function specifically.
     See https://ipython.readthedocs.io/en/stable/config/extensions/index.html
     """
-    ipython.register_magic_function(magic_load_job, magic_name="reload_job_input")
+    ipython.register_magic_function(magic_load_job, magic_name="reload_data_job")
 
 
+# TODO: add exta-plugins option
 @magic_arguments()
 @argument("-p", "--path", type=str, default=None)
 @argument("-n", "--name", type=str, default=None)
@@ -27,8 +47,8 @@ def load_ipython_extension(ipython):
 @argument("-t", "--template", type=str, default=None)
 def magic_load_job(line: str):
     """
-    You can use %initialize_vdk_job line magic within your Notebook to reload the job_input variable
-    for your current job
+    You can use %initialize_data_job line magic within your Notebook to reload the data_job which is an instance of
+    JobControl. Using it you can get an initialized job_input variable and work with it.
     See more for line magic: https://ipython.readthedocs.io/en/stable/interactive/magics.html
     """
     args = parse_argstring(magic_load_job, line)
@@ -36,10 +56,7 @@ def magic_load_job(line: str):
 
 
 def load_job(
-    path: str = None, name: str = None, arguments: str = None, template: str = None
+        path: str = None, name: str = None, arguments: str = None, template: str = None
 ):
-    path = pathlib.Path(path) if path else pathlib.Path(os.getcwd())
-    with StandaloneDataJobFactory.create(
-        data_job_directory=path, name=name, job_args=arguments, template_name=template
-    ) as job_input:
-        get_ipython().push(variables={"job_input": job_input})
+    job = JobControl(path, name, arguments, template)
+    get_ipython().push(variables={"data_job": job})
