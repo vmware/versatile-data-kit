469513ce5e8355a32acd6d4cb6158a2b068f5fcd
murphp15 <murphp15@tcd.ie>
2023-03-21 15:52:03 +0000

    vdk-core: add an ingestor plugin which handles iterating over all value
    Signed-off-by: murphp15 <murphp15@tcd.ie>

--- examples/ingest-and-anonymize-plugin/plugins/vdk-poc-anonymize/src/vdk/plugin/anonymize/ingester_plugin.py
+++ examples/ingest-and-anonymize-plugin/plugins/vdk-poc-anonymize/src/vdk/plugin/anonymize/ingester_plugin.py
@@ -4,9 +4,8 @@ import logging
 from typing import Any
 from typing import Dict
 from typing import List
-from typing import Optional
-from typing import Tuple
 
+from vdk.api.plugin.column_transformation_ingester_plugin import ColumnLevelTransformer
 from vdk.api.plugin.plugin_input import IIngesterPlugin
 from vdk.plugin.anonymize.anonymizer import Anonymizer
 
@@ -20,15 +19,14 @@ Ingester Plugins are implemented by inheriting IIngesterPlugin and impelemented 
 See IIngesterPlugin docs for more info.
 """
 
-
-class AnonymizationIngesterPlugin(IIngesterPlugin):
+class AnonymizationIngesterPlugin(ColumnLevelTransformer):
     def __init__(
         self, anonymization_fields: Dict[str, List[str]], anonymizer: Anonymizer
     ) -> None:
         self._anonymization_fields = anonymization_fields
         self._anonymizer = anonymizer
 
-    def _anonymize_if_needed(self, destination_table: str, key: str, value: Any):
+    def transform(self, destination_table: str, key: str, value: Any):
         table = destination_table if destination_table else ""
         if (
             key
@@ -38,22 +36,3 @@ class AnonymizationIngesterPlugin(IIngesterPlugin):
             return self._anonymizer.anonymize(value)
         else:
             return value
-
-    # inherited
-    def pre_ingest_process(
-        self,
-        payload: List[dict],
-        destination_table: Optional[str] = None,
-        target: Optional[str] = None,
-        collection_id: Optional[str] = None,
-        metadata: IngestionMetadata = None,
-    ) -> Tuple[List[Dict], Optional[IngestionMetadata]]:
-        if self._anonymization_fields:
-            payload = [
-                {
-                    k: self._anonymize_if_needed(destination_table, k, v)
-                    for (k, v) in item.items()
-                }
-                for item in payload
-            ]
-        return payload, metadata
