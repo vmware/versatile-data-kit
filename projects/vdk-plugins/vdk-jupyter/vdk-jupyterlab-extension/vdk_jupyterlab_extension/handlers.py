818f63657c07cff16b56b7843744d33298b26f81
Duygu Hasan <duyguhsnhsn@gmail.com>
2023-03-27 15:39:45 +0300

    add VdkUi mmethod for deployment

--- projects/vdk-plugins/vdk-jupyter/vdk-jupyterlab-extension/vdk_jupyterlab_extension/handlers.py
+++ projects/vdk-plugins/vdk-jupyter/vdk-jupyterlab-extension/vdk_jupyterlab_extension/handlers.py
@@ -127,7 +127,7 @@ class CreateJobHandler(APIHandler):
 class CreateDeploymentHandler(APIHandler):
     """
     Class responsible for handling POST request for creating a deployment of  Data Job given its name, team, path,
-    Rest API URL, deployment reason and flag whether it is enabled (that wil basically un-pause the job)
+    Rest API URL, deployment reason and flag whether it is enabled (that will basically un-pause the job)
     Response: return a json formatted str including:
         ::error field with error message if an error exists
         ::message field with status of the Vdk operation
@@ -137,19 +137,22 @@ class CreateDeploymentHandler(APIHandler):
     def post(self):
         input_data = self.get_json_body()
         try:
-            # add vdkUi operation
-            self.finish(json.dumps({"message": f"", "error": ""}))
+            status = VdkUI.create_deployment(
+                input_data[VdkOption.NAME.value],
+                input_data[VdkOption.TEAM.value],
+                input_data[VdkOption.REST_API_URL.value],
+                input_data[VdkOption.PATH.value],
+                input_data[VdkOption.DEPLOYMENT_REASON.value],
+                input_data[VdkOption.DEPLOY_ENABLE.value],
+            )
+            self.finish(json.dumps({"message": f"{status}", "error": ""}))
         except Exception as e:
             self.finish(json.dumps({"message": f"{e}", "error": "true"}))
 
 
 def setup_handlers(web_app):
     host_pattern = ".*$"
-
     base_url = web_app.settings["base_url"]
-    run_job_route_pattern = url_path_join(base_url, "vdk-jupyterlab-extension", "run")
-    run_job_handlers = [(run_job_route_pattern, RunJobHandler)]
-    web_app.add_handlers(host_pattern, run_job_handlers)
 
     def add_handler(handler, endpoint):
         job_route_pattern = url_path_join(
