0d4dcce32e1c7d560f98cf6cea8e23ccc1be91c9
Gabriel Georgiev <gageorgiev@vmware.com>
2023-03-28 21:48:48 +0300

    remove todo
    Signed-off-by: Gabriel Georgiev <gageorgiev@vmware.com>

--- projects/control-service/projects/pipelines_control_service/src/main/java/com/vmware/taurus/service/execution/JobExecutionService.java
+++ projects/control-service/projects/pipelines_control_service/src/main/java/com/vmware/taurus/service/execution/JobExecutionService.java
@@ -102,8 +102,6 @@ public class JobExecutionService {
           jobExecutionRequest.getStartedBy().equals("vdk-control-cli")
               ? ExecutionType.MANUAL.getValue()
               : ExecutionType.SCHEDULED.getValue());
-      // TODO: we need to extract "vdk-control-cli" as a global string as it currently appears here
-      // and in the execution API
 
       Map<String, String> envs = new LinkedHashMap<>();
       envs.put(JobEnvVar.VDK_OP_ID.getValue(), opId);
