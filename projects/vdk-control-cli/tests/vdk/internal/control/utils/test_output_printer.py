8d0316f55d50489db39a3a5b05f3434561ce0f0e
Antoni Ivanov <aivanov@vmware.com>
2023-03-30 00:47:55 +0300

    vdk-control-cli: refactor output printing with printer class
    Currently if a command needs to print similar types of data in multiple
    places
    or formats, the developer would have to duplicate the printing code for
    each location or format. This would lead to a lot of redundant code,
    which is difficult to maintain and prone to errors. This is making the
    code more "DRY".
    
    Also, users and devs are limited to a fixed set of output formats
    provided by the application. This could be restricting if the devs needs
    to print data in a format that is not supported by the application.
    E.g I wanted to add `rich` or `streamlit` type potentially.
    
    By introducing support for customizable output formats with the Printer
    class and related functions, users can define their own output formats
    and register them with the application using the printer decorator. This
    allows users to print data in any format they desire, providing greater
    flexibility and customization options.
    
    Testing Done: unit tests (incl new ones)
    
    Signed-off-by: Antoni Ivanov <aivanov@vmware.com>

---
+++ projects/vdk-control-cli/tests/vdk/internal/control/utils/test_output_printer.py
@@ -0,0 +1,98 @@
+# Copyright 2023-2023 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+from typing import Any
+from typing import Dict
+from typing import List
+from unittest.mock import patch
+
+import pytest
+from vdk.internal.control.utils import output_printer
+from vdk.internal.control.utils.output_printer import _PrinterJson
+from vdk.internal.control.utils.output_printer import _PrinterText
+from vdk.internal.control.utils.output_printer import create_printer
+from vdk.internal.control.utils.output_printer import Printer
+
+
+class TestPrinterText:
+    def test_print_dict(self):
+        with patch("click.echo") as mock_echo:
+            printer = _PrinterText()
+            data = {"key": "value"}
+
+            printer.print_dict(data)
+
+            expected_output = '{\n  "key": "value"\n}'
+            mock_echo.assert_called_once_with(expected_output)
+
+    def test_print_table_with_data(self):
+        with patch("click.echo") as mock_echo:
+            printer = _PrinterText()
+
+            data = [{"key1": "value1", "key2": 2}, {"key1": "value3", "key2": 4}]
+
+            printer.print_table(data)
+
+            expected_output = (
+                "╒════════╤════════╕\n"
+                "│ key1   │   key2 │\n"
+                "╞════════╪════════╡\n"
+                "│ value1 │      2 │\n"
+                "├────────┼────────┤\n"
+                "│ value3 │      4 │\n"
+                "╘════════╧════════╛"
+            )
+            mock_echo.assert_called_once_with(expected_output)
+
+    def test_print_table_with_no_data(self):
+        with patch("click.echo") as mock_echo:
+            printer = _PrinterText()
+            data = []
+
+            printer.print_table(data)
+
+            expected_output = "No Data Jobs."
+            mock_echo.assert_called_once_with(expected_output)
+
+
+class TestPrinterJson:
+    def test_print_dict(self):
+        with patch("click.echo") as mock_echo:
+            printer = _PrinterJson()
+
+            data = {"key": "value"}
+
+            printer.print_dict(data)
+
+            expected_output = '{\n  "key": "value"\n}'
+            mock_echo.assert_called_once_with(expected_output)
+
+    def test_print_table(self):
+        with patch("click.echo") as mock_echo:
+            printer = _PrinterJson()
+            data = [
+                {"key1": "value1", "key2": "value2"},
+                {"key1": "value3", "key2": "value4"},
+            ]
+            printer.print_table(data)
+
+            expected_output = '[\n  {\n    "key1": "value1",\n    "key2": "value2"\n  },\n  {\n    "key1": "value3",\n    "key2": "value4"\n  }\n]'
+            mock_echo.assert_called_once_with(expected_output)
+
+
+class TestCreatePrinter:
+    def test_create_printer_with_registered_format(self):
+        class MockPrinter(Printer):
+            def print_dict(self, data: Dict[str, Any]) -> None:
+                pass
+
+            def print_table(self, data: List[Dict[str, Any]]) -> None:
+                pass
+
+        output_printer._registered_printers["mock"] = MockPrinter
+
+        printer = create_printer("mock")
+        assert isinstance(printer, MockPrinter)
+
+    def test_create_printer_with_unregistered_format(self):
+        with pytest.raises(ValueError):
+            create_printer("invalid_format")
