ffab105b4af5ee819ea8a35803d7134b3b05edb7
ddakov <ddakov@vmware.com>
2023-04-04 20:11:24 +0300

    control-service: expose supported python versions
    Add unit test.
    
    Signed-off-by: Dako Dakov <ddakov@vmware.com>

---
+++ projects/control-service/projects/pipelines_control_service/src/test/java/com/vmware/taurus/datajobs/DataJobsServiceControllerTest.java
@@ -0,0 +1,49 @@
+package com.vmware.taurus.datajobs;
+
+import com.vmware.taurus.controlplane.model.data.DataJobApiInfo;
+import com.vmware.taurus.service.UserAgentService;
+import com.vmware.taurus.service.deploy.SupportedPythonVersions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class DataJobsServiceControllerTest {
+
+    @Test
+    public void testInfo() {
+        // Mock dependencies
+        UserAgentService userAgentService = Mockito.mock(UserAgentService.class);
+        SupportedPythonVersions supportedPythonVersions = Mockito.mock(SupportedPythonVersions.class);
+
+        // Create instance of DataJobsServiceController
+        DataJobsServiceController controller = new DataJobsServiceController(userAgentService, supportedPythonVersions);
+
+        // Prepare test data
+        Set<String> supportedVersions = new HashSet<>();
+        supportedVersions.add("python3.6");
+        supportedVersions.add("python3.7");
+        supportedVersions.add("python3.8");
+        supportedVersions.add("python3.9");
+        when(supportedPythonVersions.getSupportedPythonVersions()).thenReturn(supportedVersions);
+        when(userAgentService.getUserAgentDetails()).thenReturn("1.0.0");
+
+        // Call the API method
+        ResponseEntity<DataJobApiInfo> responseEntity = controller.info("myteam");
+
+        // Verify the response status code
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+
+        // Verify the response body
+        DataJobApiInfo response = responseEntity.getBody();
+        assertEquals("1.0.0", response.getApiVersion());
+        assertEquals(new ArrayList<>(supportedVersions), response.getSupportedPythonVersions());
+    }
+}
