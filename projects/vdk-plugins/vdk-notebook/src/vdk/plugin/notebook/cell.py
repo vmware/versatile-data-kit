4db29aefb56a459a1d34d650a80acbaaa5f25f77
Duygu Hasan <87015481+duyguHsnHsn@users.noreply.github.com>
2023-04-06 15:00:05 +0300

    vdk-notebook: add hook for saving error information into json file (#1842)
    What:
    Added a hook that saves error information from job_steps into error.json
    file.
    Also, for more informative error logs caused from cells from .ipynb
    file, added the id of the failing cell to the logs. (which will be used
    later from the Jupyter UI for marking the cell)
    
    Why: The main reason for the changes is that in the Jupyter UI we want
    to introduce dialogs for job run status which consist of shorter message
    for errors (not all of the logs). But in general, it is a good option to
    have a place where the reason for the failure is kept (the user does not
    have to go over all of the logs to see which step is failing).
    
    
    Signed-off-by: Duygu Hasan [hduygu@vmware.com](mailto:hduygu@vmware.com)
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- projects/vdk-plugins/vdk-notebook/src/vdk/plugin/notebook/cell.py
+++ projects/vdk-plugins/vdk-notebook/src/vdk/plugin/notebook/cell.py
@@ -7,8 +7,11 @@ from dataclasses import dataclass
 class Cell:
     """
     A class that represents Jupyter code cell
+    See Jupyter cell in this json schema:
+             https://github.com/jupyter/nbformat/blob/main/nbformat/v4/nbformat.v4.schema.json
     """
 
     def __init__(self, jupyter_cell):
         self.tags = jupyter_cell["metadata"].get("tags", {})
         self.source = "".join(jupyter_cell["source"])
+        self.id = jupyter_cell["id"]
