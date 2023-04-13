6498dfb2f01c0b2c1a91ea76b34cb1d423db15a6
Duygu Hasan <duyguhsnhsn@gmail.com>
2023-04-13 16:01:52 +0300

    refactor code according to codacy warnings

--- projects/vdk-plugins/vdk-jupyter/vdk-jupyterlab-extension/src/vdkTags.ts
+++ projects/vdk-plugins/vdk-jupyter/vdk-jupyterlab-extension/src/vdkTags.ts
@@ -6,15 +6,15 @@
 import { INotebookTracker } from '@jupyterlab/notebook';
 import { IThemeManager } from '@jupyterlab/apputils';
 
-function addNumberElement(number: Number, node: Element): void {
+const addNumberElement = (number: Number, node: Element) => {
   const numberElement = document.createElement('div');
   numberElement.innerText = String(number);
   numberElement.classList.add('jp-vdk-cell-num');
 
   node.appendChild(numberElement);
-}
+};
 
-function addVdkLogo(node: Element): void {
+const addVdkLogo = (node: Element) => {
   const logo = document.createElement('img');
   logo.setAttribute(
     'src',
@@ -25,16 +25,16 @@ function addVdkLogo(node: Element): void {
 
   logo.classList.add('jp-vdk-logo');
   node.appendChild(logo);
-}
+};
 
-function addVdkCellDesign(
+const addVdkCellDesign = (
   currentCell: Element,
   cells: Element[],
   vdkCellIndices: Array<Number>,
   themeManager: IThemeManager
-): void {
+) => {
   // Delete previous numbering in case of untagging elements
-  let vdkCellNums = Array.from(
+  const vdkCellNums = Array.from(
     document.getElementsByClassName('jp-vdk-cell-num')
   );
   vdkCellNums.forEach(element => {
@@ -42,7 +42,9 @@ function addVdkCellDesign(
   });
 
   // Delete previously added logo in case of untagging elements
-  let vdkCellLogo = Array.from(document.getElementsByClassName('jp-vdk-logo'));
+  const vdkCellLogo = Array.from(
+    document.getElementsByClassName('jp-vdk-logo')
+  );
   vdkCellLogo.forEach(element => {
     element.remove();
   });
@@ -50,7 +52,10 @@ function addVdkCellDesign(
   let vdkCellCounter = 0;
   for (let i = 0; i < cells.length; i++) {
     if (vdkCellIndices.includes(i)) {
-      if (themeManager.isLight(themeManager.theme?.toString()!)) {
+      if (
+        themeManager.theme &&
+        themeManager.isLight(themeManager.theme.toString())
+      ) {
         cells[i].classList.remove('jp-vdk-cell-dark');
         cells[i].classList.add('jp-vdk-cell');
       } else {
@@ -66,18 +71,26 @@ function addVdkCellDesign(
       cells[i].classList.remove('jp-vdk-cell-dark');
     }
   }
-}
+};
 
-export function trackVdkTags(
+export const trackVdkTags = (
   notebookTracker: INotebookTracker,
   themeManager: IThemeManager
-): void {
+): void => {
   const changeCells = () => {
-    if (notebookTracker.currentWidget?.model?.cells.get(0)) {
+    if (
+      notebookTracker.currentWidget &&
+      notebookTracker.currentWidget.model &&
+      notebookTracker.currentWidget.model.cells.length !== 0
+    ) {
       // Get indices of the vdk cells using cell metadata
-      let vdkCellIndices = [];
+      const vdkCellIndices = [];
       let cellIndex = 0;
-      while (notebookTracker.currentWidget?.model?.cells.get(cellIndex)) {
+      while (
+        notebookTracker.currentWidget &&
+        notebookTracker.currentWidget.model &&
+        notebookTracker.currentWidget.model.cells.get(cellIndex)
+      ) {
         let currentCellTags = notebookTracker.currentWidget?.model?.cells
           .get(cellIndex)
           .metadata.get('tags')! as ReadonlyArray<String>;
@@ -86,7 +99,11 @@ export function trackVdkTags(
         cellIndex++;
       }
 
-      if (notebookTracker.activeCell?.parent?.node.children) {
+      if (
+        notebookTracker.activeCell &&
+        notebookTracker.activeCell.parent &&
+        notebookTracker.activeCell.parent.node.children
+      ) {
         addVdkCellDesign(
           notebookTracker.activeCell.node,
           Array.from(notebookTracker.activeCell?.parent?.node.children!),
@@ -99,4 +116,4 @@ export function trackVdkTags(
 
   notebookTracker.activeCellChanged.connect(changeCells);
   themeManager.themeChanged.connect(changeCells);
-}
+};
