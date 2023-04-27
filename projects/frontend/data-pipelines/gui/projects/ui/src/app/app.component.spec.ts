79f26c862fa8da0f6e9abe7746ce48717f32e202
ivakoleva <ikoleva@vmware.com>
2023-04-27 17:00:07 +0300

    frontend: Toggleable auth (#1958)
    We need a feature flag for skipping OAuth2, so the frontend can 
    operate with a Control Service where security is disabled. It is the
    default use case when having quickstart-vdk for local demo or dev
    installation.
    
    This is a stacked PR onto auth configurations organized from
    https://github.com/vmware/versatile-data-kit/pull/1957
    Introduced authSkipped flag defaulting to false, due security standards.
    The top right corner with username and logout dropdown are rendered 
    in case auth is enabled.
    
    Testing done: verified both cases locally; added testing
    
    ---------
    
    Signed-off-by: ivakoleva <iva.koleva@clearcode.bg>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- projects/frontend/data-pipelines/gui/projects/ui/src/app/app.component.spec.ts
+++ projects/frontend/data-pipelines/gui/projects/ui/src/app/app.component.spec.ts
@@ -19,25 +19,55 @@ import {
     UrlOpenerService
 } from '@versatiledatakit/shared';
 
-import { AppConfig } from './app-config.model';
-
 import { AppConfigService } from './app-config.service';
 
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
     let routerServiceStub: jasmine.SpyObj<RouterService>;
-    let oAuthServiceStub: jasmine.SpyObj<OAuthService>;
     let navigationServiceStub: jasmine.SpyObj<NavigationService>;
     let viewContainerRefStub: jasmine.SpyObj<ViewContainerRef>;
     let dynamicComponentsServiceStub: jasmine.SpyObj<DynamicComponentsService>;
     let confirmationServiceStub: jasmine.SpyObj<ConfirmationService>;
     let urlOpenerServiceStub: jasmine.SpyObj<UrlOpenerService>;
     let appConfigServiceStub: jasmine.SpyObj<AppConfigService>;
+    const configureTestingModule = (providersAdditional: any[]) => {
+        const providersBase = [
+            UrlHelperService,
+            { provide: NavigationService, useValue: navigationServiceStub },
+            { provide: RouterService, useValue: routerServiceStub },
+            { provide: ViewContainerRef, useValue: viewContainerRefStub },
+            { provide: DynamicComponentsService, useValue: dynamicComponentsServiceStub },
+            { provide: ConfirmationService, useValue: confirmationServiceStub },
+            { provide: UrlOpenerService, useValue: urlOpenerServiceStub },
+            { provide: AppConfigService, useValue: appConfigServiceStub }
+        ];
+        TestBed.configureTestingModule({
+            schemas: [NO_ERRORS_SCHEMA],
+            declarations: [AppComponent],
+            imports: [],
+            providers: providersBase.concat(providersAdditional)
+        });
+    };
 
     beforeEach(() => {
         routerServiceStub = jasmine.createSpyObj<RouterService>('routerService', ['getState']);
-        oAuthServiceStub = jasmine.createSpyObj<OAuthService>('oAuthService', [
+        navigationServiceStub = jasmine.createSpyObj<NavigationService>('navigationService', ['initialize']);
+        appConfigServiceStub = jasmine.createSpyObj<AppConfigService>('appConfigService', [
+            'getConfig',
+            'getAuthCodeFlowConfig',
+            'getSkipAuth'
+        ]);
+        viewContainerRefStub = jasmine.createSpyObj<ViewContainerRef>('viewContainerRefStub', ['createComponent']);
+        dynamicComponentsServiceStub = jasmine.createSpyObj<DynamicComponentsService>('dynamicComponentsServiceStub', ['initialize']);
+        confirmationServiceStub = jasmine.createSpyObj<ConfirmationService>('confirmationServiceStub', ['initialize']);
+        urlOpenerServiceStub = jasmine.createSpyObj<UrlOpenerService>('urlOpenerServiceStub', ['initialize']);
+        routerServiceStub.getState.and.returnValue(new Subject());
+    });
+
+    it('should create the app with auth enabled', () => {
+        appConfigServiceStub.getSkipAuth.and.returnValue(false);
+        const oAuthServiceStub = jasmine.createSpyObj<OAuthService>('oAuthService', [
             'configure',
             'loadDiscoveryDocumentAndLogin',
             'getAccessTokenExpiration',
@@ -46,41 +76,19 @@ describe('AppComponent', () => {
             'getIdToken',
             'getIdentityClaims'
         ]);
-        navigationServiceStub = jasmine.createSpyObj<NavigationService>('navigationService', ['initialize']);
-        viewContainerRefStub = jasmine.createSpyObj<ViewContainerRef>('viewContainerRefStub', ['createComponent']);
-        dynamicComponentsServiceStub = jasmine.createSpyObj<DynamicComponentsService>('dynamicComponentsServiceStub', ['initialize']);
-        confirmationServiceStub = jasmine.createSpyObj<ConfirmationService>('confirmationServiceStub', ['initialize']);
-        urlOpenerServiceStub = jasmine.createSpyObj<UrlOpenerService>('urlOpenerServiceStub', ['initialize']);
-        appConfigServiceStub = jasmine.createSpyObj<AppConfigService>('appConfigService', ['getConfig', 'getAuthCodeFlowConfig']);
-
-        routerServiceStub.getState.and.returnValue(new Subject());
         oAuthServiceStub.getIdentityClaims.and.returnValue({});
         oAuthServiceStub.loadDiscoveryDocumentAndLogin.and.returnValue(Promise.resolve(true));
         oAuthServiceStub.getAccessTokenExpiration.and.returnValue(0);
         oAuthServiceStub.refreshToken.and.returnValue(Promise.resolve({} as TokenResponse));
-        appConfigServiceStub.getConfig.and.returnValue({} as AppConfig);
+        configureTestingModule([{ provide: OAuthService, useValue: oAuthServiceStub }]);
 
-        TestBed.configureTestingModule({
-            schemas: [NO_ERRORS_SCHEMA],
-            declarations: [AppComponent],
-            imports: [],
-            providers: [
-                UrlHelperService,
-                { provide: OAuthService, useValue: oAuthServiceStub },
-                { provide: NavigationService, useValue: navigationServiceStub },
-                { provide: RouterService, useValue: routerServiceStub },
-                { provide: ViewContainerRef, useValue: viewContainerRefStub },
-                { provide: DynamicComponentsService, useValue: dynamicComponentsServiceStub },
-                { provide: ConfirmationService, useValue: confirmationServiceStub },
-                { provide: UrlOpenerService, useValue: urlOpenerServiceStub },
-                { provide: AppConfigService, useValue: appConfigServiceStub }
-            ]
-        });
-    });
-
-    it('should create the app', () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
         expect(app).toBeTruthy();
+    });
+
+    it('should create the app with auth skipped', () => {
+        appConfigServiceStub.getSkipAuth.and.returnValue(true);
+        configureTestingModule([]);
     });
 });
