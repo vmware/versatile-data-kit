22b799fb003cd01ebc7c35687bcafe40c7c455be
murphp15 <murphp15@tcd.ie>
2023-05-05 09:59:54 +0100

    control-service: remove unused parameters
    Signed-off-by: murphp15 <murphp15@tcd.ie>

--- projects/control-service/projects/pipelines_control_service/src/test/java/com/vmware/taurus/datajobs/MockKubernetes.java
+++ projects/control-service/projects/pipelines_control_service/src/test/java/com/vmware/taurus/datajobs/MockKubernetes.java
@@ -89,12 +89,8 @@ public class MockKubernetes {
         .createCronJob(
             anyString(),
             anyString(),
-            any(),
             anyString(),
             anyBoolean(),
-            any(),
-            any(),
-            any(),
             any(),
             any(),
             any(),
