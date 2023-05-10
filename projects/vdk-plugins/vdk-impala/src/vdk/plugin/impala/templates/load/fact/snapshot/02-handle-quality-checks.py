10dacf068249f1296633f6ade2c2d2b30e3951e7
sbuldeev <sbuldeev@vmware.com>
2023-05-10 16:13:20 +0300

    Introduce checks for snapshot template and utility for the processing templates
    What:
    -Adding functionality to handle the snapshot template behavior if there are checks provided by the user
    -Adding utility.py file to be used by all processing templates that have quality checks implemented
    -Reworked scd1 template to use utility.py file
    
    Why:
    It is linked to https://github.com/vmware/versatile-data-kit/issues/1361
    
    Tests: provided positive and negative regression tests
    
    Signed-off-by: Stefan Buldeev sbuldeev@vmware.com

--- projects/vdk-plugins/vdk-impala/src/vdk/plugin/impala/templates/load/fact/snapshot/02-handle-quality-checks.py
+++ projects/vdk-plugins/vdk-impala/src/vdk/plugin/impala/templates/load/fact/snapshot/02-handle-quality-checks.py
@@ -25,7 +25,9 @@ def run(job_input: IJobInput):
     source_view = job_arguments.get("source_view")
     target_schema = job_arguments.get("target_schema")
     target_table = job_arguments.get("target_table")
+    last_arrival_ts = job_arguments.get("last_arrival_ts")
     insert_query = get_file_content(SQL_FILES_FOLDER,"02-insert-into-target.sql")
+    overwrite_target_query = get_file_content(SQL_FILES_FOLDER,"02-overwrite-target.sql")
 
     if check:
         staging_schema = job_arguments.get("staging_schema", target_schema)
@@ -38,25 +40,29 @@ def run(job_input: IJobInput):
         )
 
         insert_into_staging = insert_query.format(
-            target_schema=staging_schema,
-            target_table=staging_table_name,
+            current_target_schema=staging_schema,
+            current_target_table=staging_table_name,
+            target_schema=target_schema,
+            target_table=target_table,
             _vdk_template_insert_partition_clause=partition_clause,
             source_schema=source_schema,
             source_view=source_view,
+            last_arrival_ts=last_arrival_ts
         )
         job_input.execute_query(insert_into_staging)
 
         if check(staging_table):
-            insert_into_target = insert_query.format(
-                source_schema=staging_schema,
-                source_view=staging_table_name,
+            insert_into_target = overwrite_target_query.format(
+                staging_schema=staging_schema,
+                staging_table_name=staging_table_name,
                 _vdk_template_insert_partition_clause=partition_clause,
                 target_schema=target_schema,
-                target_table=target_table,
+                target_table=target_table
             )
             job_input.execute_query(insert_into_target)
         else:
             raise Exception("The data is not passing the quality checks!")
 
     else:
+        insert_query = insert_query.replace('{current_target_schema}.{current_target_table}', f'{target_schema}.{target_table}')
         job_input.execute_query(insert_query)
--- projects/vdk-plugins/vdk-impala/tests/functional/jobs/load_fact_snapshot_template_job/02_run_load_fact_snapshot_template.py
+++ projects/vdk-plugins/vdk-impala/tests/functional/jobs/load_fact_snapshot_template_job/02_run_load_fact_snapshot_template.py
@@ -9,6 +9,20 @@ __copyright__ = (
 
 
 def run(job_input: IJobInput) -> None:
+    def sample_check_true(tmp_table_name):
+        return True
+
+    def sample_check_false(tmp_table_name):
+        return False
+
+    template_args = job_input.get_arguments()
+    check = template_args.get("check")
+
+    if check == "use_positive_check":
+        template_args["check"] = sample_check_true
+    elif check == "use_negative_check":
+        template_args["check"] = sample_check_false
+    
     job_input.execute_template(
         template_name="load/fact/snapshot",
         template_args=job_input.get_arguments(),
