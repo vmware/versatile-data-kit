45e9614d2dab935339be15be5d568f2726043939
sbuldeev <sbuldeev@vmware.com>
2023-05-19 14:06:52 +0300

    Extract testing functionality to a test utility file

--- projects/vdk-plugins/vdk-impala/tests/functional/jobs/load_fact_snapshot_template_job/02_run_load_fact_snapshot_template.py
+++ projects/vdk-plugins/vdk-impala/tests/functional/jobs/load_fact_snapshot_template_job/02_run_load_fact_snapshot_template.py
@@ -1,6 +1,7 @@
 # Copyright 2021-2023 VMware, Inc.
 # SPDX-License-Identifier: Apache-2.0
 from vdk.api.job_input import IJobInput
+from tests.functional import test_utility
 
 __author__ = "VMware, Inc."
 __copyright__ = (
@@ -9,20 +10,12 @@ __copyright__ = (
 
 
 def run(job_input: IJobInput) -> None:
-    def sample_check_true(tmp_table_name):
-        return True
-
-    def sample_check_false(tmp_table_name):
-        return False
 
     template_args = job_input.get_arguments()
     check = template_args.get("check")
+    check = test_utility.setup_testing_check(check)
 
-    if check == "use_positive_check":
-        template_args["check"] = sample_check_true
-    elif check == "use_negative_check":
-        template_args["check"] = sample_check_false
     job_input.execute_template(
         template_name="load/fact/snapshot",
-        template_args=job_input.get_arguments(),
+        template_args=template_args,
     )
