cb4f1a7db5890f1f327f4fbef9d6efe52bb08f21
murphp15 <murphp15@tcd.ie>
2023-05-23 10:56:30 +0100

    control-service: print result body
    Signed-off-by: murphp15 <murphp15@tcd.ie>

--- projects/control-service/projects/pipelines_control_service/src/integration-test/java/com/vmware/taurus/datajobs/it/common/DataJobDeploymentExtension.java
+++ projects/control-service/projects/pipelines_control_service/src/integration-test/java/com/vmware/taurus/datajobs/it/common/DataJobDeploymentExtension.java
@@ -135,15 +135,22 @@ public class DataJobDeploymentExtension
               getClass().getClassLoader().getResourceAsStream(JOB_SOURCE_PATH + jobSource));
 
       // Upload the data job
-      MvcResult uploadResult =
-          mockMvc
+      MvcResult uploadResult = mockMvc
               .perform(
-                  post(String.format("/data-jobs/for-team/%s/jobs/%s/sources", TEAM_NAME, jobName))
-                      .with(user(USER_NAME))
-                      .content(jobZipBinary)
-                      .contentType(MediaType.APPLICATION_OCTET_STREAM))
-              .andExpect(status().isOk())
-              .andReturn();
+                      post(String.format("/data-jobs/for-team/%s/jobs/%s/sources", TEAM_NAME, jobName))
+                              .with(user(USER_NAME))
+                              .content(jobZipBinary)
+                              .contentType(MediaType.APPLICATION_OCTET_STREAM)).andReturn();
+
+
+      if (uploadResult.getResponse().getStatus() != 200) {
+        throw new Exception(
+                "status is "
+                        + uploadResult.getResponse().getStatus()
+                        + "\nbody is "
+                        + uploadResult.getResponse().getContentAsString());
+      }
+
       DataJobVersion dataJobVersion =
           MAPPER.readValue(uploadResult.getResponse().getContentAsString(), DataJobVersion.class);
 
