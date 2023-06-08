39dbac3bc49f798146a3628b007b1a3714218771
Miroslav Ivanov <40535952+mivanov1988@users.noreply.github.com>
2023-06-08 11:40:53 +0300

    control-service: produce secure base job images for python 3.8-3.11 (#2208)
    # Why
    We don't have an easy way to produce secure base job images for multiple
    Python versions.
    
    # What
    Refactored the secure base image docker file to support installing
    different Python versions. Added multiple steps for building and
    publishing numerous Python versions.
    
    # Testing done
    Local execution of build script and through the CI/CD. 
    
    -
    https://hub.docker.com/r/versatiledatakit/data-job-base-python-3.8-secure
    -
    https://hub.docker.com/r/versatiledatakit/data-job-base-python-3.9-secure
    -
    https://hub.docker.com/r/versatiledatakit/data-job-base-python-3.10-secure
    -
    https://hub.docker.com/r/versatiledatakit/data-job-base-python-3.11-secure
    
    Signed-off-by: miroslavi@vmware.com
    
    ---------
    
    Signed-off-by: Stefan miroslavi@vmware.com
    Signed-off-by: miroslavi@vmware.com

--- projects/control-service/projects/job-base-image-secure/publish-job-base.sh
+++ projects/control-service/projects/job-base-image-secure/publish-job-base.sh
@@ -8,15 +8,21 @@ VERSION_TAG="${VERSION_TAG:-"0.1dev"}"
 VDK_DOCKER_REGISTRY_URL=${VDK_DOCKER_REGISTRY_URL:-"registry.hub.docker.com/versatiledatakit"}
 
 function build_and_push_image() {
-    name="$1"
-    docker_file="$2"
+    PYTHON_MAJOR=$1
+    PYTHON_MINOR=$2
+    PYTHON_PATCH=$3
+    name="data-job-base-python-$PYTHON_MAJOR.$PYTHON_MINOR-secure"
+    docker_file="Dockerfile-data-job-base"
 
     image_repo="$VDK_DOCKER_REGISTRY_URL/$name"
     image_tag_local="$image_repo:local"
     image_tag_version="$image_repo:$VERSION_TAG"
     image_tag_latest="$image_repo:latest"
 
-    docker build -t "$image_tag_local" -f "$SCRIPT_DIR/$docker_file" "$SCRIPT_DIR"
+    docker build -t "$image_tag_local" -f "$SCRIPT_DIR/$docker_file" "$SCRIPT_DIR" \
+    --build-arg PYTHON_MAJOR=$PYTHON_MAJOR \
+    --build-arg PYTHON_MINOR=$PYTHON_MINOR \
+    --build-arg PYTHON_PATCH=$PYTHON_PATCH
 
     docker-slim build \
     --target "$image_tag_local" \
@@ -38,6 +44,10 @@ function build_and_push_image() {
     docker push "$image_tag_latest"
 }
 
-build_and_push_image \
-    "data-job-base-python-3.10-secure" \
-    Dockerfile-data-job-base
+build_and_push_image 3 8 16
+
+build_and_push_image 3 9 16
+
+build_and_push_image 3 10 11
+
+build_and_push_image 3 11 3
