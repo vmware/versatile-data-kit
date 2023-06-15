0d637e5c353b30bdaca81b05c3aeadce9d566a3b
Paul Murphy <murphp15@tcd.ie>
2023-06-15 16:03:05 +0300

    control-service: data jobs points to correct namespace (#2268)
    # Why
    BUG FIX: in a recent PR I made a change which points the data jobs
    deployer to the contrl namespace.
    I need to merge the fix asap. 
    
    
    I will add a test shortly. But I want to merge without a test to fix
    this ASAP.
    
    
    
    Signed-off-by: murphp15 <murphp15@tcd.ie>
    
    ---------
    
    Co-authored-by: github-actions <>

--- projects/control-service/projects/pipelines_control_service/src/main/java/com/vmware/taurus/service/kubernetes/ControlKubernetesService.java
+++ projects/control-service/projects/pipelines_control_service/src/main/java/com/vmware/taurus/service/kubernetes/ControlKubernetesService.java
@@ -9,7 +9,6 @@ import com.vmware.taurus.service.KubernetesService;
 import com.vmware.taurus.service.deploy.JobCommandProvider;
 import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.openapi.apis.BatchV1Api;
-import io.kubernetes.client.openapi.apis.BatchV1beta1Api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -29,15 +28,7 @@ public class ControlKubernetesService extends KubernetesService {
       @Value("${datajobs.control.k8s.k8sSupportsV1CronJob}") boolean k8sSupportsV1CronJob,
       @Qualifier("controlApiClient") ApiClient client,
       @Qualifier("controlBatchV1Api") BatchV1Api batchV1Api,
-      @Qualifier("controlBatchV1beta1Api") BatchV1beta1Api batchV1beta1Api,
       JobCommandProvider jobCommandProvider) {
-    super(
-        namespace,
-        k8sSupportsV1CronJob,
-        log,
-        client,
-        batchV1Api,
-        batchV1beta1Api,
-        jobCommandProvider);
+    super(namespace, k8sSupportsV1CronJob, log, client, batchV1Api, null, jobCommandProvider);
   }
 }
