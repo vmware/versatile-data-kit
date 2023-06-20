7c0337d1e7999686c240068d7d3f63d3916cbe7f
ddakov <ddakov@vmware.com>
2023-06-20 19:54:07 +0300

    control-service: secrets service integration test
    Add an integration test for the secrets service.
    Revert dependabot changes to spring-vault version.
    
    Signed-off-by: Dako Dakov <ddakov@vmware.com>

--- projects/control-service/projects/pipelines_control_service/src/integration-test/java/com/vmware/taurus/datajobs/it/VaultJobSecretsServiceIT.java
+++ projects/control-service/projects/pipelines_control_service/src/integration-test/java/com/vmware/taurus/datajobs/it/VaultJobSecretsServiceIT.java
@@ -31,6 +31,7 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest(
+<<<<<<< HEAD
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
     classes = ControlplaneApplication.class)
 @Testcontainers
@@ -94,3 +95,71 @@ public class VaultJobSecretsServiceIT extends BaseIT {
 // vaultContainer.execInContainer("vault","kv","list", "secret")
 // vaultContainer.execInContainer("vault","kv","list", "secret")
 // vaultContainer.execInContainer("vault", "kv", "put", "secret/test", "db_name=postgres")
+=======
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        classes = ControlplaneApplication.class)
+@Testcontainers
+public class VaultJobSecretsServiceIT extends BaseIT {
+
+    @Container
+    private static final VaultContainer vaultContainer = new VaultContainer<>("vault:1.0.2")
+            .withVaultToken("root");
+    private static VaultJobSecretsService vaultJobSecretService;
+
+    @BeforeAll
+    public static void init() throws URISyntaxException {
+        String vaultUri = vaultContainer.getHttpHostAddress();
+
+        VaultEndpoint vaultEndpoint = VaultEndpoint.from(new URI(vaultUri));
+        TokenAuthentication clientAuthentication = new TokenAuthentication("root");
+
+        VaultTemplate vaultTemplate = new VaultTemplate(vaultEndpoint, clientAuthentication);
+
+        vaultJobSecretService = new VaultJobSecretsService(vaultTemplate);
+    }
+
+
+    @Test
+    public void testGetEmptyDataJobSecrets() throws Exception {
+        Map<String, Object> result = vaultJobSecretService.readJobSecrets("testJob");
+        Assertions.assertEquals(Collections.emptyMap(), result);
+    }
+
+
+    @Test
+    public void testSetDataJobSecrets() throws Exception {
+        Map<String, Object> secrets = new HashMap<>();
+        secrets.put("key1", "value1");
+
+        vaultJobSecretService.updateJobSecrets("testJob2", secrets);
+
+        Map<String, Object> readResult = vaultJobSecretService.readJobSecrets("testJob2");
+        Assertions.assertEquals(secrets, readResult);
+    }
+
+    @Test
+    void testUpdateJobSecretsLimit() throws JsonProcessingException {
+        Map<String, Object> secrets = new HashMap<>();
+        secrets.put("key1", "value1");
+
+        vaultJobSecretService.updateJobSecrets("testJob2", secrets);
+
+
+        Map<String, Object> largeSecrets = new HashMap<>();
+        largeSecrets.put("key1", null);
+        largeSecrets.put("key2", RandomStringUtils.randomAlphabetic(1025 * 1025));
+
+
+        assertThrows(
+                DataJobSecretsSizeLimitException.class,
+                () -> vaultJobSecretService.updateJobSecrets("testJob2", largeSecrets));
+
+        // check secrets were not updated
+        Map<String, Object> readResult = vaultJobSecretService.readJobSecrets("testJob2");
+        Assertions.assertEquals(secrets, readResult);
+    }
+}
+//vaultContainer.execInContainer("vault","kv","list", "secret")
+//vaultContainer.execInContainer("vault","kv","list", "secret")
+//vaultContainer.execInContainer("vault", "kv", "put", "secret/test", "db_name=postgres")
+>>>>>>> aa20f0b2 (control-service: secrets service integration test)
