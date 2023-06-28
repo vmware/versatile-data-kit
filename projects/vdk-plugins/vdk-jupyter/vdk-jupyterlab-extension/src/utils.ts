913ddefe6c168f7ce6a7951b01b1ba3bc7983663
Duygu Hasan <duyguhsnhsn@gmail.com>
2023-06-28 15:45:41 +0300

    change formatting in utils.ts

--- projects/vdk-plugins/vdk-jupyter/vdk-jupyterlab-extension/src/utils.ts
+++ projects/vdk-plugins/vdk-jupyter/vdk-jupyterlab-extension/src/utils.ts
@@ -2,7 +2,6 @@
  * Copyright 2021-2023 VMware, Inc.
  * SPDX-License-Identifier: Apache-2.0
  */
-
 export const TRANSFORM_JOB = 'Transform Job';
 export const RUN_JOB = 'Run Job';
 export const CREATE_DEP = 'Create Deployment';
