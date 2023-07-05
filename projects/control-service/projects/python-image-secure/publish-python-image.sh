21ecea2c8e509ce6119f0ec6e15683079bbcc0af
Miroslav Ivanov <40535952+mivanov1988@users.noreply.github.com>
2023-07-05 16:21:31 +0300

    control-service: split build job base image CI/CD step (#2348)
    Split the CI/CD step for Python and base job image publishing
    due to the long-running execution (more than an hour).
    Also, added native Oracle dependencies to the base image
    which are needed for oracledb in Thick mode.
    
    Testing Done:
    
    https://gitlab.com/vmware-analytics/versatile-data-kit/-/pipelines/921583615
    
    Signed-off-by: Miroslav Ivanov miroslavi@vmware.com
    
    ---------
    
    Signed-off-by: Miroslav Ivanov miroslavi@vmware.com

---
+++ projects/control-service/projects/python-image-secure/publish-python-image.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Copyright 2021-2023 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+VERSION_TAG="${VERSION_TAG:-"0.1dev"}"
+VDK_DOCKER_REGISTRY_URL=${VDK_DOCKER_REGISTRY_URL:-"registry.hub.docker.com/versatiledatakit"}
+
+PYTHON_MAJOR=$1
+PYTHON_MINOR=$2
+PYTHON_PATCH=$3
+python_name="python-$PYTHON_MAJOR.$PYTHON_MINOR-secure"
+python_docker_file="Dockerfile-python"
+
+python_image_repo="$VDK_DOCKER_REGISTRY_URL/$python_name"
+python_image_tag_version="$python_image_repo:$VERSION_TAG"
+python_image_tag_latest="$python_image_repo:latest"
+
+docker build -t "$python_image_tag_version" -t "$python_image_tag_latest" -f "$SCRIPT_DIR/$python_docker_file" "$SCRIPT_DIR" \
+--build-arg PYTHON_MAJOR=$PYTHON_MAJOR \
+--build-arg PYTHON_MINOR=$PYTHON_MINOR \
+--build-arg PYTHON_PATCH=$PYTHON_PATCH
+
+docker push "$python_image_tag_version"
+docker push "$python_image_tag_latest"
