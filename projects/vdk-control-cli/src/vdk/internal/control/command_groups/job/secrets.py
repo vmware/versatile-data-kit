43061f857c972dd9204c8deedfdf6dd2bbe9b09b
ddakov <ddakov@vmware.com>
2023-07-05 09:25:37 +0300

    vdk-control-cli: add vdk secrets command
    review feedback.
    
    Signed-off-by: Dako Dakov <ddakov@vmware.com>

--- projects/vdk-control-cli/src/vdk/internal/control/command_groups/job/secrets.py
+++ projects/vdk-control-cli/src/vdk/internal/control/command_groups/job/secrets.py
@@ -59,9 +59,9 @@ class JobSecrets:
 
     @staticmethod
     def _to_bool(value: str) -> bool:
-        if value == "true" or value == "True":
+        if value.lower() == "true":
             return True
-        if value == "false" or value == "False":
+        if value.lower() == "false":
             return False
         raise ValueError("bool cast accept only True/true/False/false values.")
 
