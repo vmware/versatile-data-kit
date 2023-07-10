a3616bb1e421e0d991a35e81f1ea2a41fca1cd57
Goran Kokinovski <gkokinovski@vmware.com>
2023-07-10 14:44:20 +0300

    frontend: Fix router event handling in base class
    Fix router event handling in DataJobsBaseGridComponent.
    Distinguish between Browser popped state and changed Team.i
    Added new SystemEvent with id 'SE_Location_Change' bound to
    constant SE_LOCATION_CHANGE that is publicly exposed
    through '@versatiledatakit/shared'.
     - with the new core SystemEvent there is provided
    interface for its payload
     - event is fired whenever method locationToURL
    in URLStateManager is invoked
    and condition allows location change
    Small refactor, introduced interface for SystemEvent
    with id 'SE_Navigate' payload.
    
    Signed-off-by: Goran Kokinovski <gkokinovski@vmware.com>

--- projects/frontend/shared-components/gui/projects/shared/src/lib/core/system-events/event/models/event.codes.ts
+++ projects/frontend/shared-components/gui/projects/shared/src/lib/core/system-events/event/models/event.codes.ts
@@ -3,15 +3,28 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { NavigationExtras } from '@angular/router';
+
 /**
  * ** System Event ID for navigation trigger.
  *
  *   - Send event [BLOCKING]
  *   - Every Handler should return Promise.
  *
- *   - Payload {url: string | string[], extras?: NavigationExtras}
+ *   - Payload {@link SystemEventNavigatePayload}
  */
 export const SE_NAVIGATE = 'SE_Navigate';
+
+/**
+ * ** System Event ID for location change through {@link @angular/common/Location}.
+ *
+ *   - Post event [NON-BLOCKING]
+ *   - Every Handler could either consume event as void (return nothing) or return Promise.
+ *   - Execution is in queue using setTimeout of 0.
+ *
+ *   - Payload {@link SystemEventLocationChangePayload}
+ */
+export const SE_LOCATION_CHANGE = 'SE_Location_Change';
 
 /**
  * ** System Event that could be consumed by Handlers.
@@ -24,3 +37,45 @@ export const SE_NAVIGATE = 'SE_Navigate';
  *   - Payload {any}
  */
 export const SE_ALL_EVENTS = '*';
+
+// events payload types
+
+/**
+ * ** Payload send whenever {@link SE_NAVIGATE} event is fired.
+ */
+export interface SystemEventNavigatePayload {
+    url: string | string[];
+    extras?: NavigationExtras;
+}
+
+/**
+ * ** Payload post whenever {@link SE_LOCATION_CHANGE} event is fired.
+ */
+export interface SystemEventLocationChangePayload {
+    /**
+     * ** Url in string format.
+     *
+     *      - e.g. '/pathname/path-param_1/path-param_2?query-param-1=value_1&query-param-2=value_2'
+     */
+    url: string;
+    /**
+     * ** Dynamic path params in key-value map format.
+     */
+    params: { [key: string]: string };
+    /**
+     * ** Dynamic path params serialized in string format.
+     *
+     *      - e.g. '/pathname/path-param_1/path-param_2'
+     */
+    paramsSerialized: string;
+    /**
+     * ** Dynamic query params in key-value map format.
+     */
+    queryParams: { [key: string]: string };
+    /**
+     * ** Dynamic query params serialized in string format.
+     *
+     *      - e.g. 'query-param-1=value_1&query-param-2=value_2'
+     */
+    queryParamsSerialized: string;
+}
