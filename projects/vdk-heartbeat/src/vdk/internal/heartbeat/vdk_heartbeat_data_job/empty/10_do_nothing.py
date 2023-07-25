ffdee8bac07e3718299eb10e024c7319179b3b93
Antoni Ivanov <aivanov@vmware.com>
2023-07-25 16:12:29 +0300

    vdk-heartbeat: cover requirements.txt automatic installs (#2393)
    Upon `vdk deploy` the Control Service is responsible for installing all
    external dependecies defined in requirements.txt of the data job so they
    can be used during a cloud run.
    
    A recent outage in one of our customers caused data jobs to ignore their
    requirements.txt and not install any external libraries. It was trace to
    be due to a release of [data job base
    
    image](https://hub.docker.com/layers/versatiledatakit/data-job-base-python-3.11)
    
    CICD of the open source VDK did not catch this. 
    
    I am extending VDK heartbeat to test for the issue so we can catch it in
    the future.
     
    See also https://github.com/vmware/versatile-data-kit/issues/2391

--- projects/vdk-heartbeat/src/vdk/internal/heartbeat/vdk_heartbeat_data_job/empty/10_do_nothing.py
+++ projects/vdk-heartbeat/src/vdk/internal/heartbeat/vdk_heartbeat_data_job/empty/10_do_nothing.py
@@ -2,10 +2,15 @@
 # SPDX-License-Identifier: Apache-2.0
 import logging
 
+from faker import Faker
 from vdk.api.job_input import IJobInput
 
 log = logging.getLogger(__name__)
 
 
 def run(job_input: IJobInput):
+    # this is just added to test that external libraries are installed and imported correctly from requirements.txt
+    fake = Faker()
+    log.info(f"Fake name: {fake.name()}")
+
     log.info(f"Empty job that just logs and exits")
--- projects/vdk-heartbeat/src/vdk/internal/heartbeat/vdk_heartbeat_data_job/simple/10_simple.py
+++ projects/vdk-heartbeat/src/vdk/internal/heartbeat/vdk_heartbeat_data_job/simple/10_simple.py
@@ -2,15 +2,20 @@
 # SPDX-License-Identifier: Apache-2.0
 import logging
 
+from faker import Faker
 from vdk.api.job_input import IJobInput
 
 log = logging.getLogger(__name__)
 
 
 def run(job_input: IJobInput):
+    # this is just added to test that external libraries are installed and imported correctly from requirements.txt
+    fake = Faker()
+    log.info(f"Fake name: {fake.name()}")
+
     log.info(f"Get current properties ")
     props = job_input.get_all_properties()
     props["succeeded"] = "true"
-    log.info(f"Save new properties ")
+    log.info(f"Save new properties {props}")
     job_input.set_all_properties(props)
     log.info(f"Updated property now to {props['succeeded']}")
