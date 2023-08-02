cab2f5129402c26d44b38ad4690cbc34248d4799
Yoan Salambashev <yoan.g.salambashev@gmail.com>
2023-08-02 06:06:53 +0300

    vdk-jupyter: fix typo
    Signed-off-by: Yoan Salambashev <ysalambashev@vmware.com>

--- projects/vdk-plugins/vdk-jupyter/vdk-jupyterlab-extension/src/components/StatusButton.tsx
+++ projects/vdk-plugins/vdk-jupyter/vdk-jupyterlab-extension/src/components/StatusButton.tsx
@@ -17,7 +17,7 @@ export class StatusButton {
 
   constructor(commands: CommandRegistry) {
     this.buttonElement = document.createElement('button');
-    this.buttonElement.innerHTML = CHECK_STATUS_BUTTON_LABEL + ' 00:00:00'
+    this.buttonElement.innerHTML = CHECK_STATUS_BUTTON_LABEL + ' 00:00:00';
     this.buttonElement.id = CHECK_STATUS_BUTTON_ID;
     this.buttonElement.className = CHECK_STATUS_BUTTON_CLASS;
 
@@ -61,7 +61,7 @@ export class StatusButton {
       clearInterval(this.timerId);
       this.timerId = undefined;
     }
-    this.buttonElement.innerHTML = CHECK_STATUS_BUTTON_LABEL;
+    this.buttonElement.innerHTML = CHECK_STATUS_BUTTON_LABEL + ' 00:00:00';
   }
 
   private formatTime(seconds: number): string {
