35b237991366e6a2923e263dc4becd7aee36064a
Antoni Ivanov <aivanov@vmware.com>
2023-08-09 19:09:08 +0300

    vdk-impala, vdk-postgres: adopt testcontainers (#2503)
    See #2467 for details

--- projects/vdk-plugins/vdk-impala/tests/conftest.py
+++ projects/vdk-plugins/vdk-impala/tests/conftest.py
@@ -2,32 +2,31 @@
 # SPDX-License-Identifier: Apache-2.0
 import os
 import time
+from functools import partial
 from unittest import mock
 
 import pytest
+from click.testing import Result
+from testcontainers.core.container import DockerContainer
+from testcontainers.core.waiting_utils import wait_container_is_ready
+from testcontainers.core.waiting_utils import wait_for
 from vdk.plugin.impala import impala_plugin
 from vdk.plugin.test_utils.util_funcs import CliEntryBasedTestRunner
-
 
 VDK_DB_DEFAULT_TYPE = "VDK_DB_DEFAULT_TYPE"
 VDK_IMPALA_HOST = "VDK_IMPALA_HOST"
 VDK_IMPALA_PORT = "VDK_IMPALA_PORT"
 
 
-def _is_responsive(runner):
-    try:
-        result = runner.invoke(["impala-query", "--query", "SELECT 1"])
-        if result.exit_code == 0:
-            return True
-    except:
-        return False
-
-
-@pytest.fixture(scope="session")
-def docker_compose_file(pytestconfig):
-    return os.path.join(
-        os.path.dirname(os.path.abspath(__file__)), "docker-compose.yml"
-    )
+@wait_container_is_ready(Exception)
+def wait_for_impala_to_be_responsive(runner):
+    result: Result = runner.invoke(["impala-query", "--query", "SELECT 1"])
+    if result.exit_code == 0:
+        return True
+    else:
+        raise ConnectionError(
+            f"Validation query failed with error {str(result.output)}"
+        )
 
 
 @pytest.fixture(scope="session")
@@ -39,18 +38,31 @@ def docker_compose_file(pytestconfig):
         VDK_IMPALA_PORT: "21050",
     },
 )
-def impala_service(docker_ip, docker_services):
+def impala_service(request):
     """Ensure that Impala service is up and responsive."""
-    runner = CliEntryBasedTestRunner(impala_plugin)
+    port = int(os.environ[VDK_IMPALA_PORT])
+    container = DockerContainer("cpcloud86/impala:latest").with_bind_ports(port, port)
 
-    # give the server some time to start before checking if it is ready
-    # before adding this sleep there were intermittent fails of the CI/CD with error:
-    # requests.exceptions.ConnectionError:
-    #   ('Connection aborted.', ConnectionResetError(104, 'Connection reset by peer'))
-    # More info: https://stackoverflow.com/questions/383738/104-connection-reset-by-peer-socket-error-or-when-does-closing-a-socket-resu
-    time.sleep(3)
+    try:
+        container.start()
+        runner = CliEntryBasedTestRunner(impala_plugin)
+        wait_for_impala_to_be_responsive(runner)
+        # wait 2 seconds to make sure the service is up and responsive
+        # might be unnecessary but it's out of abundance of caution
+        time.sleep(2)
+        print(
+            f"Impala service started on port {container.get_exposed_port(port)} and host {container.get_container_host_ip()}"
+        )
+    except Exception as e:
+        raise RuntimeError(
+            f"Failed to start Impala service: {e}\n"
+            f"Container logs: {container.get_logs()}"
+        ) from e
 
-    docker_services.wait_until_responsive(
-        timeout=120.0, pause=1, check=lambda: _is_responsive(runner)
-    )
-    time.sleep(10)
+    def stop_container():
+        container.stop()
+        print("Impala service stopped")
+
+    request.addfinalizer(stop_container)
+
+    return container
