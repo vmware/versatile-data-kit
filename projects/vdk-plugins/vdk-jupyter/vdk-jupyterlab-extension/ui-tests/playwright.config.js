cf0d4e6e8cd66b0f34ce78a6a6a28c67bf4abf34
Yoan Salambashev <yoan.g.salambashev@gmail.com>
2023-08-23 17:44:17 +0300

    vdk-jupyter: revert IT tests related changes
    Signed-off-by: Yoan Salambashev <ysalambashev@vmware.com>

--- projects/vdk-plugins/vdk-jupyter/vdk-jupyterlab-extension/ui-tests/playwright.config.js
+++ projects/vdk-plugins/vdk-jupyter/vdk-jupyterlab-extension/ui-tests/playwright.config.js
@@ -15,6 +15,5 @@ module.exports = {
     url: 'http://localhost:8888/lab',
     timeout: 200 * 100000,
     reuseExistingServer: !process.env.CI
-  },
-  reporter: [['junit', { outputFile: '../integration_tests_results.xml' }]]
+  }
 };
