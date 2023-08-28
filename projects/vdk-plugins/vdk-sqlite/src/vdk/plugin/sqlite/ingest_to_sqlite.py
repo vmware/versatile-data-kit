80f2d1c9631cddd5da2e6e19951ade6dad484b99
Maximiliaan72 <65510967+Maximiliaan72@users.noreply.github.com>
2023-08-28 14:20:19 +0200

    Update ingest_to_sqlite.py

--- projects/vdk-plugins/vdk-sqlite/src/vdk/plugin/sqlite/ingest_to_sqlite.py
+++ projects/vdk-plugins/vdk-sqlite/src/vdk/plugin/sqlite/ingest_to_sqlite.py
@@ -93,7 +93,7 @@ class IngestToSQLite(IIngesterPlugin):
         for obj in values:
             try:
                 cur.execute(query, obj)
-                log.debug(f"{obj} ingested.")
+                log.debug("Payload was ingested.")
             except Exception as e:
                 if isinstance(e, ProgrammingError):
                     errors.log_and_rethrow(
