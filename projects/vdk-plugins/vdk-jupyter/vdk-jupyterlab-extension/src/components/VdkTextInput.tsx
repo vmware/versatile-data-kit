8fb5ad212f65870c89fabdbc7402f2bb1257f54c
Duygu Hasan <duyguhsnhsn@gmail.com>
2023-09-04 16:27:06 +0300

    make input adjusting to the different jobData values

--- projects/vdk-plugins/vdk-jupyter/vdk-jupyterlab-extension/src/components/VdkTextInput.tsx
+++ projects/vdk-plugins/vdk-jupyter/vdk-jupyterlab-extension/src/components/VdkTextInput.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, RefObject } from 'react';
 import { jobData } from '../jobData';
 import { VdkOption } from '../vdkOptions/vdk_options';
 
@@ -18,36 +18,82 @@ export interface IVdkTextInputProps {
 }
 
 export default class VDKTextInput extends Component<IVdkTextInputProps> {
+   private inputRef: RefObject<HTMLInputElement>;
+
   /**
-   * Returns a React component for rendering a div with input and value  for VDK Option.
+   * Initializes a new instance of the VDKTextInput component.
    *
    * @param props - component properties
-   * @returns React component
    */
   constructor(props: IVdkTextInputProps) {
     super(props);
+    this.inputRef = React.createRef();
   }
+
   /**
-   * Renders a div with input and value  for VDK Option.
+   * Lifecycle method called after the component has mounted.
+   */
+  componentDidMount() {
+    this.adjustInputWidth();
+  }
+
+  /**
+   * Lifecycle method called after the component has updated.
+   *
+   * @param prevProps - previous component properties
+   */
+  componentDidUpdate(prevProps: IVdkTextInputProps) {
+    if (prevProps.value !== this.props.value) {
+      this.adjustInputWidth();
+    }
+  }
+
+  /**
+   * Adjusts the input width based on its placeholder value.
+   */
+   adjustInputWidth() {
+    if (!this.inputRef.current) return;
+
+    let maxWidth = 150;
+
+    jobData.forEach((value) => {
+        const tempSpan = document.createElement('span');
+        tempSpan.innerHTML = value;
+
+        const styles = ['fontFamily', 'fontSize', 'fontWeight', 'fontStyle', 'letterSpacing', 'textTransform'];
+        styles.forEach(style => {
+            tempSpan.style[style as any] = window.getComputedStyle(this.inputRef.current!).getPropertyValue(style);
+        });
+
+        document.body.appendChild(tempSpan);
+        const spanWidth = tempSpan.getBoundingClientRect().width;
+        document.body.removeChild(tempSpan);
+
+        maxWidth = Math.max(maxWidth, spanWidth + 30);
+    });
+
+    this.inputRef.current.style.width = `${maxWidth}px`;
+}
+  /**
+   * Renders a div with input and value for VDK Option.
    *
    * @returns React element
    */
   render(): React.ReactElement {
     return (
-      <>
-        <div className="jp-vdk-input-wrapper">
-          <label className="jp-vdk-label" htmlFor={this.props.option}>
-            {this.props.label}
-          </label>
-          <input
-            type="text"
-            id={this.props.option}
-            className="jp-vdk-input"
-            placeholder={this.props.value}
-            onChange={this.onInputChange}
-          />
-        </div>
-      </>
+      <div className="jp-vdk-input-wrapper">
+        <label className="jp-vdk-label" htmlFor={this.props.option}>
+          {this.props.label}
+        </label>
+        <input
+          ref={this.inputRef}
+          type="text"
+          id={this.props.option}
+          className="jp-vdk-input"
+          placeholder={this.props.value}
+          onChange={this.onInputChange}
+        />
+      </div>
     );
   }
   /**
