65aea6ef659d9c3b149a04d1b8d5feeb798529fe
Duygu Hasan <duyguhsnhsn@gmail.com>
2023-09-05 13:18:24 +0300

    adjust non vdkTextInput inputs to the same size

--- projects/vdk-plugins/vdk-jupyter/vdk-jupyterlab-extension/src/components/VdkTextInput.tsx
+++ projects/vdk-plugins/vdk-jupyter/vdk-jupyterlab-extension/src/components/VdkTextInput.tsx
@@ -3,108 +3,117 @@ import { jobData } from '../jobData';
 import { VdkOption } from '../vdkOptions/vdk_options';
 
 export interface IVdkTextInputProps {
-  /**
-   * VdkOption to which the input is for
-   */
-  option: VdkOption;
-  /**
-   * Value corresponding to the VdkOption
-   */
-  value: string;
-  /**
-   * Label message for the input
-   */
-  label: string;
+    /**
+     * VdkOption to which the input is for
+     */
+    option: VdkOption;
+
+    /**
+     * Value corresponding to the VdkOption
+     */
+    value: string;
+
+    /**
+     * Label message for the input
+     */
+    label: string;
+
+    /**
+     * Callback for when the width of the input is computed
+     */
+    onWidthComputed?: (width: number) => void;
 }
 
 export default class VDKTextInput extends Component<IVdkTextInputProps> {
-   private inputRef: RefObject<HTMLInputElement>;
+    private inputRef: RefObject<HTMLInputElement> = React.createRef();
 
-  /**
-   * Initializes a new instance of the VDKTextInput component.
-   *
-   * @param props - component properties
-   */
-  constructor(props: IVdkTextInputProps) {
-    super(props);
-    this.inputRef = React.createRef();
-  }
-
-  /**
-   * Lifecycle method called after the component has mounted.
-   */
-  componentDidMount() {
-    this.adjustInputWidth();
-  }
-
-  /**
-   * Lifecycle method called after the component has updated.
-   *
-   * @param prevProps - previous component properties
-   */
-  componentDidUpdate(prevProps: IVdkTextInputProps) {
-    if (prevProps.value !== this.props.value) {
-      this.adjustInputWidth();
+    /**
+     * Initializes a new instance of the VDKTextInput component.
+     *
+     * @param props - component properties
+     */
+    constructor(props: IVdkTextInputProps) {
+        super(props);
     }
-  }
 
-  /**
-   * Adjusts the input width based on its placeholder value.
-   */
-   adjustInputWidth() {
-    if (!this.inputRef.current) return;
+    /**
+     * Lifecycle method called after the component has mounted.
+     */
+    componentDidMount() {
+        this.adjustInputWidth();
+    }
 
-    let maxWidth = 250;
+    /**
+     * Lifecycle method called after the component has updated.
+     *
+     * @param prevProps - previous component properties
+     */
+    componentDidUpdate(prevProps: IVdkTextInputProps) {
+        if (prevProps.value !== this.props.value) {
+            this.adjustInputWidth();
+        }
+    }
 
-    jobData.forEach((value) => {
-        const tempSpan = document.createElement('span');
-        tempSpan.innerHTML = value;
+    /**
+     * Adjusts the input width based on its placeholder value.
+     */
+    adjustInputWidth() {
+        if (!this.inputRef.current) return;
 
-        const styles = ['fontFamily', 'fontSize', 'fontWeight', 'fontStyle', 'letterSpacing', 'textTransform'];
-        styles.forEach(style => {
-            tempSpan.style[style as any] = window.getComputedStyle(this.inputRef.current!).getPropertyValue(style);
+        let maxWidth = 250;
+
+        jobData.forEach((value) => {
+            const tempSpan = document.createElement('span');
+            tempSpan.innerHTML = value;
+
+            const styles = ['fontFamily', 'fontSize', 'fontWeight', 'fontStyle', 'letterSpacing', 'textTransform'];
+            styles.forEach(style => {
+                tempSpan.style[style as any] = window.getComputedStyle(this.inputRef.current!).getPropertyValue(style);
+            });
+
+            document.body.appendChild(tempSpan);
+            const spanWidth = tempSpan.getBoundingClientRect().width;
+            document.body.removeChild(tempSpan);
+
+            maxWidth = Math.max(maxWidth, spanWidth + 100);
         });
 
-        document.body.appendChild(tempSpan);
-        const spanWidth = tempSpan.getBoundingClientRect().width;
-        document.body.removeChild(tempSpan);
+        this.inputRef.current.style.width = `${maxWidth}px`;
+        this.props.onWidthComputed && this.props.onWidthComputed(maxWidth);
+    }
 
-        maxWidth = Math.max(maxWidth, spanWidth + 30);
-    });
+    /**
+     * Renders a div with input and value for VDK Option.
+     *
+     * @returns React element
+     */
+    render(): React.ReactElement {
+        return (
+            <div className="jp-vdk-input-wrapper">
+                <label className="jp-vdk-label" htmlFor={this.props.option}>
+                    {this.props.label}
+                </label>
+                <input
+                    ref={this.inputRef}
+                    type="text"
+                    id={this.props.option}
+                    className="jp-vdk-input"
+                    placeholder={this.props.value}
+                    onChange={this.onInputChange}
+                />
+            </div>
+        );
+    }
 
-    this.inputRef.current.style.width = `${maxWidth}px`;
-}
-  /**
-   * Renders a div with input and value for VDK Option.
-   *
-   * @returns React element
-   */
-  render(): React.ReactElement {
-    return (
-      <div className="jp-vdk-input-wrapper">
-        <label className="jp-vdk-label" htmlFor={this.props.option}>
-          {this.props.label}
-        </label>
-        <input
-          ref={this.inputRef}
-          type="text"
-          id={this.props.option}
-          className="jp-vdk-input"
-          placeholder={this.props.value}
-          onChange={this.onInputChange}
-        />
-      </div>
-    );
-  }
-  /**
-   * Callback invoked upon changing the input.
-   *
-   * @param event - event object
-   */
-  private onInputChange = (event: any): void => {
-    const nameInput = event.currentTarget as HTMLInputElement;
-    let value = nameInput.value;
-    if (!value) value = this.props.value;
-    jobData.set(this.props.option, value);
-  };
+    /**
+     * Callback invoked upon changing the input.
+     *
+     * @param event - event object
+     */
+    private onInputChange = (event: any): void => {
+        const nameInput = event.currentTarget as HTMLInputElement;
+        let value = nameInput.value;
+        if (!value) value = this.props.value;
+        jobData.set(this.props.option, value);
+    };
 }
