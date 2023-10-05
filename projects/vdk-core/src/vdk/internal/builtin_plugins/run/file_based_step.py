4931b72ce218f41cb2747b9990b8e12626ef338b
Mahendra <mahendrakshirsagar176@gmail.com>
2023-10-05 13:29:34 +0530

    imp replaced with importlib

--- projects/vdk-core/src/vdk/internal/builtin_plugins/run/file_based_step.py
+++ projects/vdk-core/src/vdk/internal/builtin_plugins/run/file_based_step.py
@@ -1,6 +1,6 @@
 # Copyright 2021-2023 VMware, Inc.
 # SPDX-License-Identifier: Apache-2.0
-import imp
+import importlib
 import inspect
 import logging
 import pathlib
@@ -79,7 +79,8 @@ class StepFuncFactory:
 
             try:
                 log.debug("Loading %s ..." % filename)
-                python_module = imp.load_source(namespace, str(step.file_path))
+                loader = importlib.machinery.SourceFileLoader(namespace, str(step.file_path))
+                python_module = loader.load_module()
                 log.debug("Loading %s SUCCESS" % filename)
             except BaseException as e:
                 log.info("Loading %s FAILURE" % filename)
