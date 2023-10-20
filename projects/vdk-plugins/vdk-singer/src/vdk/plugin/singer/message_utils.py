33ef657287f709d088432467f7e4808e7eede637
Antoni Ivanov <aivanov@vmware.com>
2023-10-20 17:07:55 +0300

    vdk-singer: Singer.io plugin for data sources
    This is adding a data source plugin for singer.io.
    
    So now users can specify singer taps as data sources
    
    They can also list all singer taps that can be found with
    `vdk singer --list-taps`

---
+++ projects/vdk-plugins/vdk-singer/src/vdk/plugin/singer/message_utils.py
@@ -0,0 +1,36 @@
+# Copyright 2021-2023 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+import logging
+from typing import cast
+
+from vdk.plugin.data_sources.data_source import DataSourcePayload
+from vdk.plugin.singer.adapter import CatalogEntry
+from vdk.plugin.singer.adapter import Message
+from vdk.plugin.singer.adapter import RecordMessage
+from vdk.plugin.singer.adapter import Schema
+from vdk.plugin.singer.adapter import SchemaMessage
+from vdk.plugin.singer.adapter import StateMessage
+
+log = logging.getLogger(__name__)
+
+
+def message_to_catalog_entry(msg: SchemaMessage):
+    entry = CatalogEntry()
+    entry.tap_stream_id = msg.stream
+    entry.stream = msg.stream
+    entry.key_properties = msg.key_properties
+    entry.schema = Schema.from_dict(msg.schema)
+    return entry
+
+
+def convert_message_to_payload(message: Message) -> DataSourcePayload:
+    if isinstance(message, RecordMessage):
+        return DataSourcePayload(
+            data=message.record, metadata={"time_extracted": message.time_extracted}
+        )
+    elif isinstance(message, StateMessage):
+        return DataSourcePayload(
+            data=None, metadata=None, state=cast(StateMessage, message).value
+        )
+    else:
+        log.warning(f"Unexpected message type: {message}")
