3a9e2b4ec20bfdc1abfdfc3cd4c3ee40bbe6295b
Gabriel Georgiev <gageorgiev@vmware.com>
2023-10-26 12:16:36 +0300

    comments
    Signed-off-by: Gabriel Georgiev <gageorgiev@vmware.com>

--- projects/vdk-plugins/vdk-structlog/tests/test_plugin.py
+++ projects/vdk-plugins/vdk-structlog/tests/test_plugin.py
@@ -1,12 +1,10 @@
 # Copyright 2021-2023 VMware, Inc.
 # SPDX-License-Identifier: Apache-2.0
 import os
-import pathlib
 from unittest import mock
 
 from click.testing import Result
 from vdk.plugin.structlog import structlog_plugin
-from vdk.plugin.test_utils.util_funcs import cli_assert_equal
 from vdk.plugin.test_utils.util_funcs import CliEntryBasedTestRunner
 from vdk.plugin.test_utils.util_funcs import jobs_path_from_caller_directory
 
@@ -30,26 +28,27 @@ def test_structlog():
         log_with_bound_context = [x for x in logs if "Log statement with bound context" in x][0]
         log_with_bound_and_extra_context = [x for x in logs if "Log statement with bound context and extra context" in x][0]
 
-        # case 1
-        # currently I'm getting the job name included on all steps, not sure if this is due to a bug in vdk-structlog
+        # check for job name in logs
+        assert "job-with-bound-logger" in log_with_no_bound_context
+        assert "job-with-bound-logger" in log_with_bound_context
+        assert "job-with-bound-logger" in log_with_bound_and_extra_context
 
-        # case 2
+        # check bound logger can bind context but that bound context does not appear in the rest of logging
         assert "bound_test_value" in log_with_bound_context
         assert"bound_test_value" not in log_with_no_bound_context
 
-        # case 3
+        # check extra bound context does not appear if not set in the logging_metadata config variable
         assert "extra_test_value" not in log_with_bound_context
 
-        # case 4
+        # check for both the bound and the extra context in bound log statements which include extra context
         assert "extra_test_value" in log_with_bound_and_extra_context
-        assert "bound_test_value" in  log_with_bound_and_extra_context
+        assert "bound_test_value" in log_with_bound_and_extra_context
 
-        # case 5 (continues in the next job run)
-        # asserting one of the bound values does not appear in the logs since we've not configured it to appear
+        # check that one of the bound values does not appear in the logs since we've not configured it to appear
         assert "excluded_value" not in log_with_bound_context
         assert "excluded_value" not in log_with_bound_and_extra_context
 
-        # asserting the log level and job name appear in the logs (so we can compare to when we exclude them below)
+        # check the log level and job name appear in the logs (so we can compare to when we exclude them below)
         assert "[INFO ]" in result.stdout
         assert "job-with-bound-logger" in result.stdout
 
@@ -67,5 +66,6 @@ def test_structlog():
 
         test_log = [x for x in result.stdout.split("\n") if "Log statement with bound context and extra context" in x][0]
 
+        # check that log level and job name do not appear if we have not included them in logging_metadata
         assert "[INFO ]" not in test_log
         assert "job-with-bound-logger" not in test_log
