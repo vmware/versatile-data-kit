72f1e08c3a5e7ea4e7853f14bd06464fd2b51712
Antoni Ivanov <aivanov@vmware.com>
2023-10-30 00:13:28 +0200

    vdk-data-sources: add DataFlowInput API
    Data source can be used in this way:
    
    ```python
    def run(job_input: IJobInput):
    source = SourceDefinition(id="auto", name="auto-generated-data",
    config={})
        destination = DestinationDefinition(id="auto-dest", method="memory")
    
        with DataFlowInput(job_input) as flow_input:
            flow_input.start(source, destination)
    ```
    
    or in config.toml file
    ```toml
    [sources.auto]
    name="auto-generated-data"
    config={}
    [destinations.auto-dest]
    method="memory"
    [[flows]]
    from="auto"
    to="auto-dest"
    ```
    
    ```python
    def run(job_input: IJobInput):
        with DataFlowInput(job_input) as flow_input:
            flow_input.start_flow(toml_parser.load_config("config.toml"))
    ```
    
    flow
    
    flow comments

---
+++ projects/vdk-plugins/vdk-data-sources/src/vdk/plugin/data_sources/mapping/definitions.py
@@ -0,0 +1,72 @@
+# Copyright 2021-2023 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+from dataclasses import dataclass
+from dataclasses import field
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Optional
+
+from vdk.plugin.data_sources.ingester import DataSourceIngester
+
+
+@dataclass
+class SourceDefinition:
+    """
+    SourceDefinition class that defines the metadata and configuration for a data source.
+
+    :param id: User provided identifier for the source "instance" which can be used to refer to that source later
+    :param name: The name of the source. Must be the same name as the DataSource has been registered with (see vdk data-sources --list)
+    :param config: The configuration for the source. Refer to documented data source config options (see vdk data-sources --config).
+    """
+
+    id: str
+    name: str
+    config: Dict[str, Any] = field(default_factory=dict)
+    # TODO might be good idea to include initial state for first run
+    # initial_state: Dict[str, Any] = field(default_factory=dict)
+
+
+@dataclass
+class DestinationDefinition:
+    """
+    Describes the attributes and methods for a data destination.
+
+    :param id: User provided identifier for the destination "instance" which can be used to refer to that source later.
+    :param method: The ingestion method. Refer to IIngester for more information .
+    :param target: The ingestion target. Refer to IIngester for more information .
+    """
+
+    id: str
+    method: str
+    target: Optional[str] = None
+    # TODO: config when destination targets support being passed configs
+
+
+@dataclass
+class DataFlowMappingDefinition:
+    """
+    Defines how data flows from a source to a destination and the optional transformation in between.
+
+    :param from_source: The source definition from where data originates.
+    :param to_destination: The destination definition where data will be sent.
+    """
+
+    from_source: SourceDefinition
+    to_destination: DestinationDefinition
+    # TODO add configuration for mappings tables and columns or function
+
+
+@dataclass
+class Definitions:
+    """
+    Aggregates all the source, destination, and flow definitions for an ingestion pipeline.
+
+    :param sources: Dictionary of source definitions.
+    :param destinations: Dictionary of destination definitions.
+    :param flows: List of data flow mapping definitions.
+    """
+
+    sources: Dict[str, SourceDefinition]
+    destinations: Dict[str, DestinationDefinition]
+    flows: List[DataFlowMappingDefinition]
---
+++ projects/vdk-plugins/vdk-data-sources/tests/functional/jobs/ingest-data-flow-job/step.py
@@ -0,0 +1,14 @@
+# Copyright 2021-2023 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+from vdk.api.job_input import IJobInput
+from vdk.plugin.data_sources.mapping.data_flow import DataFlowInput
+from vdk.plugin.data_sources.mapping.definitions import DestinationDefinition
+from vdk.plugin.data_sources.mapping.definitions import SourceDefinition
+
+
+def run(job_input: IJobInput):
+    source = SourceDefinition(id="auto", name="auto-generated-data", config={})
+    destination = DestinationDefinition(id="auto-dest", method="memory")
+
+    with DataFlowInput(job_input) as flow_input:
+        flow_input.start(source, destination)
