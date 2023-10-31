6c4a89f94cd51d35e3ac2b5a055d8c5f5bc77351
Antoni Ivanov <aivanov@vmware.com>
2023-10-31 13:18:32 +0200

    vdk-duckdb: fix ingestion (#2843)
    Ingestion was not working properly due to multiple issues so it's being
    fixed.
    
    - The plugin now seamlessly integrates with the VDK connection
    interface, making it easier to manage DuckDB connections instead of
    relying on custom DuckDB Connection
    - The ingestion insertion mechanism fixes:
    - The ingestion method was not registered (so
    job_input.send(method='duckdb') was not working)
    - Change the insertion to be batch and not one by one (which is very
    slow)
    - added ingestion tests to cover regressions and verify it's working
    - Renamed duckdb_file to database (which is what duckdb name for that
    parameter is)
    
    Signed-off-by: Antoni Ivanov <aivanov@vmware.com>

--- projects/vdk-plugins/vdk-duckdb/tests/test_plugin.py
+++ projects/vdk-plugins/vdk-duckdb/tests/test_plugin.py
@@ -1,52 +1,75 @@
 # Copyright 2021-2023 VMware, Inc.
 # SPDX-License-Identifier: Apache-2.0
+import json
 import os
-import unittest
+from unittest import mock
 
 import duckdb
-from vdk.internal.core.config import Configuration
-from vdk.internal.core.errors import UserCodeError
-from vdk.plugin.duckdb.duckdb_configuration import DuckDBConfiguration
-from vdk.plugin.duckdb.ingest_to_duckdb import IngestToDuckDB
+import pytest
+from click.testing import CliRunner
+from click.testing import Result
+from vdk.plugin.duckdb import duckdb_plugin
+from vdk.plugin.test_utils.util_funcs import cli_assert_equal
+from vdk.plugin.test_utils.util_funcs import CliEntryBasedTestRunner
+from vdk.plugin.test_utils.util_funcs import jobs_path_from_caller_directory
 
 
-class TestIngestToDuckDB(unittest.TestCase):
-    def setUp(self):
-        self.temp_db_file = "test_db.duckdb"
-        self.connection = duckdb.connect(self.temp_db_file)
+@pytest.fixture(scope="function")
+def setup_duckdb(tmpdir):
+    temp_db_file = os.path.join(str(tmpdir), "test_db.duckdb")
+    connection = duckdb.connect(temp_db_file)
 
-        duckdb_conf = Configuration(
-            {
-                "DUCKDB_FILE": self.temp_db_file,
-                "DUCKDB_INGEST_AUTO_CREATE_TABLE_ENABLED": False,  # Changed configuration to False
-            },
-            {},
-        )
-        self.conf = DuckDBConfiguration(duckdb_conf)
-        self.ingester = IngestToDuckDB(self.conf)
+    with mock.patch.dict(
+        os.environ,
+        {
+            "DB_DEFAULT_TYPE": "duckdb",
+            "DUCKDB_DATABASE": temp_db_file,
+            "DUCKDB_INGEST_AUTO_CREATE_TABLE_ENABLED": "true",
+        },
+    ):
+        yield
 
-    def tearDown(self):
-        self.connection.close()
-        os.remove(self.temp_db_file)
+        connection.close()
+        os.remove(temp_db_file)
 
-    def test_ingest_payload(self):
-        destination_table = "test_table"
-        payload = [{"col1": 1, "col2": "text"}]
 
-        try:
-            self.ingester.ingest_payload(payload, destination_table=destination_table)
-        except UserCodeError as e:
-            self.assertIn(
-                "destination_table does not exist in the target database", str(e)
-            )
+def test_duckbd_plugin(setup_duckdb):
+    runner = CliEntryBasedTestRunner(duckdb_plugin)
 
-        # Check the table contents if the ingest didn't raise an error
-        else:
-            with duckdb.connect(self.temp_db_file) as conn:
-                cursor = conn.cursor()
-                cursor.execute(f"SELECT * FROM {destination_table}")
-                result = cursor.fetchall()
-                self.assertEqual(result, [(1, "text")])
+    result: Result = runner.invoke(
+        ["run", jobs_path_from_caller_directory("test-duckdb-job")]
+    )
 
-    if __name__ == "__main__":
-        unittest.main()
+    cli_assert_equal(0, result)
+
+    _verify_sql_steps(runner)
+    _verify_ingest_step(runner)
+
+
+def _verify_sql_steps(runner):
+    actual_rs = _sql_query(runner, "SELECT * FROM stocks")
+    cli_assert_equal(0, actual_rs)
+    expected_data = [
+        {"date": "2020-01-01", "symbol": "GOOG", "price": 123.0},
+        {"date": "2020-01-01", "symbol": "GOOG", "price": 123.0},
+    ]
+    assert json.loads(actual_rs.output) == expected_data
+
+
+def _verify_ingest_step(runner):
+    actual_rs = _sql_query(runner, "SELECT * FROM test_duckdb_table")
+    cli_assert_equal(0, actual_rs)
+    expected_data = [
+        {"bool_col": 0, "dec_col": "1.234", "int_col": 2, "str_col": "str"}
+    ]
+    assert json.loads(actual_rs.output) == expected_data
+
+
+def _sql_query(runner, query):
+    actual_rs: Result = runner.invoke(
+        ["sql-query", "-o", "json", "--query", query],
+        runner=CliRunner(
+            mix_stderr=False
+        ),  # TODO: replace when CliEntryBasedTestRunner add support for it
+    )
+    return actual_rs
