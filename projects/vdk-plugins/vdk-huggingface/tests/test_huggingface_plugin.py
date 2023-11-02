fe9e8fd7bfc85cf078cbfb583fece73d2b1eb841
Antoni Ivanov <aivanov@vmware.com>
2023-11-02 14:06:49 +0200

    vdk-huggingface: add new ingest plugin (#2858)
    Versatile Data Kit (VDK) plugin for integrating with Huggingface as both
    a data source and a target.
    
    This plugin allows you to ingest data payloads into a Huggingface
    repository and makes it easier to work with datasets stored in
    Huggingface.

---
+++ projects/vdk-plugins/vdk-huggingface/tests/test_huggingface_plugin.py
@@ -0,0 +1,51 @@
+# Copyright 2021-2023 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+import json
+import os
+import pathlib
+import time
+
+import pytest
+from click.testing import Result
+from huggingface_hub import HfApi
+from vdk.plugin.huggingface import plugin_entry
+from vdk.plugin.test_utils.util_funcs import cli_assert_equal
+from vdk.plugin.test_utils.util_funcs import CliEntryBasedTestRunner
+from vdk.plugin.test_utils.util_funcs import jobs_path_from_caller_directory
+
+
+@pytest.mark.skipif(
+    os.environ.get("HUGGINGFACE_TOKEN") is None
+    or os.environ.get("HUGGINGFACE_REPO_ID") is None,
+    reason="Skipping test until HUGGINGFACE_TOKEN and HUGGINGFACE_REPO_ID and are set",
+)
+def test_huggingface_upload():
+    runner = CliEntryBasedTestRunner(plugin_entry)
+
+    time_var = time.time()
+    result: Result = runner.invoke(
+        [
+            "run",
+            jobs_path_from_caller_directory("huggingface-job"),
+            "--arguments",
+            json.dumps({"time": time_var}),
+        ]
+    )
+    cli_assert_equal(0, result)
+
+    api = HfApi()
+    file_path = api.hf_hub_download(
+        repo_id=os.environ["HUGGINGFACE_REPO_ID"],
+        filename="test_vdk_table",
+        repo_type="dataset",
+    )
+    row = [{"str_col": "str", "int_col": 2, "bool_col": False, "time": time_var}]
+    assert json.loads(pathlib.Path(file_path).read_text()) == row * 10
+
+    file_path = api.hf_hub_download(
+        repo_id=os.environ["HUGGINGFACE_REPO_ID"],
+        filename="test_vdk_table2",
+        repo_type="dataset",
+    )
+    row = [{"str_col": "str", "int_col": 2, "bool_col": False, "time": time_var}]
+    assert json.loads(pathlib.Path(file_path).read_text()) == row * 10
