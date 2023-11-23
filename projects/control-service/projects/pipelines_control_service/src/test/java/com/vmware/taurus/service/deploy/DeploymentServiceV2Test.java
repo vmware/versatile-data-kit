a227dc5e913a182e51ddfdf2997e0b82f08d7c73
mrMoZ1 <mzhivkov@vmware.com>
2023-11-23 03:11:07 +0200

    control-service: unit tests for data job persistence classes
    Signed-off-by: mrMoZ1 <mzhivkov@vmware.com>

--- projects/control-service/projects/pipelines_control_service/src/test/java/com/vmware/taurus/service/deploy/DeploymentServiceV2Test.java
+++ projects/control-service/projects/pipelines_control_service/src/test/java/com/vmware/taurus/service/deploy/DeploymentServiceV2Test.java
@@ -9,9 +9,11 @@ import com.vmware.taurus.ControlplaneApplication;
 import com.vmware.taurus.controlplane.model.data.DataJobResources;
 import com.vmware.taurus.datajobs.TestUtils;
 import com.vmware.taurus.datajobs.ToModelApiConverter;
+import com.vmware.taurus.service.model.ActualDataJobDeployment;
 import com.vmware.taurus.service.model.DeploymentStatus;
 import com.vmware.taurus.service.model.DesiredDataJobDeployment;
 import com.vmware.taurus.service.model.JobDeployment;
+import com.vmware.taurus.service.repository.ActualJobDeploymentRepository;
 import com.vmware.taurus.service.repository.DesiredJobDeploymentRepository;
 import com.vmware.taurus.service.repository.JobsRepository;
 import org.junit.jupiter.api.AfterEach;
@@ -21,11 +23,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = ControlplaneApplication.class)
-public class DeploymentServiceV2WriteTest {
+public class DeploymentServiceV2Test {
 
   @Autowired JobsRepository jobsRepository;
   @Autowired DesiredJobDeploymentRepository desiredJobDeploymentRepository;
   @Autowired DeploymentServiceV2 deploymentServiceV2;
+  @Autowired ActualJobDeploymentRepository actualJobDeploymentRepository;
 
   @Test
   public void testUpdateDesiredDeployment_expectNewDeployment() {
@@ -53,10 +56,66 @@ public class DeploymentServiceV2WriteTest {
     compareSavedDeploymentWithTestDeployment(jobDeployment, savedDeployment, "user");
   }
 
+  @Test
+  public void testUpdateDeploymentEnabledStatus() {
+
+    var dataJob = ToModelApiConverter.toDataJob(TestUtils.getDataJob("teamName", "jobName"));
+    jobsRepository.save(dataJob);
+    var initialDeployment = new DesiredDataJobDeployment();
+    initialDeployment.setDataJob(dataJob);
+    initialDeployment.setDataJobName(dataJob.getName());
+    initialDeployment.setEnabled(false);
+    desiredJobDeploymentRepository.save(initialDeployment);
+
+    var desiredDeployment = desiredJobDeploymentRepository.findById(dataJob.getName()).get();
+
+    Assertions.assertFalse(desiredDeployment.getEnabled());
+
+    deploymentServiceV2.updateDeploymentEnabledStatus(dataJob.getName(), true);
+
+    desiredDeployment = desiredJobDeploymentRepository.findById(dataJob.getName()).get();
+
+    Assertions.assertTrue(desiredDeployment.getEnabled());
+
+  }
+
+  @Test
+  public void testFindAllDesiredDataJobDeployments() {
+    Assertions.assertEquals(0, deploymentServiceV2.findAllDesiredDataJobDeployments().keySet().size());
+
+    var dataJob = ToModelApiConverter.toDataJob(TestUtils.getDataJob("teamName", "jobName"));
+    jobsRepository.save(dataJob);
+    var initialDeployment = new DesiredDataJobDeployment();
+    initialDeployment.setDataJob(dataJob);
+    initialDeployment.setDataJobName(dataJob.getName());
+    desiredJobDeploymentRepository.save(initialDeployment);
+
+    Assertions.assertEquals(1, deploymentServiceV2.findAllDesiredDataJobDeployments().keySet().size());
+
+  }
+
+  @Test
+  public void testFindAllActualDataJobDeployments() {
+
+    Assertions.assertEquals(0,
+        deploymentServiceV2.findAllActualDataJobDeployments().keySet().size());
+    var deployment = new ActualDataJobDeployment();
+
+    var dataJob = ToModelApiConverter.toDataJob(TestUtils.getDataJob("teamName", "jobName"));
+    deployment.setDataJobName(dataJob.getName());
+
+    jobsRepository.save(dataJob);
+    actualJobDeploymentRepository.save(deployment);
+
+    Assertions.assertEquals(1,
+        deploymentServiceV2.findAllActualDataJobDeployments().keySet().size());
+  }
+
   @AfterEach
   public void cleanup() {
     desiredJobDeploymentRepository.deleteAll();
     jobsRepository.deleteAll();
+    actualJobDeploymentRepository.deleteAll();
   }
 
   private void compareSavedDeploymentWithTestDeployment(
---
+++ projects/control-service/projects/pipelines_control_service/src/test/java/com/vmware/taurus/service/deploy/JobImageDeployerV2Test.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2021-2023 VMware, Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.vmware.taurus.service.deploy;
+
+import com.vmware.taurus.ControlplaneApplication;
+import com.vmware.taurus.datajobs.TestUtils;
+import com.vmware.taurus.datajobs.ToModelApiConverter;
+import com.vmware.taurus.service.kubernetes.DataJobsKubernetesService;
+import com.vmware.taurus.service.model.ActualDataJobDeployment;
+import com.vmware.taurus.service.model.DataJob;
+import com.vmware.taurus.service.model.DataJobDeploymentResources;
+import com.vmware.taurus.service.model.DesiredDataJobDeployment;
+import io.kubernetes.client.openapi.ApiException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+@SpringBootTest(classes = ControlplaneApplication.class)
+public class JobImageDeployerV2Test {
+
+  @Autowired
+  private JobImageDeployerV2 jobImageDeployerV2;
+  @SpyBean
+  private DataJobsKubernetesService dataJobsKubernetesService;
+
+  @Test
+  public void testScheduleJob() throws ApiException {
+    var dataJob = ToModelApiConverter.toDataJob(TestUtils.getDataJob("teamName", "jobName"));
+    Mockito.doNothing().when(dataJobsKubernetesService).createCronJob(Mockito.any());
+
+    var actualDeployment = jobImageDeployerV2.scheduleJob(dataJob, getDesiredDeployment(dataJob),
+        null, false, false, "test-job");
+
+    verifyActualDeployment(actualDeployment);
+  }
+
+  @Test
+  public void testScheduleJob_presentInK8S() throws ApiException {
+    var dataJob = ToModelApiConverter.toDataJob(TestUtils.getDataJob("teamName", "jobName"));
+    Mockito.doNothing().when(dataJobsKubernetesService).updateCronJob(Mockito.any());
+
+    var actualDeployment = jobImageDeployerV2.scheduleJob(dataJob, getDesiredDeployment(dataJob),
+        null, false, true, "test-job");
+
+    verifyActualDeployment(actualDeployment);
+  }
+
+  private void verifyActualDeployment(ActualDataJobDeployment actualDeployment) {
+    Assertions.assertNotNull(actualDeployment);
+    Assertions.assertNotNull(actualDeployment.getDeploymentVersionSha());
+    Assertions.assertEquals("jobName", actualDeployment.getDataJobName());
+    Assertions.assertEquals("testPython", actualDeployment.getPythonVersion());
+    Assertions.assertEquals("testSha", actualDeployment.getGitCommitSha());
+    Assertions.assertEquals("testSched", actualDeployment.getSchedule());
+    Assertions.assertNotNull(actualDeployment.getResources());
+    Assertions.assertEquals("user", actualDeployment.getLastDeployedBy());
+    Assertions.assertTrue(actualDeployment.getEnabled());
+  }
+
+  private DesiredDataJobDeployment getDesiredDeployment(DataJob dataJob) {
+    var initialDeployment = new DesiredDataJobDeployment();
+    initialDeployment.setDataJob(dataJob);
+    initialDeployment.setDataJobName(dataJob.getName());
+    initialDeployment.setEnabled(true);
+    initialDeployment.setResources(new DataJobDeploymentResources());
+    initialDeployment.setSchedule("testSched");
+    initialDeployment.setUserInitiated(true);
+    initialDeployment.setGitCommitSha("testSha");
+    initialDeployment.setPythonVersion("testPython");
+    initialDeployment.setLastDeployedBy("user");
+
+    return initialDeployment;
+  }
+}
