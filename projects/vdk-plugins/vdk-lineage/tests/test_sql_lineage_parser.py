d85d8f050451218459a770f99cae3755c78ac0dc
Duygu Hasan <duyguhsnhsn@gmail.com>
2024-01-08 13:08:32 +0200

    revert to previous query

--- projects/vdk-plugins/vdk-lineage/tests/test_sql_lineage_parser.py
+++ projects/vdk-plugins/vdk-lineage/tests/test_sql_lineage_parser.py
@@ -6,40 +6,58 @@ from vdk.plugin.lineage.sql_lineage_parser import get_table_lineage_from_query
 
 def test_get_insert_table_lineage_from_query_complex():
     query = """
-        -- job_name: stage-processing-hourly
-        -- op_id: stage-processing-hourly-1647557100-ccrrw
-        -- template: vdk.templates.load.dimension.scd2
-        /* TO DO DROP AND RECREATE TARGET TABLE ON FULL RELOAD OR DATA TYPE CHANGE */
-        -- DROP TABLE stg.dim_sddc_h;
-        -- CREATE TABLE stg.dim_sddc_h STORED AS PARQUET AS SELECT * FROM stg.stg_dim_sddc_h;
-
-        -- The SHUFFLE hint is removed for parser compatibility. Reintroduce if running on Impala.
-        INSERT OVERWRITE TABLE stg.dim_sddc_h
-            WITH
-            tgt_filtered AS (
-                SELECT *
-                FROM stg.dim_sddc
-                WHERE valid_to_ts != '2999-01-01 00:00:00'
-                  AND CONCAT(CAST(dim_sddc_id AS STRING), CAST(valid_from_ts AS STRING)) NOT IN (
-                      SELECT CONCAT(CAST(dim_sddc_id AS STRING), CAST(valid_from_ts AS STRING))
-                      FROM processing_stg.vw_dim_sddc_h
-                  )
-            ),
-            src_filtered AS (
-                SELECT *
-                FROM processing_stg.vw_dim_sddc_h
-                WHERE CONCAT(CAST(dim_sddc_id AS STRING), CAST(valid_from_ts AS STRING)) NOT IN (
-                    SELECT CONCAT(CAST(dim_sddc_id AS STRING), CAST(valid_from_ts AS STRING))
-                    FROM tgt_filtered
-                )
-            )
-        SELECT *
-        FROM tgt_filtered
-        UNION ALL
-        SELECT COALESCE(tgt.dim_sddc_h_id, uuid()), src.*
-        FROM src_filtered AS src
-        LEFT JOIN stg.dim_sddc_h AS tgt ON src.dim_sddc_id = tgt.dim_sddc_id
-                                         AND src.valid_from_ts = tgt.valid_from_ts
+          -- job_name: stage-processing-hourly
+      -- op_id: stage-processing-hourly-1647557100-ccrrw
+      -- template: vdk.templates.load.dimension.scd2
+      /* TO DO DROP AND RECREATE TARGET TABLE ON FULL RELOAD OR DATA TYPE CHANGE */
+      -- DROP TABLE stg.dim_sddc_h;
+      -- CREATE TABLE stg.dim_sddc_h STORED AS PARQUET AS SELECT * FROM stg.stg_dim_sddc_h;
+      -- /* +SHUFFLE */ below is a query hint to Impala. -- Do not remove! https://www.cloudera.com/documentation/enterprise/5-9-x/topics/impala_hints.html
+    INSERT OVERWRITE TABLE stg.dim_sddc_h /* +SHUFFLE */
+        WITH
+        -- filter from the target all elements that define non-current state and are not updated/present in the source
+     tgt_filtered AS (
+        SELECT
+            *
+        FROM
+            stg.dim_sddc
+        WHERE
+            valid_to_ts != '2999-01-01 00:00:00'
+            AND CONCAT(CAST(dim_sddc_id AS STRING),
+            CAST(valid_from_ts AS STRING)) NOT IN (
+            SELECT
+                CONCAT(CAST(dim_sddc_id AS STRING),
+                CAST(valid_from_ts AS STRING))
+            FROM
+                processing_stg.vw_dim_sddc_h ) ),
+        -- filter from the source all elements which are present in
+     tgt_filtered src_filtered AS (
+        SELECT
+            *
+        FROM
+            processing_stg.vw_dim_sddc_h
+        WHERE
+            CONCAT(CAST(dim_sddc_id AS STRING),
+            CAST(valid_from_ts AS STRING)) NOT IN (
+            SELECT
+                CONCAT(CAST(dim_sddc_id AS STRING),
+                CAST(valid_from_ts AS STRING))
+            FROM
+                tgt_filtered ) ) (
+        SELECT
+            *
+        FROM
+            tgt_filtered )
+    UNION ALL (
+    SELECT
+        COALESCE(tgt.dim_sddc_h_id,
+        uuid()),
+        src.*
+    FROM
+        src_filtered AS src
+    LEFT JOIN stg.dim_sddc_h AS tgt ON
+        src.dim_sddc_id = tgt.dim_sddc_id
+        AND src.valid_from_ts = tgt.valid_from_ts )
     """
     lineage_data = get_table_lineage_from_query(query, "test_schema", "test_catalog")
 
