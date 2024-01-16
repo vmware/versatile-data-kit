ef69ad1f03c5bca2d42d71b88ca143d649d139df
Antoni Ivanov <aivanov@vmware.com>
2024-01-16 17:28:29 +0200

    vdk-oracle: fixes
    1. Fix the tests to be timezone independant. Currently they were failing
    because fromtimestamp is timezone dependant so it will return different
    reseult depending on the timezone
    
    2. Simplify passing secrets as configuration by removing redudant
    configuration options and note it's just a prototype until such change
    is in core. It is unnecsary to pass 3 configuration options to start
    passing secret. Just read everytime secrets if there's one use if not
    no. Kept option "ORACLE_USE_SECRETS" as a kill switch just in case.

--- projects/vdk-plugins/vdk-oracle/src/vdk/plugin/oracle/oracle_configuration.py
+++ projects/vdk-plugins/vdk-oracle/src/vdk/plugin/oracle/oracle_configuration.py
@@ -13,8 +13,6 @@ ORACLE_USER = "ORACLE_USER"
 ORACLE_PASSWORD = "ORACLE_PASSWORD"
 ORACLE_USE_SECRETS = "ORACLE_USE_SECRETS"
 ORACLE_THICK_MODE = "ORACLE_THICK_MODE"
-ORACLE_USER_SECRET = "ORACLE_USER_SECRET"
-ORACLE_PASSWORD_SECRET = "ORACLE_PASSWORD_SECRET"
 ORACLE_CONNECTION_STRING = "ORACLE_CONNECTION_STRING"
 
 
@@ -27,12 +25,6 @@ class OracleConfiguration:
 
     def get_oracle_password(self) -> str:
         return self.__config.get_value(ORACLE_PASSWORD)
-
-    def get_oracle_user_secret(self) -> str:
-        return self.__config.get_value(ORACLE_USER_SECRET)
-
-    def get_oracle_password_secret(self) -> str:
-        return self.__config.get_value(ORACLE_PASSWORD_SECRET)
 
     def get_oracle_connection_string(self) -> str:
         return self.__config.get_value(ORACLE_CONNECTION_STRING)
@@ -65,21 +57,13 @@ class OracleConfiguration:
         )
         config_builder.add(
             key=ORACLE_USE_SECRETS,
-            default_value=False,
-            description="Set this flag to use secrets to connect to Oracle",
-        )
-        config_builder.add(
-            key=ORACLE_USER_SECRET,
-            default_value=None,
-            description="The user secret key if using secrets to connect to Oracle",
-        )
-        config_builder.add(
-            key=ORACLE_PASSWORD_SECRET,
-            default_value=None,
-            description="The password secret key if using secrets to connect to Oracle",
+            default_value=True,
+            description="Set this flag to use secrets to connect to Oracle. This is protype option. "
+            "Leave default value unless there are issues.",
         )
         config_builder.add(
             key=ORACLE_THICK_MODE,
             default_value=True,
-            description="Use oracle thick mode, default is True. Set to False to disable oracle thick mode. More info: https://python-oracledb.readthedocs.io/en/latest/user_guide/appendix_b.html",
+            description="Use oracle thick mode. Set to False to disable oracle thick mode. "
+            "More info: https://python-oracledb.readthedocs.io/en/latest/user_guide/appendix_b.html",
         )
--- projects/vdk-plugins/vdk-oracle/tests/jobs/oracle-ingest-job-different-payloads-no-table/10_ingest.py
+++ projects/vdk-plugins/vdk-oracle/tests/jobs/oracle-ingest-job-different-payloads-no-table/10_ingest.py
@@ -36,7 +36,7 @@ def run(job_input):
             "int_data": 12,
             "float_data": 1.2,
             "bool_data": True,
-            "timestamp_data": datetime.datetime.fromtimestamp(1700554373),
+            "timestamp_data": datetime.datetime.utcfromtimestamp(1700554373),
         },
         {
             "id": 6,
