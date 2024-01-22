9615fe8906c3d1acf8cbc0ea3de0b2d2d4473a6f
Yoan Salambashev <36246462+yonitoo@users.noreply.github.com>
2024-01-22 16:57:22 +0200

    examples: add Confluence data retrieval example Data Job (#3040)
    What: 
    A data job that retrieves documents from Confluence using [LangChain's
    ConfluenceLoader](https://python.langchain.com/docs/integrations/document_loaders/confluence).
    The user sets Confluence space key to fetch data from Confluence space. 
    However, if they want to fetch a single document by ID, they could do so
    by setting the document ID.
    The Data Job consists of 2 steps: 
    - 10_fetch_confluence_document -> fetches data from a single Confluence
    document);
     - 20_fetch_confluence_space -> fetches data from a Confluence space.
    
    Why: 
    As part of the Vector Database ingestion initiative, we want to build
    the type of RAG data population pipeline a big organisation would build.
    
    Closes: #2990 
    
    Signed-off-by: Yoan Salambashev <yoan.salambashev@broadcom.com>

---
+++ examples/confluence-data-retrieval-example/20_fetch_confluence_space.py
@@ -0,0 +1,41 @@
+# Copyright 2021-2024 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+import logging
+import os
+
+from langchain_community.document_loaders import ConfluenceLoader
+from vdk.api.job_input import IJobInput
+
+log = logging.getLogger(__name__)
+
+
+def fetch_confluence_space(url, token, space_key):
+    try:
+        # For more info regarding the LangChain ConfluenceLoader:
+        # https://python.langchain.com/docs/integrations/document_loaders/confluence
+        loader = ConfluenceLoader(url=url, token=token)
+        documents = loader.load(
+            space_key=space_key, include_attachments=True, limit=20, max_pages=20
+        )
+        return documents
+    except Exception as e:
+        log.error(f"Error fetching documents from Confluence: {e}")
+        return None
+
+
+def run(job_input: IJobInput):
+    log.info(f"Starting job step {__name__}")
+
+    confluence_url = os.environ.get("VDK_CONFLUENCE_URL")
+    token = os.environ.get("VDK_CONFLUENCE_TOKEN")
+    space_key = os.environ.get("VDK_CONFLUENCE_SPACE_KEY")
+
+    docs = fetch_confluence_space(confluence_url, token, space_key)
+
+    if docs:
+        log.info(f"{len(docs)} documents fetched successfully.")
+        log.info("Printing the first 50 chars of each doc")
+        for i, doc in enumerate(docs):
+            print(f"{i}: {doc.page_content[:50]}")
+    else:
+        log.error(f"Failed to fetch any documents from the space with key {space_key}.")
