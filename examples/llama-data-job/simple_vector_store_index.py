2523369378cb529082ae0a772e783a7b14b4936b
Duygu Hasan <duyguhsnhsn@gmail.com>
2024-02-15 14:22:42 +0200

    add embedings

--- examples/llama-data-job/simple_vector_store_index.py
+++ examples/llama-data-job/simple_vector_store_index.py
@@ -1,83 +1,14 @@
 # Copyright 2021-2023 VMware, Inc.
 # SPDX-License-Identifier: Apache-2.0
-import json
 import logging
 import os
 from datetime import datetime
 
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, Settings
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from vdk.api.job_input import IJobInput
 from llama_index.readers.confluence import ConfluenceReader
-from confluence_document import ConfluenceDocument
 log = logging.getLogger(__name__)
-
-
-def read_json_file(file_path):
-    try:
-        with open(file_path) as file:
-            return json.load(file)
-    except (FileNotFoundError, json.JSONDecodeError) as e:
-        log.error(f"Error reading JSON file: {e}")
-        return None
-
-
-def write_json_file(file_path, data):
-    try:
-        with open(file_path, "w") as file:
-            json.dump(data, file, indent=4)
-    except OSError as e:
-        log.error(f"Error writing JSON file: {e}")
-
-
-def update_saved_documents(file_path, new_docs):
-    existing_docs = read_json_file(file_path) or []
-
-    if (
-        isinstance(existing_docs, list)
-        and existing_docs
-        and isinstance(existing_docs[0], dict)
-    ):
-        existing_docs = [
-            ConfluenceDocument(
-                doc["metadata"], doc["data"], doc["metadata"].get("deleted", False)
-            )
-            for doc in existing_docs
-        ]
-
-    existing_docs_dict = {doc.metadata["page_id"]: doc for doc in existing_docs}
-
-    for doc in new_docs:
-        existing_docs_dict[doc.metadata["page_id"]] = doc
-
-    updated_docs_list = list(existing_docs_dict.values())
-
-    serialized_docs = [doc.serialize() for doc in updated_docs_list]
-    write_json_file(file_path, serialized_docs)
-
-
-def flag_deleted_pages(file_path, current_confluence_documents):
-    existing_docs = read_json_file(file_path)
-    if existing_docs is None:
-        log.error("Existing documents not found. Exiting.")
-        return
-
-    existing_docs = [
-        ConfluenceDocument(
-            doc["metadata"], doc["data"], doc["metadata"].get("deleted", False)
-        )
-        for doc in existing_docs
-    ]
-
-    current_page_ids = {doc.metadata["page_id"] for doc in current_confluence_documents}
-
-    num_deleted = 0
-    for doc in existing_docs:
-        if doc.metadata["page_id"] not in current_page_ids:
-            doc.metadata["deleted"] = True
-            num_deleted += 1
-    log.info(f"Found {num_deleted} deleted pages.")
-
-    serialized_docs = [doc.serialize() for doc in existing_docs]
-    write_json_file(file_path, serialized_docs)
 
 
 class ConfluenceDataSource:
@@ -94,10 +25,7 @@ class ConfluenceDataSource:
             # TODO: think about configurable limits ? or some streaming solution
             # How do we fit all documents in memory ?
             raw_documents = self.reader.load_data(cql=cql_query, max_num_results=10)
-            return [
-                ConfluenceDocument(doc.metadata, doc.text)
-                for doc in raw_documents
-            ]
+            return raw_documents
         except Exception as e:
             log.error(f"Error fetching documents from Confluence: {e}")
             raise e
@@ -144,26 +72,19 @@ def run(job_input: IJobInput):
     space_key = get_value(job_input, "confluence_space_key")
     parent_page_id = get_value(job_input, "confluence_parent_page_id")
     last_date = get_value(job_input, "last_date", "1900-01-01 12:00")
-    data_file = get_value(
-        job_input,
-        "data_file",
-        os.path.join(job_input.get_temporary_write_directory(), "confluence_data.json"),
-    )
 
     confluence_reader = ConfluenceDataSource(confluence_url, token, space_key)
 
-    updated_docs = confluence_reader.fetch_updated_pages_in_confluence_space(
+    documents = confluence_reader.fetch_updated_pages_in_confluence_space(
         last_date, parent_page_id
     )
-    log.info(f"Found {len(updated_docs)} updated pages")
-    update_saved_documents(data_file, updated_docs)
+    log.info(f"Found {len(documents)} updated pages")
 
     # This is buggy , it doesn't account for server timezone and local timezone
     # But also assumes that server clock and local clock are synchronized (which they are likely not)
     # The ts should be the one of the latest processed page.
     set_property(job_input, "last_date", datetime.now().strftime("%Y-%m-%d %H:%M"))
 
-    flag_deleted_pages(
-        data_file,
-        confluence_reader.fetch_all_pages_in_confluence_space(parent_page_id),
-    )
+    Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+    index = VectorStoreIndex.from_documents(documents)
+    index.storage_context.persist(persist_dir="./persist_dir")
