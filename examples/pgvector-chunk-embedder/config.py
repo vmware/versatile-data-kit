f8e3ee5625789ef8d9c4f1d76316a7b0834b8a18
Yoan Salambashev <yoan.g.salambashev@gmail.com>
2024-02-15 16:56:55 +0200

    examples: delete the pgvector-chunk-embedder job
    Signed-off by: Yoan Salambashev <yoan.salambashev@broadcom.com>

--- examples/pgvector-chunk-embedder/config.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2021-2024 VMware, Inc.
-# SPDX-License-Identifier: Apache-2.0
-
-DOCUMENTS_JSON_FILE_LOCATION = "documents_example.json"
-CLEANED_DOCUMENTS_JSON_FILE_LOCATION = "cleaned_documents_example.json"
-CHUNKS_PKL_FILE_LOCATION = "chunks_example.pkl"
-EMBEDDINGS_PKL_FILE_LOCATION = "embeddings_example.pkl"
-CHUNK_SIZE = 16
-CHUNK_OVERLAP = 5
