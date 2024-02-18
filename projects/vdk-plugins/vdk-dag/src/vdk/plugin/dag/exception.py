104864470969a16eb9392f2c0f89cd707dbaf22c
Antoni Ivanov <aivanov@vmware.com>
2024-02-18 18:43:19 +0200

    vdk-dag: improve error handling and error messages
    To make it easier to debug VDK DAG jobs some improvements in error
    handling are done.
    - Stop using deprecated UserCodeError in favour of domain oriented
    exceptoin class
    - Improve the formatting of the error message in case of failure.
    
    Testing Done: existing tests for regression.
    Here is a comparison before and now in the formatting:
    https://gist.githubusercontent.com/antoniivanov/8f87cdef1c610fcd24361197bcba45dc/raw/c31b990de2c0b1f49ac5989419cfc6ab12aac934/gistfile1.txt

--- projects/vdk-plugins/vdk-dag/src/vdk/plugin/dag/exception.py
+++ projects/vdk-plugins/vdk-dag/src/vdk/plugin/dag/exception.py
@@ -1,10 +1,5 @@
 # Copyright 2023-2024 Broadcom
 # SPDX-License-Identifier: Apache-2.0
-
-# Copyright 2021-2024 VMware, Inc.
-# SPDX-License-Identifier: Apache-2.0
-import json
-from typing import Any
 from typing import List
 from typing import Optional
 
