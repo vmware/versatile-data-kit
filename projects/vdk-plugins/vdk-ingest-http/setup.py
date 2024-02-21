fa23abd59a5b972ab0beedc94c6dd3956fcfda16
Gabriel Georgiev <gageorgiev@vmware.com>
2024-02-21 17:42:28 +0200

    remove ingest-http support
    Signed-off-by: Gabriel Georgiev <gageorgiev@vmware.com>

--- projects/vdk-plugins/vdk-ingest-http/setup.py
+++ projects/vdk-plugins/vdk-ingest-http/setup.py
@@ -30,6 +30,5 @@ setuptools.setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
     ],
 )
