631873277aa550e9c57edf887f9ed1fc1fa828cd
Duygu Hasan <87015481+duyguHsnHsn@users.noreply.github.com>
2024-03-01 19:33:55 +0200

    vdk-core: add test for different db connection and ingest target  (#3160)
    What: added a test that shows we can have different db connection and
    ingest target in a data job
    Linked to: https://github.com/vmware/versatile-data-kit/issues/3128
    
    Follows the scenario: 
    - write and read data from sqlite
    - change the data a little
    - send changed data to another database (duckdb)
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ projects/vdk-core/tests/functional/run/jobs/different-source-and-ingest-target/1_step.py
@@ -0,0 +1,18 @@
+# Copyright 2023-2024 Broadcom
+# SPDX-License-Identifier: Apache-2.0
+from vdk.api.job_input import IJobInput
+
+
+def run(job_input: IJobInput):
+    job_input.execute_query("CREATE TABLE stocks (date text, symbol text, price real)")
+    job_input.execute_query("INSERT INTO stocks VALUES ('2020-01-01', 'GOOG', 122.0)")
+    result = job_input.execute_query("SELECT * FROM stocks")
+    payload = {
+        "date": result[0][0],
+        "symbol": result[0][1],
+        "price": 500,
+    }
+
+    job_input.send_object_for_ingestion(
+        payload=payload, destination_table="test_duckdb_table", method="duckdb"
+    )
