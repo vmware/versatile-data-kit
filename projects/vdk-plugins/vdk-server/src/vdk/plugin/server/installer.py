233d15e3e11dfed0e8b0001e0d9a02e95d3007b7
Paul Murphy <murphp15@tcd.ie>
2024-03-14 08:27:29 +0000

    vdk-server: Upgrade k8s version (#3209)
    # Why
    We have always used an old version of k8s in the installer because
    internally VDK was running on an old version and we would use it to
    catch regressions.
    They are now using a newer version so we can upgrade the version here
    accordingly
    
    # How was this tested? 
    
    Deployed it locally, created and ran a job.

--- projects/vdk-plugins/vdk-server/src/vdk/plugin/server/installer.py
+++ projects/vdk-plugins/vdk-server/src/vdk/plugin/server/installer.py
@@ -484,7 +484,7 @@ class Installer:
                         "--name",
                         self.kind_cluster_name,
                         "--image",
-                        "kindest/node:v1.20.15",
+                        "kindest/node:v1.24.17",
                     ],
                     capture_output=True,
                 )
