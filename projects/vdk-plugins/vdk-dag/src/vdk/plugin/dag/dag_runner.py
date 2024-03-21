c39a1c7c15d26a4bf916b6e061eb6434f6579360
Duygu Hasan <duyguhsnhsn@gmail.com>
2024-03-21 14:49:45 +0200

    modify DagInput with class variable and methods

--- projects/vdk-plugins/vdk-dag/src/vdk/plugin/dag/dag_runner.py
+++ projects/vdk-plugins/vdk-dag/src/vdk/plugin/dag/dag_runner.py
@@ -6,7 +6,8 @@ from typing import Dict
 from typing import List
 from typing import Optional
 
-from vdk.plugin.dag.api.dag import IDagInput, IDagJobStatus
+from vdk.plugin.dag.api.dag import IDagInput
+from vdk.plugin.dag.api.dag import IDagJobStatus
 from vdk.plugin.dag.dag import Dag
 from vdk.plugin.dag.remote_data_job import JobStatus
 
@@ -26,8 +27,8 @@ class DagInput(IDagInput, IDagJobStatus):
     """
     This module is responsible for the execution of DAG of Data Jobs and status checks after the run.
     """
-    def __init__(self):
-        self._running_dag = None
+
+    _running_dag = None  # class variable to hold the DAG instance
 
     def run_dag(self, jobs: List[Dict]):
         """
@@ -36,35 +37,44 @@ class DagInput(IDagInput, IDagJobStatus):
         :param jobs: the list of jobs that are part of the DAG
         :return:
         """
-        self._running_dag = Dag(TEAM_NAME, DAG_CONFIG, JOB_NAME, EXECUTION_ID)
-        self._running_dag.build_dag(jobs)
-        self._running_dag.execute_dag()
-        log.info(f"DAG summary:\n{self._running_dag}")
+        DagInput._running_dag = Dag(TEAM_NAME, DAG_CONFIG, JOB_NAME, EXECUTION_ID)
+        DagInput._running_dag.build_dag(jobs)
+        DagInput._running_dag.execute_dag()
+        log.info(f"DAG summary:\n{DagInput._running_dag}")
 
-    def get_job_status(self, job_name: str) -> Optional[JobStatus]:
+    @classmethod
+    def get_job_status(cls, job_name: str) -> Optional[JobStatus]:
         """
         Get job status.
         :return: JobStatus
         """
-        if self._running_dag:
-            return self._running_dag.get_job_status(job_name)
+        if cls._running_dag:
+            return cls._running_dag.get_job_status(job_name)
         else:
-            log.warning(f"Job status cannot be checked since there are no registered DAG runs!")
+            log.warning(
+                f"Job status cannot be checked since there are no registered DAG runs!"
+            )
             return None
 
-    def get_all_jobs_execution_status(self) -> Optional[Dict[str, JobStatus]]:
+    @classmethod
+    def get_all_jobs_execution_status(cls) -> Optional[Dict[str, JobStatus]]:
         """
         Fetches the statuses of all jobs in the latest DAG execution.
 
         :return: Dict[str, JobStatus] - A dictionary where keys are job names and values are their statuses.
         """
-        if self._running_dag:
-            return self._running_dag.get_all_jobs_execution_status()
+        if cls._running_dag:
+            return cls._running_dag.get_all_jobs_execution_status()
         else:
-            log.warning(f"Job statuses cannot be checked since there are no registered DAG runs!")
+            log.warning(
+                f"Job statuses cannot be checked since there are no registered DAG runs!"
+            )
             return None
 
-    def get_jobs_execution_status(self, job_names: List[str]) -> Optional[Dict[str, JobStatus]]:
+    @classmethod
+    def get_jobs_execution_status(
+        cls, job_names: List[str]
+    ) -> Optional[Dict[str, JobStatus]]:
         """
         Fetches the execution statuses of specified jobs.
 
@@ -72,8 +82,10 @@ class DagInput(IDagInput, IDagJobStatus):
         :return: Dict[str, JobStatus] - A dictionary where keys are job names from the input list and values are their
          statuses.
         """
-        if self._running_dag:
-            self._running_dag.get_jobs_execution_status(job_names)
+        if cls._running_dag:
+            cls._running_dag.get_jobs_execution_status(job_names)
         else:
-            log.warning(f"Job statuses cannot be checked since there are no registered DAG runs!!")
+            log.warning(
+                f"Job statuses cannot be checked since there are no registered DAG runs!!"
+            )
             return None
