13686b10fbedbb718d2d62c577afce0f830ad7a0
Yoan Salambashev <yoan.g.salambashev@gmail.com>
2024-03-21 18:35:29 +0200

    control-service: add aws ses smtp
    Signed-off-by: Yoan Salambashev <yoan.salambashev@broadcom.com>

---
+++ projects/control-service/projects/pipelines_control_service/src/main/java/com/vmware/taurus/service/notification/AmazonEmailNotification.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2023-2024 Broadcom
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.vmware.taurus.service.notification;
+
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import com.amazonaws.services.simpleemail.model.*;
+import com.google.common.collect.Lists;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+
+/**
+ * The AmazonEmailNotification class is responsible for implementing the email notifications
+ * functionality in AWS using SES. The single exposed send method accepts pre-build notification object.
+ *
+ * @see DataJobNotification
+ * @see NotificationContent
+ */
+//@Profile("AWS")
+@Primary
+@Component
+public class AmazonEmailNotification extends EmailNotification {
+
+  static Logger log = LoggerFactory.getLogger(AmazonEmailNotification.class);
+
+  private String region;
+  private final AmazonSimpleEmailService sesClient;
+
+  public AmazonEmailNotification(EmailConfiguration emailConfiguration) {
+      super(emailConfiguration);
+      this.region = "us-east-1";
+      this.sesClient = AmazonSimpleEmailServiceClientBuilder.standard()
+              .withCredentials(new DefaultAWSCredentialsProviderChain())
+              .withRegion(this.region)
+              .build();
+  }
+
+  @Override
+  public void send(NotificationContent notificationContent) throws MessagingException {
+    SendEmailResult sendEmailResult = sesClient.sendEmail(
+            new SendEmailRequest()
+                .withSource("yoan.salambashev@broadcom.com")
+                .withDestination(new Destination(Lists.newArrayList("yoan.salambashev@broadcom.com")))
+                .withMessage(
+                    new Message(
+                        new Content("this is a test"), new Body(new Content("this is a test")))));
+    log.info("Email sent using Amazon SES. Message ID: {}", sendEmailResult.getMessageId());
+  }
+}
