3381dc2bc7e84831d5f91ea6b47512f2376be44b
Yoan Salambashev <yoan.g.salambashev@gmail.com>
2024-03-27 14:40:50 +0200

    control-service: assume powerful role to send notifications
    Signed-off by: Yoan Salambashev <yoan.salambashev@broadcom.com>

--- projects/control-service/projects/pipelines_control_service/src/main/java/com/vmware/taurus/service/notification/AmazonEmailNotification.java
+++ projects/control-service/projects/pipelines_control_service/src/main/java/com/vmware/taurus/service/notification/AmazonEmailNotification.java
@@ -5,13 +5,17 @@
 
 package com.vmware.taurus.service.notification;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.auth.STSAssumeRoleSessionCredentialsProvider;
+import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClientBuilder;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
 import com.amazonaws.services.simpleemail.model.*;
 import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
@@ -30,29 +34,41 @@ import javax.mail.MessagingException;
 public class AmazonEmailNotification extends EmailNotification {
 
   static Logger log = LoggerFactory.getLogger(AmazonEmailNotification.class);
+  private final String roleArn;
 
-  private String region;
-  private final AmazonSimpleEmailService sesClient;
+  private final String region;
 
-  public AmazonEmailNotification(EmailConfiguration emailConfiguration) {
+  public AmazonEmailNotification(EmailConfiguration emailConfiguration, @Value("datajobs.aws.roleArn") String roleArn) {
     super(emailConfiguration);
     log.info("Constructing AmazonEmailNotification class");
     this.region = "us-east-1";
-    this.sesClient =
-        AmazonSimpleEmailServiceClientBuilder.standard()
-            .withCredentials(new DefaultAWSCredentialsProviderChain())
-            .withRegion(this.region)
-            .build();
+    this.roleArn = roleArn;
   }
 
   @Override
   public void send(NotificationContent notificationContent) throws MessagingException {
+    String roleSessionName = "email-session";
+
+    STSAssumeRoleSessionCredentialsProvider credentialsProvider = new STSAssumeRoleSessionCredentialsProvider
+            .Builder(this.roleArn, roleSessionName)
+            .withStsClient(AWSSecurityTokenServiceClientBuilder.standard()
+                    .withCredentials(new DefaultAWSCredentialsProviderChain())
+                    .withRegion(this.region)
+                    .build())
+            .build();
+
+    AmazonSimpleEmailService sesClient = AmazonSimpleEmailServiceClientBuilder.standard()
+            .withCredentials(new AWSStaticCredentialsProvider(credentialsProvider.getCredentials()))
+            .withRegion(this.region)
+            .build();
+
     log.info("Send AmazonEmailNotification");
     SendEmailResult sendEmailResult =
         sesClient.sendEmail(
             new SendEmailRequest()
                 .withSource("paulm2@vmware.com")
-                .withDestination(new Destination(Lists.newArrayList("paulm2@vmware.com")))
+                .withDestination(
+                    new Destination(Lists.newArrayList("yoan.salambashev@broadcom.com")))
                 .withMessage(
                     new Message(
                         new Content("this is a test"), new Body(new Content("this is a test")))));
