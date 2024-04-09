ba92f7664dde25cc59e5ec0e96627a8f00a463de
murphp15 <murphp15@tcd.ie>
2024-04-09 12:58:58 +0100

    gpu management poc

---
+++ projects/control-service/projects/gpu_management/docs/VDK_INTEGRATION.md
@@ -0,0 +1,16 @@
+
+# API definition
+
+
+
+
+
+
+
+# Scenario 1:
+
+![ddf](user_wants_to_deploy_job_even_though_team_is_over_budget.svg)
+
+# Scenario 2:
+
+![ddf](user_wants_to_deploy_job_even_though_team_is_over_budget.svg)
--- projects/control-service/projects/gpu_management/src/main/kotlin/com/vmware/gpu/management/api/GpuResourceManager.kt
+++ projects/control-service/projects/gpu_management/src/main/kotlin/com/vmware/gpu/management/api/GpuResourceManager.kt
@@ -1,32 +1,11 @@
 package com.vmware.gpu.management.api
 
+
+
 interface GpuResourceManager {
+
     fun jobEnded(jobUniqueIdentifier: JobUniqueIdentifier)
     fun markJobPriority(jobUniqueIdentifier: JobUniqueIdentifier, priority: Boolean)
     fun tryProvisionResources(jobUniqueIdentifier: JobUniqueIdentifier, amount: Float): List<JobAction>
 }
 
-
-data class JobUniqueIdentifier(val jobName: String,val teamName: String){
-    val uniqueID = this.teamName + "____" + this.jobName
-}
-
-
-sealed interface JobAction
-data class CreateJob(val nodeName: String, val jobUniqueIdentifier: JobUniqueIdentifier, val amount: Float) :
-    JobAction {
-    constructor(nodeName: String, provisionResourceRequest: ProvisionResourceRequest) :
-            this(
-                nodeName,
-                provisionResourceRequest.jobUniqueIdentifier,
-                provisionResourceRequest.amount
-            )
-}
-data class MoveJob(val nodeName: String, val jobUniqueIdentifier: JobUniqueIdentifier, val amount: Float) : JobAction
-data class DeleteJob(val jobUniqueIdentifier: JobUniqueIdentifier) : JobAction {
-    val uId = jobUniqueIdentifier.uniqueID
-}
-
-data class ProvisionResourceRequest(val jobUniqueIdentifier: JobUniqueIdentifier, val amount: Float){
-    val teamName = jobUniqueIdentifier.teamName
-}
