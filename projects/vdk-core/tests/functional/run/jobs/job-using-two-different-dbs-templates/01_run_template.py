e1302f7c358431b5a8f8df8851498aa29de6bfb1
Dilyan Marinov <91800778+DeltaMichael@users.noreply.github.com>
2024-05-09 14:44:14 +0300

    vdk-core: support templates for multiple databases (#3351)
    Signed-off-by: Dilyan Marinov <mdilyan@vmware.com>
    Co-authored-by: Dilyan Marinov <mdilyan@vmware.com>

---
+++ projects/vdk-core/tests/functional/run/jobs/job-using-two-different-dbs-templates/01_run_template.py
@@ -0,0 +1,13 @@
+# Copyright 2023-2024 Broadcom
+# SPDX-License-Identifier: Apache-2.0
+import logging
+
+import pandas as pd
+from vdk.api.job_input import IJobInput
+
+log = logging.getLogger(__name__)
+
+
+def run(job_input: IJobInput):
+    args = {"first_db": "sqlite", "second_db": "duckdb"}
+    job_input.execute_template("mul-sql", args)
---
+++ projects/vdk-core/tests/functional/run/jobs/template-multiple-different-dbs-sql/20_insert_first.sql
@@ -0,0 +1,2 @@
+[{first_db}]
+INSERT INTO stocks VALUES ('2020-01-01', 'GOOG', 122.0)
