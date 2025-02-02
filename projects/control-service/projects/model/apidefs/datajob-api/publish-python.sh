b225a22e6fb8787dbc03823eb25db7aba4983c9a
David Hotham <david.hotham@blueyonder.co.uk>
2025-02-02 22:51:26 +0000

    publish wheels
    Signed-off-by: David Hotham <david.hotham@blueyonder.co.uk>

--- projects/control-service/projects/model/apidefs/datajob-api/publish-python.sh
+++ projects/control-service/projects/model/apidefs/datajob-api/publish-python.sh
@@ -9,9 +9,9 @@ cd build/python
 python3.7 -m venv venv
 source venv/bin/activate
 
-pip install -U pip setuptools wheel twine
+pip install -U pip build twine
 pip install -r requirements.txt
 
 rm -rf dist/
-python setup.py sdist --formats=gztar
-twine upload --repository-url "$PIP_REPO_UPLOAD_URL" -u "$PIP_REPO_UPLOAD_USER_NAME" -p "$PIP_REPO_UPLOAD_USER_PASSWORD" dist/*tar.gz
+python -m build
+twine upload --repository-url "$PIP_REPO_UPLOAD_URL" -u "$PIP_REPO_UPLOAD_USER_NAME" -p "$PIP_REPO_UPLOAD_USER_PASSWORD" dist/*
